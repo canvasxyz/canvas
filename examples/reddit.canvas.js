ce65caac95d471ef2107f60e737a4f0fb49535c3
Raymond Z <raykyri@gmail.com>
2022-03-18 15:15:21 -0400

    naively calculate thread and comment scores, and sort by them, for reddit demo

--- examples/reddit.canvas.js
+++ examples/reddit.canvas.js
@@ -12,39 +12,88 @@ const comments = canvas.model('comments', {
   creator: 'text',
   createdAt: 'datetime',
 });
-const upvotes = canvas.model('upvotes', {
+const threadVotes = canvas.model('threadVotes', {
   id: 'primary',
   threadId: 'threads.id',
   creator: 'text',
   createdAt: 'datetime',
-}, [canvas.uniqueIndex('threadId', 'creator')]);
-const commentUpvotes = canvas.model('commentUpvotes', {
+}); // canvas.uniqueIndex('threadId', 'creator')
+const commentVotes = canvas.model('commentVotes', {
   id: 'primary',
   commentId: 'comments.id',
   creator: 'text',
   createdAt: 'datetime',
-}, [canvas.uniqueIndex('commentId', 'creator')]);
+}); // canvas.uniqueIndex('commentId', 'creator')
 
-canvas.route('/latest', 'SELECT * FROM threads ORDER BY createdAt DESC OFFSET :offset LIMIT 30');
-canvas.route('/top', `SELECT threads.*, SUM(MIN(1, 1 / (julianday('now') - julianday(upvotes.created)))) as score, group_concat(upvotes.creator)
-FROM threads JOIN upvotes ON thread.id = upvotes.threadId
-WHERE votes.createdAt > NOW() - 90
-ORDER BY score OFFSET :offset LIMIT 30`);
-canvas.route('/:id/comments', `SELECT comments.*, COUNT(commentUpvotes), group_concat(commentUpvotes.creator) as upvotes FROM comments
-JOIN commentUpvotes ON comments.id=commentUpvotes.commentId
-ORDER BY upvotes DESC
-OFFSET :offset
+canvas.route('/latest', `SELECT threads.*, COUNT(comments.id)
+FROM threads
+    JOIN comments ON comments.threadId = threads.id
+GROUP BY threads.id
+ORDER BY threads.createdAt DESC
+LIMIT 30
+`);
+
+canvas.route('/top', `SELECT
+    threads.*,
+    SUM(
+        1 / (cast(strftime('%s','now') as float) * 1000 - threadVotes.createdAt)
+    ) AS score,
+    group_concat(threadVotes.creator)
+FROM threads
+    LEFT JOIN threadVotes ON threads.id = threadVotes.threadId
+    WHERE threadVotes.createdAt > datetime('now', '-90 days')
+GROUP BY threads.id
+ORDER BY score DESC
+LIMIT 30`);
+
+canvas.route('/threads/:threadId/comments', `SELECT
+    comments.*,
+    SUM(
+        1 / (cast(strftime('%s','now') as float) * 1000 - commentVotes.createdAt)
+    ) AS score,
+    group_concat(commentVotes.creator)
+FROM comments
+    LEFT JOIN commentVotes ON comments.id = commentVotes.commentId
+    WHERE comments.threadId = :threadId
+GROUP BY comments.id
+ORDER BY score DESC
 LIMIT 30`);
 
 const thread = canvas.action('thread(title, link)', function (title, link) {
-  return canvas.db.threads.create({ id: this.id, title, link, creator: this.origin });
+    return canvas.db.threads.create({
+        id: this.id,
+        title,
+        link,
+        creator: this.origin,
+        createdAt: +new Date()
+    });
 });
 const comment = canvas.action('comment(threadId, text)', function (threadId, text) {
-  return canvas.db.comments.create({ id: this.id, threadId, text, creator: this.origin });
+    return canvas.db.comments.create({
+        id: this.id,
+        threadId,
+        text,
+        creator: this.origin,
+        createdAt: +new Date()
+    });
 });
-const upvote = canvas.action('upvote(threadId)', function (threadId) {
-  return canvas.db.upvotes.create({ id: this.id, threadId, creator: this.origin });
+const voteThread = canvas.action('voteThread(threadId, value)', function (threadId, value) {
+    //if (value !== 1 || value !== -1) return false;
+    return canvas.db.threadVotes.create({
+        id: this.id,
+        threadId,
+        creator: this.origin,
+        // value,
+        createdAt: timestamp,
+    });
 });
-const upvoteComment = canvas.action('upvoteComment(commentId)', function (commentId) {
-  return canvas.db.commentUpvotes.create({ id: this.id, commentId, creator: this.origin });
+const voteComment = canvas.action('voteComment(commentId, value)', function (commentId, value) {
+    //if (value !== 1 || value !== -1) return false;
+    return canvas.db.commentVotes.create({
+        id: this.id,
+        commentId,
+        creator: this.origin,
+        // value,
+        createdAt: timestamp,
+    });
 });
