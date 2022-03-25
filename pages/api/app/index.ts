4e5373cc2b08d81d1202548691ed10a8862b82a6
Joel Gustafson <joelg@mit.edu>
2022-03-25 12:36:17 -0400

    add timestamp to draft specs to avoid hash collisions for now

--- pages/api/app/index.ts
+++ pages/api/app/index.ts
@@ -2,7 +2,7 @@ import { NextApiRequest, NextApiResponse } from "next"
 import { StatusCodes } from "http-status-codes"
 
 import { prisma } from "utils/server/services"
-import { getDefaultSpecTemplate } from "utils/server/defaultSpecTemplate"
+import { getDefaultSpec } from "utils/server/defaultSpecTemplate"
 import { alphanumeric } from "utils/shared/regexps"
 
 import * as t from "io-ts"
@@ -24,7 +24,7 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
 
 	const { slug } = await prisma.app.create({
 		select: { slug: true },
-		data: { ...req.body, draft_spec: getDefaultSpecTemplate() },
+		data: { ...req.body, draft_spec: getDefaultSpec() },
 	})
 
 	res.status(StatusCodes.CREATED).setHeader("Location", `/app/${slug}`).end()
--- utils/server/defaultSpecTemplate.ts
+++ utils/server/defaultSpecTemplate.ts
@@ -1,5 +1,8 @@
 import fs from "node:fs"
 
-export function getDefaultSpecTemplate(): string {
-	return fs.readFileSync("examples/reddit.canvas.js", "utf-8")
+export function getDefaultSpec(): string {
+	const template = fs.readFileSync("examples/reddit.canvas.js", "utf-8")
+	const date = new Date()
+	const header = `// Created on ${date.toDateString()} at ${date.toTimeString()}`
+	return `${header}\n\n${template}`
 }
