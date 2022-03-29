a8997037fa727aa8e7d3b99fa2b4215201067f5d
Raymond Z <raykyri@gmail.com>
2022-03-29 12:04:07 -0400

    add header menu

---
+++ components/UserMenu.tsx
@@ -0,0 +1,49 @@
+import { useState } from "react"
+import ReactDOM from "react-dom"
+import { usePopper } from "react-popper"
+import { Popover } from "@headlessui/react"
+import Link from "next/link"
+import dynamic from "next/dynamic"
+
+function UserMenu() {
+	const [referenceElement, setReferenceElement] = useState<HTMLButtonElement | null>(null)
+	const [popperElement, setPopperElement] = useState<HTMLDivElement | null>(null)
+	const { styles, attributes } = usePopper(referenceElement, popperElement, {
+		placement: "bottom-end",
+		strategy: "absolute",
+		modifiers: [
+			{
+				name: "arrow",
+			},
+		],
+	})
+
+	return (
+		<Popover className="">
+			<Popover.Button ref={setReferenceElement} className={`text-sm pt-0.25`}>
+				username
+			</Popover.Button>
+
+			{ReactDOM.createPortal(
+				<Popover.Panel
+					ref={setPopperElement}
+					className="absolute z-10 bg-white border border-gray-200 rounded shadow w-28 mt-2"
+					style={styles.popper}
+					{...attributes.popper}
+				>
+					<div>
+						<a
+							className="block px-3 py-2 hover:bg-gray-100 text-sm border-b border-gray-200"
+							href="#"
+							onClick={() => alert("unimplemented: logout")}
+						>
+							Logout
+						</a>
+					</div>
+				</Popover.Panel>,
+				document.querySelector(".app-body")!
+			)}
+		</Popover>
+	)
+}
+export default dynamic(async () => UserMenu, { ssr: false })
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,7 @@ import type { AppProps } from "next/app"
 import Head from "next/head"
 import Link from "next/link"
 
+import UserMenu from "components/UserMenu"
 import "../styles/globals.css"
 
 export default function App({ Component, pageProps }: AppProps) {
@@ -14,8 +15,11 @@ export default function App({ Component, pageProps }: AppProps) {
 				<div className="app-header"></div>
 				<div className="app-body">
 					<div className="my-4 pb-3.5 border-b border-gray-200">
-						<div className="px-10 mx-auto font-semibold">
-							<Link href="/">Canvas</Link>
+						<div className="px-10 mx-auto flex">
+							<div className="flex-1 font-semibold">
+								<Link href="/">Canvas</Link>
+							</div>
+							<UserMenu />
 						</div>
 					</div>
 					<div className="px-10 mx-auto">
