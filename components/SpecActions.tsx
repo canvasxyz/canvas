4cbfe920693f290d956e896dc0e9f20b08c79f3a
Joel Gustafson <joelg@mit.edu>
2022-03-30 13:12:50 -0400

    better loading behavior in SpecActions

--- components/SpecActions.tsx
+++ components/SpecActions.tsx
@@ -6,14 +6,15 @@ import { Action } from "utils/server/types"
 const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 export function Actions(props: { multihash: string }) {
-	const { data, error } = useSWR(`/api/instance/${props.multihash}/actions`, fetcher, { refreshInterval: 1000 })
-	useEffect(() => {
-		console.log("holy shit got data and error", data, error)
-	}, [data, error])
+	const { data, error } = useSWR<Action[]>(`/api/instance/${props.multihash}/actions`, fetcher, {
+		refreshInterval: 1000,
+	})
 
 	return (
 		<div className="border border-gray-300 rounded overflow-scroll bg-gray-50 pb-3 overflow-x-scroll">
-			{Array.isArray(data) && data.length > 0 ? (
+			{data === undefined ? null : data.length === 0 ? (
+				<div>No actions yet</div>
+			) : (
 				<table className="table-auto text-left text-xs leading-tight w-full">
 					<thead className="bg-white border-b border-gray-300">
 						<tr>
@@ -34,8 +35,6 @@ export function Actions(props: { multihash: string }) {
 						))}
 					</tbody>
 				</table>
-			) : (
-				<div>No actions yet</div>
 			)}
 		</div>
 	)
