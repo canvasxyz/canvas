e9edceb5791c8e22af6fadc8f1095cb7a17b12e5
Joel Gustafson <joelg@mit.edu>
2022-04-06 23:49:43 -0400

    removed dist and lib libraries

--- packages/canvas-cli/dist/run.js
+++
@@ -1,7 +0,0 @@
-import * as fs from "node:fs";
-import * as path from "node:path";
-import { App } from "canvas-core";
-export default async function run(args) {
-    const multihash = fs.readFileSync(path.resolve(args.path, "spec.cid"), "utf-8");
-    await App.initialize({ multihash, path: args.path, port: args.port });
-}
--- packages/canvas-core/lib/index.js
+++
@@ -1,4 +0,0 @@
-export * from "./app.js";
-export * from "./appStatus.js";
-export * from "./models.js";
-export * from "./actions.js";
