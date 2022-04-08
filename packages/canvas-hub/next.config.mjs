69550768292a8bf75d2d6a60a0d20ae1af955f02
Raymond Z <raykyri@gmail.com>
2022-04-08 14:28:44 -0400

    add webpack ignore plugin

--- packages/canvas-hub/next.config.mjs
+++ packages/canvas-hub/next.config.mjs
@@ -3,6 +3,7 @@ import ForkTsCheckerWebpackPlugin from "fork-ts-checker-webpack-plugin";
 const config = {
 	webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
 		config.plugins.push(new ForkTsCheckerWebpackPlugin())
+		config.plugins.push(new webpack.IgnorePlugin({ resourceRegExp: /^electron$/ }));
 
 		// return the modified config
 		return config
