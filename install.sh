c46d0a4df3e4a95d6b76abc70be5f92a8bad4cad
Raymond Z <raykyri@gmail.com>
2022-04-11 15:18:22 -0400

    prompt to overwrite past installs of canvas cli

--- install.sh
+++ install.sh
@@ -1,8 +1,24 @@
 #!/usr/bin/env sh
 
 INSTALL_PATH=$(npm config get prefix)/bin/canvas
-rm -f ${INSTALL_PATH}
-touch ${INSTALL_PATH}
-echo "#!/usr/bin/env sh" >> ${INSTALL_PATH}
-echo "node ${PWD}/packages/canvas-cli/index.js \$@" >> ${INSTALL_PATH}
+
+if test -f "$INSTALL_PATH"; then
+    read -p "Already installed. Overwrite any past install? [y/N] " -n 1 -r
+    if [[ $REPLY =~ ^[Yy]$ ]]
+    then
+        # Overwrite any past install
+        echo "#!/usr/bin/env sh" > ${INSTALL_PATH}
+    else
+        echo
+        echo "Aborting, ok!"
+        exit 1
+    fi
+else
+    # Create a new install
+    touch ${INSTALL_PATH}
+    echo "#!/usr/bin/env sh" >> ${INSTALL_PATH}
+fi
+echo "node ${PWD}/packages/canvas-cli/dist/index.js \$@" >> ${INSTALL_PATH}
 chmod +x ${INSTALL_PATH}
+echo
+echo "Done!"
