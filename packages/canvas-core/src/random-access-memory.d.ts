ebdb60efb1c2c583b58c87a71069e630f70e9177
Joel Gustafson <joelg@mit.edu>
2022-04-13 11:33:45 -0400

    fix typo in typescript declaration

--- packages/canvas-core/src/random-access-memory.d.ts
+++ packages/canvas-core/src/random-access-memory.d.ts
@@ -1,4 +1,4 @@
-declare module "random-access-file" {
+declare module "random-access-memory" {
 	import { RandomAccessStorage } from "random-access-storage"
 	export default function randomAccessMemory(): RandomAccessStorage
 }
