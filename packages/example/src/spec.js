8e8911cd6f2fa461b642fc2347adbe6116005874
Raymond Z <raykyri@gmail.com>
2022-05-24 14:34:11 -0400

    use individual exports, not default export

--- packages/example/src/spec.js
+++ packages/example/src/spec.js
@@ -1,29 +1,29 @@
-const spec = {
-	models: {
-		threads: {
-			title: "string",
-		},
-		likes: {
-			threadId: "string",
-			value: "boolean",
-		},
+const models = {
+	threads: {
+		title: "string",
 	},
-	routes: {
-		"/threads":
-			"SELECT threads.id, threads.title, threads.timestamp, COUNT(IIF(likes.value, 1, NULL)) as likes FROM threads LEFT JOIN likes ON likes.threadId = threads.id GROUP BY threads.id",
-	},
-	actions: {
-		createThread: function (title) {
-			if (!title || !title.trim()) throw new Error("Invalid title")
-			this.db.threads.set(this.hash, { title })
-		},
-		like: function (threadId) {
-			this.db.likes.set(this.from + threadId, { threadId, value: true })
-		},
-		unlike: function (threadId) {
-			this.db.likes.set(this.from + threadId, { threadId, value: false })
-		},
+	likes: {
+		threadId: "string",
+		value: "boolean",
 	},
 }
 
-export default spec
+const routes = {
+	"/threads":
+		"SELECT threads.id, threads.title, threads.timestamp, COUNT(IIF(likes.value, 1, NULL)) as likes FROM threads LEFT JOIN likes ON likes.threadId = threads.id GROUP BY threads.id",
+}
+
+const actions = {
+	createThread: function (title) {
+		if (!title || !title.trim()) throw new Error("Invalid title")
+		this.db.threads.set(this.hash, { title })
+	},
+	like: function (threadId) {
+		this.db.likes.set(this.from + threadId, { threadId, value: true })
+	},
+	unlike: function (threadId) {
+		this.db.likes.set(this.from + threadId, { threadId, value: false })
+	},
+}
+
+export { models, routes, actions }
