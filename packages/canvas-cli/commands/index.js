74d590e74fcb8812ac639a12fcf91453c113f1fe
Raymond Z <raykyri@gmail.com>
2022-05-25 00:48:33 -0400

    add canvas init command

--- packages/canvas-cli/commands/index.js
+++ packages/canvas-cli/commands/index.js
@@ -1,7 +1,8 @@
+import * as init from "./init.js"
 import * as info from "./info.js"
 import * as list from "./list.js"
 
 import * as run from "./run.js"
 import * as download from "./download.js"
 
-export const commands = [info, list, run, download]
+export const commands = [init, info, list, run, download]
---
+++ packages/canvas-cli/commands/init.js
@@ -0,0 +1,54 @@
+import fs from "fs"
+import path from "node:path"
+
+export const command = "init <spec>"
+export const desc = "Create a starter spec"
+
+export const builder = (yargs) => {
+	yargs.positional("spec", {
+		describe: "Path to spec file to create",
+		type: "string",
+		demandOption: true,
+	})
+}
+
+export async function handler(args) {
+	if (fs.existsSync(args.spec)) {
+		console.log("File already exists, refusing to overwrite")
+		return
+	}
+
+	const content = `
+const models = {
+	threads: {
+		title: "string",
+	},
+	likes: {
+		threadId: "string",
+		value: "boolean",
+	},
+}
+
+const routes = {
+	"/threads":
+		"SELECT threads.id, threads.title, threads.timestamp, COUNT(IIF(likes.value, 1, NULL)) as likes FROM threads LEFT JOIN likes ON likes.threadId = threads.id GROUP BY threads.id",
+}
+
+const actions = {
+	createThread: function (title) {
+		if (!title || !title.trim()) throw new Error("Invalid title")
+		this.db.threads.set(this.hash, { title })
+	},
+	like: function (threadId) {
+		this.db.likes.set(this.from + threadId, { threadId, value: true })
+	},
+	unlike: function (threadId) {
+		this.db.likes.set(this.from + threadId, { threadId, value: false })
+	},
+}
+
+export { models, routes, actions }
+`
+	fs.writeFileSync(args.spec, content.trim())
+	console.log(`Created starter spec at ${args.spec}`)
+}
