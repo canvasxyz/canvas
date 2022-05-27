9993f09af04cef40ba3fe8a242c9b9f158eb8dbf
Joel Gustafson <joelg@mit.edu>
2022-05-26 21:06:34 -0400

    core: remove separate runtime validatoes for action and session payloads

--- packages/canvas-core/src/sessions.ts
+++ packages/canvas-core/src/sessions.ts
@@ -15,20 +15,18 @@ export type SessionPayload = {
 	session_duration: number
 }
 
-export const sessionPayloadType: t.Type<SessionPayload> = t.type({
-	from: t.string,
-	spec: t.string,
-	timestamp: t.number,
-	session_public_key: t.string,
-	session_duration: t.number,
-})
-
 export type Session = {
-	payload: SessionPayload
 	signature: string
+	payload: SessionPayload
 }
 
 export const sessionType: t.Type<Session> = t.type({
-	payload: sessionPayloadType,
 	signature: t.string,
+	payload: t.type({
+		from: t.string,
+		spec: t.string,
+		timestamp: t.number,
+		session_public_key: t.string,
+		session_duration: t.number,
+	}),
 })
