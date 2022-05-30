ddbd3129f3504203cd313820e10f772f3ae9ef56
Joel Gustafson <joelg@mit.edu>
2022-05-30 10:33:32 -0400

    add HEAD method to CLI server and ETag header to responses

--- packages/canvas-cli/commands/run.js
+++ packages/canvas-cli/commands/run.js
@@ -110,7 +110,14 @@ export async function handler(args) {
 	server.use(bodyParser.json())
 
 	server.get("/", (req, res) => {
+		res.header("ETag", `"${core.multihash}"`)
 		res.json({ multihash: core.multihash, spec: core.spec })
+		return
+	})
+
+	server.head("/", (req, res) => {
+		res.status(StatusCodes.OK)
+		res.header("ETag", `"${core.multihash}"`)
 		return
 	})
 
