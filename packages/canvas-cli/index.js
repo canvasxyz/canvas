a7ea40261555a7112270227830c4e1d7466eaa30
Raymond Z <raykyri@gmail.com>
2022-06-09 10:54:26 -0400

    notify user when cli is updated

--- packages/canvas-cli/index.js
+++ packages/canvas-cli/index.js
@@ -1,9 +1,17 @@
 #!/usr/bin/env node
-
+import fs from "node:fs"
+import updateNotifier from "update-notifier"
 import yargs from "yargs"
 import { hideBin } from "yargs/helpers"
 
 import { commands } from "./commands/index.js"
+import pkg from "./package.json" assert { type: "json" }
+
+const notifier = updateNotifier({
+  pkg,
+  updateCheckInterval: 1 // always check
+})
+notifier.notify()
 
 yargs(hideBin(process.argv))
 	.command(commands)
