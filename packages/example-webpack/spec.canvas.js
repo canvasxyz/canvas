c361e208d224532fa34a1c36f6ca039a6736b98d
Raymond Z <raykyri@gmail.com>
2022-07-05 17:17:17 -0400

    fix like counting

--- packages/example-webpack/spec.canvas.js
+++ packages/example-webpack/spec.canvas.js
@@ -5,6 +5,7 @@ export const models = {
 		indexes: ["updated_at"],
 	},
 	likes: {
+		from_id: "string",
 		post_id: "string",
 		value: "boolean",
 	},
@@ -14,7 +15,7 @@ export const routes = {
 	"/posts":
 		"SELECT posts.id, posts.from_id, posts.content, posts.updated_at, COUNT(IIF(likes.value, 1, NULL)) as likes FROM posts LEFT JOIN likes ON likes.post_id = posts.id GROUP BY posts.id ORDER BY posts.updated_at DESC LIMIT 50",
 	"/posts/:from_id":
-		"SELECT posts.id, posts.from_id, posts.content, posts.updated_at, COUNT(IIF(likes.value, 1, NULL)) as likes, COUNT(IIF(my_likes.value, 1, NULL)) as my_likes, 1 as yr_likes FROM posts LEFT JOIN likes ON likes.post_id = posts.id LEFT JOIN likes my_likes ON my_likes.post_id = posts.id AND posts.from_id = :from_id GROUP BY posts.id ORDER BY posts.updated_at DESC LIMIT 50",
+		"SELECT posts.id, posts.from_id, posts.content, posts.updated_at, COUNT(IIF(likes.value, 1, NULL)) as likes, COUNT(IIF(my_likes.value, 1, NULL)) as my_likes FROM posts LEFT JOIN likes ON likes.post_id = posts.id LEFT JOIN likes my_likes ON my_likes.post_id = posts.id AND my_likes.post_id = posts.id AND my_likes.from_id = :from_id GROUP BY posts.id ORDER BY posts.updated_at DESC LIMIT 50",
 }
 
 export const actions = {
@@ -22,9 +23,17 @@ export const actions = {
 		this.db.posts.set(this.hash, { content, from_id: this.from })
 	},
 	like(postId) {
-		this.db.likes.set(`${this.from}/${postId}`, { post_id: postId, value: true })
+		this.db.likes.set(`${this.from}/${postId}`, {
+			from_id: this.from,
+			post_id: postId,
+			value: true,
+		})
 	},
 	unlike(postId) {
-		this.db.likes.set(`${this.from}/${postId}`, { post_id: postId, value: false })
+		this.db.likes.set(`${this.from}/${postId}`, {
+			from_id: this.from,
+			post_id: postId,
+			value: false,
+		})
 	},
 }
