96e414ec528c20de14a2b7f670e8eb89bbdedff3
Raymond Z <raykyri@gmail.com>
2022-08-12 12:21:52 -0400

    reimplement `canvas export`

--- packages/canvas-cli/commands/export.js
+++ packages/canvas-cli/commands/export.js
@@ -1,10 +1,11 @@
 import fs from "node:fs"
 import path from "node:path"
-
 import chalk from "chalk"
 
+import { getQuickJS } from "quickjs-emscripten"
+
 import { Core } from "@canvas-js/core"
-import { locateSpec } from "../utils.js"
+import { locateSpec, setupRpcs } from "../utils.js"
 
 export const command = "export <spec>"
 export const desc = "Export actions and sessions to stdout"
@@ -24,24 +25,47 @@ export const builder = (yargs) => {
 			desc: "IPFS HTTP API URL",
 			default: "http://localhost:5001",
 		})
+		.option("verbose", {
+			type: "boolean",
+			desc: "Enable verbose logging",
+			default: false,
+		})
+		.option("compact", {
+			describe: "Don't pretty-print exported JSON",
+			type: "boolean",
+			default: false,
+		})
 }
 
 export async function handler(args) {
-	const { directory } = await locateSpec({ ...args, temp: false })
+	const { directory, name, spec, development } = await locateSpec(args)
 
-	// TODO: select message log from database
-	for await (const entry of []) {
-		if (entry.type === "put") {
-			const value = JSON.parse(entry.value)
-			if (entry.key.startsWith(Core.actionKeyPrefix)) {
-				console.log(JSON.stringify({ type: "action", ...value }))
-			} else if (entry.key.startsWith(Core.sessionKeyPrefix)) {
-				console.log(JSON.stringify({ type: "session", ...value }))
-			} else {
-				console.error(chalk.red("[canvas-cli] Skipping invalid entry"))
-			}
+	const quickJS = await getQuickJS()
+	const rpc = setupRpcs(args)
+	const core = await Core.initialize({
+		name,
+		spec,
+		directory,
+		quickJS,
+		verbose: args.verbose,
+		rpc,
+		unchecked: true,
+		development,
+	})
+
+	let i = 0
+	for await (const [key, value] of core.store.getHistoryStream()) {
+		if (key.startsWith(Core.actionKeyPrefix)) {
+			console.log(JSON.stringify({ type: "action", ...value }, null, args.compact ? null : 2))
+		} else if (key.startsWith(Core.sessionKeyPrefix)) {
+			console.log(JSON.stringify({ type: "session", ...value }, null, args.compact ? null : 2))
+		} else {
+			console.error(chalk.red("[canvas-cli] Skipping invalid entry"))
 		}
+		i++
 	}
 
-	// TODO: close the db connection
+	console.error(chalk.yellow(`Exported ${i} action${i === 1 ? "" : "s"}`))
+	await core.close()
+	process.exit(0)
 }
