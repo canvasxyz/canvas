3007b57b566bef45bd74cb4c4e3914f04dfb7042
Raymond Z <raykyri@gmail.com>
2022-10-13 13:46:05 -0400

    set up appstore ui inside example-chat-client

--- packages/example-chat-client/src/App.tsx
+++ packages/example-chat-client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useMemo, useCallback, useRef, useLayoutEffect, useEffect } from "react"
+import React, { MouseEventHandler, useState, useMemo, useCallback, useRef, useLayoutEffect, useEffect } from "react"
 import * as ReactDOM from "react-dom"
 
 import { useRoute, useCanvas } from "@canvas-js/hooks"
@@ -13,7 +13,77 @@ type Post = {
 	all_likes: string
 }
 
+export const ModularSelect: React.FC<{
+	name: string
+	category: string
+	onclick?: MouseEventHandler
+	disabled?: boolean
+	selected?: boolean
+}> = ({ name, category, onclick, disabled, selected }) => {
+	return (
+		<div
+			onClick={onclick}
+			className={`flex mb-8 cursor-pointer ${
+				disabled ? "pointer-events-none opacity-50" : "transform transition duration-200 hover:translate-y-0.5"
+			}`}
+		>
+			<div className="w-12">{selected && <div className="mt-4">&#9654;</div>}</div>
+			{/* icon */}
+			<div className="mr-4">
+				<div className="icon w-14 h-14 rounded-xl bg-gray-200 border border-white shadow-lg"></div>
+			</div>
+			{/* text */}
+			<div className="w-full leading-snug">
+				<div className={`text-lg mt-0.5 ${disabled ? "opacity-50" : ""}`}>{name}</div>
+				<div className="text-gray-400">{category}</div>
+			</div>
+		</div>
+	)
+}
+
 export const App: React.FC<{}> = ({}) => {
+	const [selectedHash, setSelectedHash] = useState("a")
+
+	return (
+		<div>
+			<div className="w-full p-4 border-b border-white text-center">Canvas</div>
+			<div className="p-10">
+				<div className="flex">
+					<div className="w-72 mr-10">
+						<ModularSelect
+							name="Flappy Bird"
+							category="Games"
+							onclick={() => setSelectedHash("a")}
+							selected={selectedHash === "a"}
+						/>
+						<ModularSelect
+							name="Gov House"
+							category="Tools"
+							onclick={() => setSelectedHash("b")}
+							selected={selectedHash === "b"}
+						/>
+						<ModularSelect name="Home" category="Tools" disabled />
+						<ModularSelect name="TinyRoam" category="Tools" disabled />
+					</div>
+					<div className="w-72">
+						{!selectedHash ? (
+							<div className="text-gray-400">Select an app</div>
+						) : (
+							<>
+								<div className="w-72 h-96 border border-gray-200 rounded-lg shadow-lg bg-white">
+									<Modular hash={selectedHash} />
+								</div>
+								<div className="m-1 mt-8 font-mono text-sm text-gray-400">Loading {selectedHash} ...</div>
+							</>
+						)}
+					</div>
+				</div>
+			</div>
+		</div>
+	)
+}
+
+export const Modular: React.FC<{ hash: string }> = ({ hash }) => {
 	const {
 		error: canvasError,
 		cid,
@@ -55,9 +125,10 @@ export const App: React.FC<{}> = ({}) => {
 		useMemo,
 		useRef,
 	}
-	return <AppChild routes={routes} actions={actions} hooks={hooks} fn={mod.fn} />
+
+	return <ModularChild routes={routes} actions={actions} hooks={hooks} fn={mod.fn} />
 }
 
-export const AppChild = ({ routes, actions, hooks, fn }: { routes: any; actions: any; hooks: any; fn: any }) => {
+export const ModularChild = ({ routes, actions, hooks, fn }: { routes: any; actions: any; hooks: any; fn: any }) => {
 	return fn({ React, actions, routes, ...hooks }, { props: {} })
 }
