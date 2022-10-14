aec7f72495b333eb62fce2f875f9970f4062c32b
Raymond Z <raykyri@gmail.com>
2022-10-13 21:57:33 -0400

    rename

--- flappy.jsx
+++ flappy.jsx
@@ -1,44 +1,20 @@
 export const models = {
-	posts: {
+	highscores: {
 		id: "string",
 		from_id: "string",
-		content: "string",
-		updated_at: "datetime",
-	},
-	likes: {
-		id: "string",
-		from_id: "string",
-		post_id: "string",
-		value: "boolean",
+		name: "string",
+		score: "integer",
 		updated_at: "datetime",
 	},
 }
 
 export const routes = {
-	"/posts": `
-    SELECT posts.*, COUNT(IIF(likes.value, 1, NULL)) as likes
-        FROM posts LEFT JOIN likes ON likes.post_id = posts.id
-        WHERE posts.updated_at <= :t
-        GROUP BY posts.id ORDER BY posts.updated_at DESC LIMIT 50`,
-	"/posts/as/:from_id": `
-	  SELECT posts.*, COUNT(likes.id) as likes, COUNT(my_likes.id) as my_likes,
-        group_concat(likes.from_id) as all_likes
-        FROM posts
-        LEFT JOIN likes ON likes.post_id = posts.id
-        LEFT JOIN likes my_likes ON my_likes.post_id = posts.id
-            AND my_likes.post_id = posts.id AND my_likes.from_id = :from_id
-        GROUP BY posts.id ORDER BY posts.updated_at DESC LIMIT 50`,
+	"/highscores": "SELECT highscores.* FROM highscores ORDER BY score DESC LIMIT 10",
 }
 
 export const actions = {
-	createPost(content) {
-		this.db.posts.set(this.hash, { content, from_id: this.from })
-	},
-	like(postId) {
-		this.db.likes.set(`${this.from}/${postId}`, { post_id: postId, from_id: this.from, value: true })
-	},
-	unlike(postId) {
-		this.db.likes.set(`${this.from}/${postId}`, { post_id: postId, from_id: this.from, value: false })
+	createScore(score) {
+		this.db.highscores.set(this.hash, { score, from_id: this.from })
 	},
 }
 
