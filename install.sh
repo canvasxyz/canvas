7260a7e03fe0b7824f732e1358065f70a77dfc59
Joel Gustafson <joelg@mit.edu>
2022-11-09 13:25:49 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -18,7 +18,7 @@ else
     touch ${INSTALL_PATH}
     echo "#!/usr/bin/env sh" >> ${INSTALL_PATH}
 fi
-echo "node ${PWD}/packages/canvas-cli/dist/index.js \$@" >> ${INSTALL_PATH}
+echo "node ${PWD}/packages/cli/dist/index.js \$@" >> ${INSTALL_PATH}
 chmod +x ${INSTALL_PATH}
 echo
 echo "Done!"
