1bb2dfd45c4ea9b56ae961f9d5be7ffef18d9e4d
Joel Gustafson <joelg@mit.edu>
2022-11-17 10:41:12 -0500

    bootstrap: extract env variables in one line

--- packages/bootstrap-peer/index.js
+++ packages/bootstrap-peer/index.js
@@ -10,14 +10,16 @@ import { createFromProtobuf } from "@libp2p/peer-id-factory"
 import { isLoopback } from "@libp2p/utils/multiaddr/is-loopback"
 import { isPrivate } from "@libp2p/utils/multiaddr/is-private"
 
-const bootstrapList = process.env.BOOTSTRAP_LIST.split(" ")
+const { PEER_ID, BOOTSTRAP_LIST, PORT, FLY_APP_NAME } = process.env
 
-const peerId = await createFromProtobuf(Buffer.from(process.env.PEER_ID, "base64"))
+const bootstrapList = BOOTSTRAP_LIST.split(" ")
+
+const peerId = await createFromProtobuf(Buffer.from(PEER_ID, "base64"))
 
 const RELAY_HOP_TIMEOUT = 0x7fffffff
 
 const address = await new Promise((resolve, reject) => {
-	dns.resolve(`${process.env.FLY_APP_NAME}.fly.dev`, (err, [address]) => {
+	dns.resolve(`${FLY_APP_NAME}.fly.dev`, (err, [address]) => {
 		if (err || address === undefined) {
 			reject(err)
 		} else {
@@ -26,8 +28,8 @@ const address = await new Promise((resolve, reject) => {
 	})
 })
 
-const listen = [`/ip4/0.0.0.0/tcp/${process.env.PORT}/ws`]
-const announce = [`/ip4/${address}/tcp/${process.env.PORT}/ws`]
+const listen = [`/ip4/0.0.0.0/tcp/${PORT}/ws`]
+const announce = [`/ip4/${address}/tcp/${PORT}/ws`]
 const announceFilter = (multiaddrs) => multiaddrs.filter((multiaddr) => !isLoopback(multiaddr) && !isPrivate(multiaddr))
 
 const libp2p = await createLibp2p({
