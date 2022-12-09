2937c0ec3abff16e391ec658f30cfe8e5f178bf8
Raymond Z <raykyri@gmail.com>
2022-12-09 14:59:27 -0500

    add reconnect code to client

--- packages/hooks/src/useWebsocket.ts
+++ packages/hooks/src/useWebsocket.ts
@@ -1,39 +1,44 @@
 import { useState, useEffect } from "react"
 
-const WS_KEEPALIVE = 30000
+const WS_KEEPALIVE = 3000
 
-const setupWebsocket = (host: string, onclose: () => void) => {
+type WebSocketExt = {
+	waitingForHeartbeat?: boolean
+	timer?: ReturnType<typeof setTimeout>
+}
+
+const setupWebsocket = (host: string, reconnect: () => void) => {
 	const wsHost = host.startsWith("/") ? `ws://${document.location.host}${host}` : host
-	const ws: WebSocket & { waitingForHeartbeat?: boolean } = new WebSocket(wsHost)
+	const ws: WebSocket & WebSocketExt = new WebSocket(wsHost)
 
 	// Set up keep-alive
-	ws.addEventListener("open", () => {
-		ws.waitingForHeartbeat = false
-		ws.addEventListener("pong", () => (ws.waitingForHeartbeat = false))
+	ws.addEventListener("message", (event) => {
+		if (event.data === "pong") ws.waitingForHeartbeat = false
 	})
-	const timer = setInterval(() => {
-		if (ws.readyState !== ws.OPEN) return
-		if (ws.waitingForHeartbeat === true) {
-			console.log("ws: server timed out")
-			ws.close()
-			clearInterval(timer)
-			onclose()
-		} else {
-			ws.waitingForHeartbeat = true
-			ws.send("ping")
-		}
-	}, WS_KEEPALIVE)
+	ws.addEventListener("open", (event) => {
+		ws.timer = setInterval(() => {
+			if (ws.readyState !== ws.OPEN) return
+			if (ws.waitingForHeartbeat === true) {
+				console.log("ws: closing connection, server did not respond to keep-alive")
+				ws.close()
+				clearInterval(ws.timer)
+				reconnect()
+			} else {
+				ws.waitingForHeartbeat = true
+				ws.send("ping")
+			}
+		}, WS_KEEPALIVE)
+	})
 
 	ws.addEventListener("close", () => {
 		console.log("ws: connection closed")
-		clearInterval(timer)
-		onclose()
+		clearInterval(ws.timer)
+		reconnect()
 	})
 
 	window.addEventListener("beforeunload", () => {
 		console.log("ws: window unloading")
 		ws.close()
-		onclose()
 	})
 
 	return ws
@@ -44,10 +49,9 @@ export function useWebsocket({ isLoading, host }: { isLoading: boolean; host: st
 
 	useEffect(() => {
 		if (isLoading) return
-		const ws = setupWebsocket(host, () => {
-			setWS(null)
-		})
-		setWS(ws)
+		// Set up a websocket, and re-connect whenever connection fails
+		const reconnect = () => setWS(setupWebsocket(host, reconnect))
+		setWS(setupWebsocket(host, reconnect))
 	}, [isLoading, host])
 
 	return ws
