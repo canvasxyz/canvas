26f78d69586c15031ace7c66aa42d5a8dc40803f
Bob Webb <bob.wbb@gmail.com>
2022-12-28 20:32:05 +0000

    move block provider to verifiers package

--- packages/verifiers/src/index.ts
+++ packages/verifiers/src/index.ts
@@ -1,3 +1,11 @@
+import { ethersBlockToCanvasBlock, EthereumBlockProvider } from "./ethereum/ethereum_block_provider.js"
 import { getActionSignatureData, getSessionSignatureData } from "./ethereum/verify_ethereum.js"
 import { verifyActionSignature, verifySessionSignature } from "./verify.js"
-export { getActionSignatureData, getSessionSignatureData, verifyActionSignature, verifySessionSignature }
+export {
+	getActionSignatureData,
+	getSessionSignatureData,
+	ethersBlockToCanvasBlock,
+	EthereumBlockProvider,
+	verifyActionSignature,
+	verifySessionSignature,
+}
