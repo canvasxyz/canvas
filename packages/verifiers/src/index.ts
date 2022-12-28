64443ca694f147fdd51ba7aa68acf608e900d13d
Bob Webb <bob.wbb@gmail.com>
2022-12-28 20:17:32 +0000

    make ethereum directory in verifiers

--- packages/verifiers/src/index.ts
+++ packages/verifiers/src/index.ts
@@ -1,3 +1,3 @@
-import { getActionSignatureData, getSessionSignatureData } from "./verify_ethereum.js"
+import { getActionSignatureData, getSessionSignatureData } from "./ethereum/verify_ethereum.js"
 import { verifyActionSignature, verifySessionSignature } from "./verify.js"
 export { getActionSignatureData, getSessionSignatureData, verifyActionSignature, verifySessionSignature }
