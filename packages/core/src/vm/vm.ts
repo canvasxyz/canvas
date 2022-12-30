fdaed70608c7e741ffc0dfd6a06958a21a2f2897
Joel Gustafson <joelg@mit.edu>
2022-12-30 09:22:42 -0500

    core: remove unused function

--- packages/core/src/vm/vm.ts
+++ packages/core/src/vm/vm.ts
@@ -509,17 +509,5 @@ function validate<T>(type: t.Type<T>, value: any, message?: string): T {
 	}
 }
 
-function parseFunctionParameters(source: string): string[] {
-	return source
-		.replace(/[/][/].*$/gm, "") // strip single-line comments
-		.replace(/\s+/g, "") // strip white space
-		.replace(/[/][*][^/*]*[*][/]/g, "") // strip multi-line comments
-		.split("){", 1)[0]
-		.replace(/^[^(]*[(]/, "") // extract the parameters
-		.replace(/=[^,]+/g, "") // strip any ES6 defaults
-		.split(",")
-		.filter(Boolean) // split & filter [""]
-}
-
 const assertPattern = (value: string, pattern: RegExp, message: string) =>
 	assert(pattern.test(value), `${message}: ${JSON.stringify(value)} does not match pattern ${pattern.source}`)
