4c67727294f68a124ec29ca3e6dee354a2fad5ec
Joel Gustafson <joelg@mit.edu>
2023-01-02 10:42:00 -0500

    core: test the contents of the message store in sources.test.ts

--- packages/core/test/sources.test.ts
+++ packages/core/test/sources.test.ts
@@ -1,9 +1,11 @@
 import test from "ava"
 import assert from "node:assert"
+import * as cbor from "microcbor"
 
-import { Core, ApplicationError, compileSpec } from "@canvas-js/core"
+import { Core, compileSpec } from "@canvas-js/core"
 
 import { TestSigner } from "./utils.js"
+import { fromHex, parseIPFSURI, toBuffer } from "@canvas-js/core/lib/utils.js"
 
 const MessageBoard = await compileSpec({
 	models: {
@@ -65,9 +67,9 @@ test("Apply source actions", async (t) => {
 	const createAction = await signer.sign("create", { content: "lorem ipsum" })
 	const { hash: createActionHash } = await core.applyAction(createAction)
 	const voteAction = await signer.sign("vote", { post_id: createActionHash, value: 1 })
-	const {} = await core.applyAction(voteAction)
+	const { hash: voteActionHash } = await core.applyAction(voteAction)
 	const voteSourceAction = await signer.sign("vote", { post_id: sourceActionHash, value: -1 })
-	const {} = await core.applyAction(voteSourceAction)
+	const { hash: voteSourceActionHash } = await core.applyAction(voteSourceAction)
 
 	t.deepEqual(core.modelStore.database.prepare("SELECT * FROM posts").all(), [
 		{
@@ -96,6 +98,68 @@ test("Apply source actions", async (t) => {
 			post_id: sourceActionHash,
 			value: -1,
 			updated_at: voteSourceAction.payload.timestamp,
+		},
+	])
+
+	const sourceCID = parseIPFSURI(MessageBoard.uri)
+	assert(sourceCID !== null)
+
+	t.deepEqual(core.messageStore.database.prepare("SELECT * FROM actions").all(), [
+		{
+			id: 1,
+			hash: fromHex(sourceActionHash),
+			signature: fromHex(sourceAction.signature),
+			from_address: fromHex(sourceSigner.wallet.address),
+			session_address: null,
+			timestamp: sourceAction.payload.timestamp,
+			call: sourceAction.payload.call,
+			args: toBuffer(cbor.encode(sourceAction.payload.args)),
+			chain: "eth",
+			chain_id: 1,
+			blockhash: null,
+			source: toBuffer(sourceCID.bytes),
+		},
+		{
+			id: 2,
+			hash: fromHex(createActionHash),
+			signature: fromHex(createAction.signature),
+			from_address: fromHex(signer.wallet.address),
+			session_address: null,
+			timestamp: createAction.payload.timestamp,
+			call: createAction.payload.call,
+			args: toBuffer(cbor.encode(createAction.payload.args)),
+			chain: "eth",
+			chain_id: 1,
+			blockhash: null,
+			source: null,
+		},
+		{
+			id: 3,
+			hash: fromHex(voteActionHash),
+			signature: fromHex(voteAction.signature),
+			from_address: fromHex(signer.wallet.address),
+			session_address: null,
+			timestamp: voteAction.payload.timestamp,
+			call: voteAction.payload.call,
+			args: toBuffer(cbor.encode(voteAction.payload.args)),
+			chain: "eth",
+			chain_id: 1,
+			blockhash: null,
+			source: null,
+		},
+		{
+			id: 4,
+			hash: fromHex(voteSourceActionHash),
+			signature: fromHex(voteSourceAction.signature),
+			from_address: fromHex(signer.wallet.address),
+			session_address: null,
+			timestamp: voteSourceAction.payload.timestamp,
+			call: voteSourceAction.payload.call,
+			args: toBuffer(cbor.encode(voteSourceAction.payload.args)),
+			chain: "eth",
+			chain_id: 1,
+			blockhash: null,
+			source: null,
 		},
 	])
 
