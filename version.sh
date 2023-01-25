88c1a9df53a5779d98a0ab5e0ee7fe5d37f1b194
Raymond Z <raykyri@gmail.com>
2023-01-24 19:18:09 -0500

    automatically run npm install in ./version.sh

--- version.sh
+++ version.sh
@@ -6,3 +6,5 @@ mv package.json-v$1 package.json
 
 for f in ./packages/*/package.json; do cat $f | jq "$SET_VERSION | $SET_DEPENDENCIES" > $f-v$1; mv $f-v$1 $f; done
 for f in ./examples/*/package.json; do cat $f | jq "$SET_VERSION | $SET_DEPENDENCIES" > $f-v$1; mv $f-v$1 $f; done
+
+npm install
