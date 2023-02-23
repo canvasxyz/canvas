10ed011859a7878dbae05ce22fe8eef6ec4d9eae
Bob Webb <bob.wbb@gmail.com>
2023-02-23 18:08:12 +0000

    remove postcss-loader

--- examples/notes/webpack.config.js
+++ examples/notes/webpack.config.js
@@ -21,7 +21,7 @@ module.exports = {
 			},
 			{
 				test: /\.css$/,
-				use: ["style-loader", "css-loader", "postcss-loader"],
+				use: ["style-loader", "css-loader"],
 			},
 			{
 				test: /\.svg$/,
