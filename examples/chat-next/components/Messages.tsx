14eff38c196c259f2aa6f0f89502041105a88389
Raymond Z <raykyri@gmail.com>
2023-02-24 22:55:15 -0500

    chat-next: rewrite infinite scroll using virtuoso, set intersection

--- examples/chat-next/components/Messages.tsx
+++ examples/chat-next/components/Messages.tsx
@@ -1,4 +1,6 @@
-import React, { useCallback, useEffect, useLayoutEffect, useRef } from "react"
+import _ from "lodash"
+import React, { useCallback, useEffect, useLayoutEffect, useRef, useState } from "react"
+import { Virtuoso } from "react-virtuoso"
 
 import { useEnsName } from "wagmi"
 import { Client, useRoute } from "@canvas-js/hooks"
@@ -11,25 +13,81 @@ type Post = {
 }
 
 export const MessagesInfiniteScroller: React.FC<{}> = ({}) => {
-	const { data, error } = useRoute<Post>("/posts", {})
+	const [posts, setPosts] = useState<Post[]>([])
+	const [cursor, setCursor] = useState<string>("")
 
-	const scrollContainer = useRef<HTMLDivElement>(null)
-	useLayoutEffect(() => {
-		if (scrollContainer.current !== null) {
-			scrollContainer.current.scrollTop = scrollContainer.current.scrollHeight
+	// Virtuoso uses firstItemIndex to maintain scroll position when
+	// items are added. It should always be set *relative to its
+	// original value* and cannot be negative, so we initialize it
+	// with a very large MAX_VALUE.
+	const MAX_VALUE = 999999999
+	const virtuoso = useRef(null)
+	const [firstItemIndex, setFirstItemIndex] = useState<number>(MAX_VALUE)
+
+	// Posts are fetched declaratively, by updating `cursor`.
+	const { data: fetched, error } = useRoute<Post>("/posts", { before: cursor })
+
+	// Because of pagination, /posts returns a window of posts ordered
+	// the same as displayed posts, but potentially overlapping or
+	// interleaved, so we use a set comparision to filter out duplicates.
+	//
+	// New posts may enter *anywhere* in the window, but we only maintain
+	// a subscription to the latest (before = "") or (before = cursor)
+	useEffect(() => {
+		if (!fetched) return
+
+		if (posts.length === 0) {
+			const filtered = [...fetched]
+			filtered.reverse()
+			setPosts(filtered)
+		} else {
+			const postsM = new Map(posts.map((f) => [f.id, f]))
+			const filtered = fetched.filter((item) => !postsM.has(item.id))
+			if (filtered.length === 0) return
+			setFirstItemIndex(firstItemIndex - filtered.length)
+			filtered.reverse()
+			// TODO: Interleave new posts according to updated_at, so if we
+			// receive new posts out-of-order (happens frequenty on first insert)
+			// then they won't persist out-of-order
+			setPosts([...filtered, ...posts])
 		}
-	}, [data])
+	}, [fetched, posts])
+
+	const startReached = useCallback(
+		(event: React.UIEvent<HTMLElement>) => {
+			console.log("startReached")
+			if (posts.length === 0) return
+
+			setTimeout(() => {
+				const earliestPost = posts[0]
+				const newCursor = earliestPost?.updated_at?.toString()
+				if (!earliestPost || cursor === earliestPost.updated_at) return // Nothing more
+				setCursor(earliestPost.updated_at)
+				console.log("cursor changed:", earliestPost.updated_at)
+			}, 500)
+		},
+		[posts, cursor]
+	)
+
+	const itemContent = useCallback((index, post) => <Post key={post.id} {...post} />, [])
+	const followOutput = useCallback((isAtBottom) => (isAtBottom ? "smooth" : false), [])
 
 	return (
-		<div id="scroll-container" ref={scrollContainer}>
-			<ul className="tree-view">
-				{data &&
-					data.map((_, i, posts) => {
-						const post = posts[posts.length - i - 1]
-						return <Post key={post.id} {...post} />
-					})}
-			</ul>
-		</div>
+		<ul className="tree-view">
+			{posts.length > 0 && (
+				<Virtuoso
+					ref={virtuoso}
+					firstItemIndex={firstItemIndex}
+					initialTopMostItemIndex={posts.length}
+					itemContent={itemContent}
+					data={posts}
+					startReached={startReached}
+					followOutput={followOutput}
+					style={{ flex: "1 1 auto", overscrollBehavior: "contain" }}
+					increaseViewportBy={{ bottom: 0, top: 40 }}
+				/>
+			)}
+		</ul>
 	)
 }
 
@@ -42,7 +100,6 @@ export const Messages: React.FC<{ client: Client | null }> = ({ client }) => {
 			if (event.key === "Enter" && input !== null && client !== null) {
 				try {
 					const { hash } = await client.createPost({ content: input.value })
-					console.log("created post", hash)
 					input.value = ""
 					setTimeout(() => input.focus(), 0)
 				} catch (err) {
