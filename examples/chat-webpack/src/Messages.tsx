af860ce1af3ac8ec9d3c0560b0d0b758d2e2f892
Raymond Zhong <raykyri@users.noreply.github.com>
2023-02-27 13:43:01 -0500

    Infinite scroll: Interleave posts that arrive out-of-order (#145)
    * port infinite scroll to chat-webpack
    
    * interleave posts that arrive out-of-order

--- examples/chat-webpack/src/Messages.tsx
+++ examples/chat-webpack/src/Messages.tsx
@@ -1,9 +1,9 @@
 import React, { useCallback, useEffect, useLayoutEffect, useRef, useState, useMemo, useContext } from "react"
+import { Virtuoso } from "react-virtuoso"
 import _ from "lodash"
 
 import { useEnsName } from "wagmi"
-import { useRoute } from "@canvas-js/hooks"
-
+import { Client, useRoute } from "@canvas-js/hooks"
 import { AppContext } from "./AppContext"
 
 type Post = {
@@ -11,6 +11,135 @@ type Post = {
 	from_id: `0x${string}`
 	content: string
 	updated_at: number
+}
+
+export const MessagesInfiniteScroller: React.FC<{}> = ({}) => {
+	const [posts, setPosts] = useState<Post[]>([])
+	const [cursor, setCursor] = useState<string | number>("")
+
+	// Virtuoso uses firstItemIndex to maintain scroll position when
+	// items are added. It should always be set *relative to its
+	// original value* and cannot be negative, so we initialize it
+	// with a very large MAX_VALUE.
+	const MAX_VALUE = 999999999
+	const virtuoso = useRef(null)
+	const [firstItemIndex, setFirstItemIndex] = useState<number>(MAX_VALUE)
+
+	// Past posts are fetched declaratively, by updating `cursor`.
+	// Because of pagination, the route returns a window of posts ordered
+	// monotonically the same as displayed posts, but potentially overlapping
+	// or interleaved with them, so we use a Map to filter out duplicates.
+	const { data: pastPosts } = useRoute<Post>("/posts", { before: cursor }, { subscribe: false })
+
+	// Maintain a subscription to the most recent page of posts.
+	// We assume that posts are received in-order, an assumption which
+	// may be violated when generating data.
+	const { data: newPosts } = useRoute<Post>("/posts", { before: "" })
+
+	useEffect(() => {
+		if (!pastPosts || !newPosts) return
+
+		if (posts.length === 0) {
+			const filteredNewPosts = [...newPosts]
+			filteredNewPosts.reverse()
+			setPosts(filteredNewPosts)
+		} else {
+			const postsM = new Map(posts.map((f) => [f.id, f]))
+			const filteredPastPosts = pastPosts.filter((item) => !postsM.has(item.id))
+			const filteredNewPosts = newPosts.filter((item) => !postsM.has(item.id))
+			if (filteredPastPosts.length === 0 && filteredNewPosts.length === 0) return
+			setFirstItemIndex(firstItemIndex - filteredPastPosts.length)
+			filteredPastPosts.reverse()
+			filteredNewPosts.reverse()
+
+			// Interleave new posts according to updated_at, so if we
+			// receive new posts out-of-order (happens frequently on batch insert)
+			// they won't persist out-of-order
+			let result
+			if (
+				// check if all posts are ordered
+				(filteredPastPosts.length === 0 && posts.length === 0) ||
+				(posts.length === 0 && filteredNewPosts.length === 0)
+			) {
+				result = [...filteredPastPosts, ...posts, ...filteredNewPosts]
+			} else {
+				// check if past + present posts are ordered
+				if (
+					filteredPastPosts.length === 0 ||
+					posts.length === 0 ||
+					filteredPastPosts[filteredPastPosts.length - 1].updated_at < posts[0].updated_at
+				) {
+					result = [...filteredPastPosts, ...posts]
+				} else {
+					result = _.sortedUniqBy(_.sortBy([...filteredPastPosts, ...posts], "updated_at"), "updated_at")
+				}
+				// check if present + new posts are ordered
+				if (
+					result.length === 0 ||
+					filteredNewPosts.length === 0 ||
+					result[result.length - 1].updated_at < filteredNewPosts[0].updated_at
+				) {
+					result = [...result, ...filteredNewPosts]
+				} else {
+					result = _.sortedUniqBy(_.sortBy([...result, ...filteredNewPosts], "updated_at"), "updated_at")
+				}
+			}
+			setPosts(result)
+
+			// Scroll-to-bottom doesn't seem to work correctly, Virtuoso incorrectly
+			// caps the maximum scroll to `scroller.offsetHeight - scroller.scrollHeight`
+			// when there might be additional not-yet-rendered content at the bottom.
+			if (filteredPastPosts.length === 0) {
+				const scroller = document.querySelector("[data-virtuoso-scroller=true]") as HTMLElement
+				if (scroller === null) return
+				// Only scroll-to-bottom if we're already near the bottom
+				if (scroller.scrollTop + scroller.offsetHeight < scroller.scrollHeight - 40) return
+				setTimeout(() => {
+					scroller.scrollTop = 99999999
+					setTimeout(() => {
+						scroller.scrollTop = 99999999
+					}, 10)
+				}, 10)
+			}
+		}
+	}, [newPosts, pastPosts, posts])
+
+	const startReached = useCallback(
+		(index: number) => {
+			if (posts.length === 0) return
+
+			setTimeout(() => {
+				const earliestPost = posts[0]
+				const newCursor = earliestPost?.updated_at?.toString()
+				if (!earliestPost || cursor === earliestPost.updated_at) return // Nothing more
+				setCursor(earliestPost.updated_at)
+				console.log("cursor changed:", earliestPost.updated_at)
+			}, 500)
+		},
+		[posts, cursor]
+	)
+
+	const itemContent = useCallback((index: number, post: Post) => <Post key={post.id} {...post} />, [])
+	// const followOutput = useCallback((isAtBottom) => (isAtBottom ? "auto" : false), [])
+
+	return (
+		<ul className="tree-view">
+			{posts.length > 0 && (
+				<Virtuoso
+					atBottomThreshold={40}
+					ref={virtuoso}
+					firstItemIndex={firstItemIndex}
+					initialTopMostItemIndex={posts.length}
+					itemContent={itemContent}
+					data={posts}
+					startReached={startReached}
+					// followOutput={followOutput}
+					style={{ flex: "1 1 auto", overscrollBehavior: "contain" }}
+					increaseViewportBy={{ bottom: 40, top: 40 }}
+				/>
+			)}
+		</ul>
+	)
 }
 
 export const Messages: React.FC = ({}) => {
@@ -44,107 +173,23 @@ export const Messages: React.FC = ({}) => {
 		}
 	}, [isReady])
 
-	const [cursor, setCursor] = useState("")
-	const [pages, setPages] = useState<Record<string, Post[]>>({})
-	const [messages, setMessages] = useState<Post[]>([])
-	const [latest, setLatest] = useState<Post[]>([]) // only used to trigger the LayoutEffect to scroll to bottom
-	const [trailing, setTrailing] = useState<Post[]>([]) // messages no longer in latest, but not in scrollback
-
-	// Subscribe to both the latest posts, and scrollback
-	const callbackLatest = useCallback(
-		(data: Post[] | null, error: Error | null) => {
-			if (!data) return
-			const hashes = new Set(data.map((d) => d.id))
-			setLatest(data)
-			setTrailing(latest.filter((d) => !hashes.has(d.id)).concat(trailing))
-		},
-		[trailing, setTrailing]
-	)
-	const callbackPrev = useCallback(
-		(data: Post[] | null, error: Error | null) => {
-			if (!data || cursor === "") return
-			setPages({ ...pages, [cursor]: data })
-		},
-		[cursor]
-	)
-
-	const { data: curr, error } = useRoute<Post>("/posts", { before: "" }, undefined, callbackLatest)
-	const { data: prev } = useRoute<Post>("/posts", { before: cursor }, undefined, callbackPrev)
-	useEffect(() => {
-		const scrollback = Object.keys(pages).reduce((acc: Post[], page) => acc.concat(pages[page]), [])
-		setMessages(latest.concat(trailing).concat(scrollback))
-	}, [latest, pages])
-
-	// Load more on scroll
-	const handleScroll = useMemo(
-		() =>
-			_.throttle((event: React.UIEvent<HTMLElement>) => {
-				if ((event?.target as HTMLElement)?.scrollTop > 50) return
-				if (messages.length > 0)
-					setTimeout(() => {
-						const earliestPost = messages[messages.length - 1]?.updated_at?.toString()
-						if (cursor === earliestPost) return // Nothing more to load
-						setCursor(earliestPost)
-					})
-			}, 750),
-		[messages, cursor]
-	)
-
-	// Jump to bottom on load, and when the current page updates, but not when messages updates
-	const scrollContainer = useRef<HTMLDivElement>(null)
-	useLayoutEffect(() => {
-		if (!curr?.length || !messages?.length) return
-		setTimeout(() => {
-			if (scrollContainer.current !== null) {
-				scrollContainer.current.scrollTop = scrollContainer.current.scrollHeight
-			}
-		})
-	}, [latest, curr?.length !== 0 && prev?.length !== 0 && messages.length !== 0])
-
 	return (
 		<div id="messages" className="window">
 			<div className="title-bar">
 				<div className="title-bar-text">Messages</div>
 			</div>
-			{error ? (
-				<div className="window-body">
-					<ul className="tree-view">
-						<li>{error.toString()}</li>
-					</ul>
-				</div>
-			) : (
-				<div className="window-body">
-					<div id="scroll-container" ref={scrollContainer} onScroll={handleScroll}>
-						<ul className="tree-view">
-							<Posts posts={messages} />
-						</ul>
-					</div>
-					<input
-						type="text"
-						disabled={!isReady}
-						ref={inputRef}
-						onKeyDown={handleKeyDown}
-						placeholder={isReady ? "" : "Start a session to chat"}
-					/>
-				</div>
-			)}
+			<div className="window-body">
+				<MessagesInfiniteScroller />
+				<input
+					type="text"
+					disabled={!isReady}
+					ref={inputRef}
+					onKeyDown={handleKeyDown}
+					placeholder={isReady ? "" : "Start a session to chat"}
+				/>
+			</div>
 		</div>
 	)
-}
-
-const Posts: React.FC<{ posts: null | Post[] }> = (props) => {
-	if (props.posts === null) {
-		return null
-	} else {
-		return (
-			<>
-				{props.posts.map((_, i, posts) => {
-					const post = posts[posts.length - i - 1]
-					return <Post key={post.id} {...post} />
-				})}
-			</>
-		)
-	}
 }
 
 const Post: React.FC<Post> = ({ from_id, content, updated_at }) => {
