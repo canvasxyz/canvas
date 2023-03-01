639780845acb75a7f04b8ec820bff9aad3242501
Raymond Z <raykyri@gmail.com>
2023-02-28 22:31:19 -0500

    add times

--- examples/chat-advanced/src/Messages.tsx
+++ examples/chat-advanced/src/Messages.tsx
@@ -1,6 +1,7 @@
 import React, { useCallback, useEffect, useLayoutEffect, useRef, useState, useMemo, useContext } from "react"
 import { Virtuoso } from "react-virtuoso"
 import _ from "lodash"
+import { DateTime } from "luxon"
 
 import { useEnsName } from "wagmi"
 import { Client, useRoute } from "@canvas-js/hooks"
@@ -198,11 +199,34 @@ const Post: React.FC<Post> = ({ from_id, content, updated_at }) => {
 	const address = `${from_id.slice(0, 5)}…${from_id.slice(-4)}`
 	// use wagmi's internal cache for ens names
 	const { data, isError, isLoading } = useEnsName({ address: from_id })
+	const [displayTime, setDisplayTime] = useState<string>()
+
+	useEffect(() => {
+		const now = DateTime.local()
+		const past = DateTime.fromMillis(updated_at)
+		const secs = now.diff(past, "seconds").seconds
+		if (secs < 60) {
+			setDisplayTime("just now")
+			return
+		}
+		const mins = now.diff(past, "minutes").minutes
+		if (mins < 60) {
+			setDisplayTime(`${Math.floor(mins)}min ago`)
+			return
+		}
+		const hrs = now.diff(past, "hours").hours
+		if (hrs < 10) {
+			setDisplayTime(`${Math.floor(hrs)}h ago`)
+			return
+		}
+		setDisplayTime(past.toString())
+	}, [updated_at])
 
 	return (
 		<li>
 			{data && <span className="address address-ens">[{data}]</span>}
 			<span className="address">{address} &gt;</span> {content}
+			<span className="time-ago">{displayTime}</span>
 		</li>
 	)
 }
