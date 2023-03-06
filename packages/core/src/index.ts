81f36fd21a99fb65bacbb8909ff22d338ab4c56a
Joel Gustafson <joelg@mit.edu>
2023-03-06 15:26:59 -0500

    core: add top-level validateSpec export

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,3 +2,6 @@ export * from "./core.js"
 export * from "./errors.js"
 export * from "./api.js"
 export * from "./websockets.js"
+
+import { VM } from "@canvas-js/core/components/vm"
+export const validateSpec = (spec: string) => VM.validate(spec)
