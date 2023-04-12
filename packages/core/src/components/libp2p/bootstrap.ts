297e187d52b6792eb3aa79ce534292126ff0a2b7
Joel Gustafson <joelg@mit.edu>
2023-04-12 18:21:46 -0400

    core: uncomment all bootstrap servers

--- packages/core/src/components/libp2p/bootstrap.ts
+++ packages/core/src/components/libp2p/bootstrap.ts
@@ -1,5 +1,5 @@
 export const defaultBootstrapList = [
-	// "/dns4/canvas-bootstrap-p0.fly.dev/tcp/4003/wss/p2p/12D3KooWP4DLJuVUKoThfzYugv8c326MuM2Tx38ybvEyDjLQkE2o",
-	// "/dns4/canvas-bootstrap-p1.fly.dev/tcp/4003/wss/p2p/12D3KooWRftkCBMtYou4pM3VKdqkKVDAsWXnc8NabUNzx7gp7cPT",
+	"/dns4/canvas-bootstrap-p0.fly.dev/tcp/4003/wss/p2p/12D3KooWP4DLJuVUKoThfzYugv8c326MuM2Tx38ybvEyDjLQkE2o",
+	"/dns4/canvas-bootstrap-p1.fly.dev/tcp/4003/wss/p2p/12D3KooWRftkCBMtYou4pM3VKdqkKVDAsWXnc8NabUNzx7gp7cPT",
 	"/dns4/canvas-bootstrap-p2.fly.dev/tcp/4003/wss/p2p/12D3KooWPopNdRnzswSd8oVxrUBKGhgKzkYALETK7EHkToy7DKk3",
 ]
