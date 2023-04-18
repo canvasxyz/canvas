2951d3bbc8e9ce25245a25393153836077f3b957
Bob Webb <bob.wbb@gmail.com>
2023-04-18 17:00:24 +0100

    make opts optional

--- packages/core/src/components/modelStore/types.ts
+++ packages/core/src/components/modelStore/types.ts
@@ -8,5 +8,5 @@ export interface ModelStore {
 	close(): Promise<void>
 	applyEffects(context: { timestamp: number }, effects: Effect[]): Promise<void>
 	getRoute(route: string, params?: Record<string, string>): Promise<Record<string, ModelValue>[]>
-	exportModel(modelName: string, options: { offset: number; limit: number }): AsyncIterable<Record<string, ModelValue>>
+	exportModel(modelName: string, options?: { offset: number; limit: number }): AsyncIterable<Record<string, ModelValue>>
 }
