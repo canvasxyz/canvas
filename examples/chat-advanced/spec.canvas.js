e0e597360dce57441b1b9fccb668478c624b0eca
Raymond Z <raykyri@gmail.com>
2023-04-20 16:43:53 -0700

    update chat-advanced spec

--- examples/chat-advanced/spec.canvas.js
+++ examples/chat-advanced/spec.canvas.js
@@ -1,36 +1,33 @@
-const CanvasChat = "ipfs://QmYAV9u6a3aHyhbPExMQauivZmrfyDdPH19yPZAZL6iF2e";
+const CanvasChat = "ipfs://QmShbAFGESPX8MgvJt39Gzdm2KcQzvysdHW8vgVpRKStL9"
 
-export const chains = ["eip155:1"];
+export const chains = ["eip155:1"]
 
 export const models = {
-  posts: {
-    id: "string",
-    content: "string",
-    from_id: "string",
-    updated_at: "datetime",
-    indexes: ["updated_at"],
-  },
-};
+	posts: {
+		id: "string",
+		content: "string",
+		from_id: "string",
+		updated_at: "datetime",
+		indexes: ["updated_at"],
+	},
+}
 
 export const routes = {
-  "/posts": ({ before = "" }, { db }) => {
-    return db.queryRaw(
-      `SELECT * FROM posts WHERE updated_at < :before ORDER BY updated_at DESC LIMIT 50`,
-      { before },
-    );
-  },
-};
+	"/posts": ({ before = "" }, { db }) => {
+		return db.queryRaw(`SELECT * FROM posts WHERE updated_at < :before ORDER BY updated_at DESC LIMIT 50`, { before })
+	},
+}
 
 export const actions = {
-  createPost({ content }, { db, hash, from }) {
-    db.posts.set(hash, { content, from_id: from });
-  },
-};
+	createPost({ content }, { db, hash, from }) {
+		db.posts.set(hash, { content, from_id: from })
+	},
+}
 
 export const sources = {
-  [CanvasChat]: {
-    createPost({ content }, { db, hash, from }) {
-      db.posts.set(hash, { content, from_id: from, imported: true })
-    },
-  },
-};
+	[CanvasChat]: {
+		createPost({ content }, { db, hash, from }) {
+			db.posts.set(hash, { content, from_id: from, imported: true })
+		},
+	},
+}
