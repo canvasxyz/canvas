1ce679b154c109c22e680e4b757f80dc3f626de3
Joel Gustafson <joelg@mit.edu>
2023-04-20 23:23:55 -0400

    core: testnet bootstrap multiaddrs are /dns6 not /dns4

--- packages/core/src/bootstrap.ts
+++ packages/core/src/bootstrap.ts
@@ -5,7 +5,7 @@ export const defaultBootstrapList = [
 ]
 
 export const testnetBootstrapList = [
-	"/dns4/canvas-testnet-p0.internal/tcp/8080/ws/p2p/12D3KooWFgSKAMeskSYawUoTu41PYaFEENmzB6evXJfceW5xLPRW",
-	"/dns4/canvas-testnet-p1.internal/tcp/8080/ws/p2p/12D3KooWM7JabMS95FXoBSYpVBuRFXx3NbCnBhPACG2SuSyuJQwn",
-	"/dns4/canvas-testnet-p2.internal/tcp/8080/ws/p2p/12D3KooWSTEPj46WriuPpGeEEaGWLo95Wkeu4pZxe3QwDiAATp1o",
+	"/dns6/canvas-testnet-p0.internal/tcp/8080/ws/p2p/12D3KooWFgSKAMeskSYawUoTu41PYaFEENmzB6evXJfceW5xLPRW",
+	"/dns6/canvas-testnet-p1.internal/tcp/8080/ws/p2p/12D3KooWM7JabMS95FXoBSYpVBuRFXx3NbCnBhPACG2SuSyuJQwn",
+	"/dns6/canvas-testnet-p2.internal/tcp/8080/ws/p2p/12D3KooWSTEPj46WriuPpGeEEaGWLo95Wkeu4pZxe3QwDiAATp1o",
 ]
