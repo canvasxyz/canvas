e44aad3d4fbb52da09d9fc34713601a764d37df7
Joel Gustafson <joelg@mit.edu>
2023-04-20 20:11:52 -0400

    core: add testnetBootstrapList

--- packages/core/src/components/libp2p/bootstrap.ts
+++ packages/core/src/components/libp2p/bootstrap.ts
@@ -3,3 +3,9 @@ export const defaultBootstrapList = [
 	"/dns4/canvas-bootstrap-p1.fly.dev/tcp/4003/wss/p2p/12D3KooWRftkCBMtYou4pM3VKdqkKVDAsWXnc8NabUNzx7gp7cPT",
 	"/dns4/canvas-bootstrap-p2.fly.dev/tcp/4003/wss/p2p/12D3KooWPopNdRnzswSd8oVxrUBKGhgKzkYALETK7EHkToy7DKk3",
 ]
+
+export const testnetBootstrapList = [
+	"/dns4/canvas-testnet-p0.internal/tcp/8080/ws/p2p/12D3KooWFgSKAMeskSYawUoTu41PYaFEENmzB6evXJfceW5xLPRW",
+	"/dns4/canvas-testnet-p1.internal/tcp/8080/ws/p2p/12D3KooWM7JabMS95FXoBSYpVBuRFXx3NbCnBhPACG2SuSyuJQwn",
+	"/dns4/canvas-testnet-p2.internal/tcp/8080/ws/p2p/12D3KooWSTEPj46WriuPpGeEEaGWLo95Wkeu4pZxe3QwDiAATp1o",
+]
