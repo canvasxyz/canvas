ba656fb07bd654774d52b91c77fe5998b585adf3
Raymond Z <raykyri@gmail.com>
2023-05-08 17:51:23 -0400

    fix test

--- packages/core/test/chains.test.ts
+++ packages/core/test/chains.test.ts
@@ -44,29 +44,16 @@ test("missing signer declaration for provided chain implementation", async (t) =
 	})
 
 	const [mainnet, gnosis] = [new EthereumChainImplementation(1), new EthereumChainImplementation(0x64)]
-	const core = await Core.initialize({
-		chains: [mainnet, gnosis],
-		spec,
-		directory: null,
-		offline: true,
-	})
-
-	t.teardown(() => core.close())
-
-	const mainnetSigner = new TestSigner(core.app, mainnet)
-	const gnosisSigner = new TestSigner(core.app, gnosis)
-
-	await t.notThrowsAsync(async () => {
-		const action = await mainnetSigner.sign("foo", {})
-		await core.apply(action)
-	})
-
 	await t.throwsAsync(
 		async () => {
-			const action = await gnosisSigner.sign("foo", {})
-			await core.apply(action)
+			const core = await Core.initialize({
+				chains: [mainnet, gnosis],
+				spec,
+				directory: null,
+				offline: true,
+			})
 		},
-		{ message: "unsupported action signer (eip155:100)" }
+		{ message: "ipfs://QmPrP7ZVTKSkoNtpfr24XH1UNqSugEDNSqY9BGV7hE8SF9 contract didn't declare a signer for eip155:100" }
 	)
 })
 
