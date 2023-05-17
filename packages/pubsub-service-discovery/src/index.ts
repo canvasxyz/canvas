39617cc72f9852f82930728366d6147f8079e09a
Joel Gustafson <joelg@mit.edu>
2023-05-17 10:05:14 -0400

    move pubsub-service-discovery into own repo

--- packages/pubsub-service-discovery/src/index.ts
+++
@@ -1,7 +0,0 @@
-export { ServiceRecordCache, MemoryCache, MemoryCacheInit } from "./cache.js"
-export {
-	pubsubServiceDiscovery,
-	ServiceDiscovery,
-	ServiceDiscoveryInit,
-	ServiceDiscoveryComponents,
-} from "./service.js"
