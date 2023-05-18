2f87b5d4936de3ce014827dddc0982f27f5c6c00
Bob Webb <bob.wbb@gmail.com>
2023-05-18 16:21:59 +0100

    move message list into messages panel component, add scroll behaviour

--- examples/interwallet/src/views/MessagesPanel.tsx
+++ examples/interwallet/src/views/MessagesPanel.tsx
@@ -1,6 +1,5 @@
 import React, { useEffect, useState } from "react"
 import { db } from "../models/db"
-import { MessageList } from "./MessageList"
 import { useLiveQuery } from "dexie-react-hooks"
 
 const toRoomKey = (address1: string, address2: string) => {
@@ -14,9 +13,36 @@ export const MessagesPanel = ({ address, currentUserAddress }: { address: string
 	const messageEvents =
 		useLiveQuery(async () => await db.messageEvents.where({ room_id: roomKey }).sortBy("timestamp"), [roomKey]) || []
 
+	const messagesEndRef = React.useRef<HTMLDivElement>(null)
+
+	useEffect(() => {
+		messagesEndRef.current?.scrollIntoView({ behavior: "auto" })
+	}, [messageEvents])
+
 	return (
 		<>
-			<MessageList messages={messageEvents} />
+			<div className="flex flex-col grow ml-3 mr-3 gap-3 overflow-y-auto">
+				{messageEvents.map((message, index) => {
+					const is_sent = message.sender == "1"
+					return (
+						<div key={index}>
+							<div className="flex justify-center text-gray-300">{message.timestamp}</div>
+							<div className={`flex ${is_sent ? "flex-row" : "flex-row-reverse"}`}>
+								<div
+									className={
+										is_sent
+											? "p-3 rounded-r-lg rounded-tl-lg bg-blue-500 text-white"
+											: "p-3 rounded-l-lg rounded-tr-lg bg-gray-200 text-black"
+									}
+								>
+									{message.message}
+								</div>
+							</div>
+						</div>
+					)
+				})}
+				<div ref={messagesEndRef} />
+			</div>
 			{/* <div > */}
 			<form
 				className="m-3 flex flex-row"
@@ -40,7 +66,6 @@ export const MessagesPanel = ({ address, currentUserAddress }: { address: string
 					className="h-10 w-full rounded-xl bg-gray-100 focus:outline-none pl-2"
 				></input>
 			</form>
-			{/* </div> */}
 		</>
 	)
 }
