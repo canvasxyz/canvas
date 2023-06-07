cae95fa5c39f7ccd09126e6e8ba43d17c1f2d323
Bob Webb <bob.wbb@gmail.com>
2023-06-07 17:06:17 +0200

    RoomName can support arbitrary number of other members

--- interwallet/src/client/views/RoomName.tsx
+++ interwallet/src/client/views/RoomName.tsx
@@ -10,27 +10,39 @@ export interface RoomNameProps {
 	room: Room
 }
 
-export const RoomName: React.FC<RoomNameProps> = ({ room }) => {
-	if (room.members.length !== 2) {
-		throw new Error("rooms must have exactly two members")
-	}
-
-	const { user } = useContext(AppContext)
-
-	const recipient = useMemo(
-		() => user && room.members.find(({ address }) => getAddress(address) !== user.address),
-		[room, user]
-	)
-	const { data: name } = useEnsName({ address: recipient?.address ?? undefined })
+const EnsName = ({ address }: { address: string }) => {
+	const { data: name } = useEnsName({ address: address as `0x${string}` })
 
 	if (name) {
 		return <span>{name}</span>
-	} else if (recipient) {
-		const head = recipient.address.slice(0, 6)
-		const tail = recipient.address.slice(-4)
+	} else {
+		const head = address.slice(0, 6)
+		const tail = address.slice(-4)
 		return (
 			<span>
 				{head}…{tail}
+			</span>
+		)
+	}
+}
+
+export const RoomName: React.FC<RoomNameProps> = ({ room }) => {
+	const { user } = useContext(AppContext)
+
+	const otherRoomMembers = useMemo(
+		() => user && room.members.filter(({ address }) => getAddress(address) !== user.address),
+		[room, user]
+	)
+
+	if (otherRoomMembers) {
+		return (
+			<span>
+				{otherRoomMembers.map((member, index) => (
+					<React.Fragment key={member.address}>
+						<EnsName address={member.address} />
+						{index < otherRoomMembers.length - 1 && ", "}
+					</React.Fragment>
+				))}
 			</span>
 		)
 	} else {
