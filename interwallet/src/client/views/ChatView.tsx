dd2cb57ec10bc85afaf45e02a82b7a69030d5c06
Bob Webb <bob.wbb@gmail.com>
2023-06-16 14:43:41 +0100

    add room when page reloads

--- interwallet/src/client/views/ChatView.tsx
+++ interwallet/src/client/views/ChatView.tsx
@@ -79,59 +79,7 @@ export const ChatView = ({
 			// if the current user is a member of the room
 			if (room.members.find(({ address }) => address === user.address)) {
 				await db.rooms.add(room)
-				// await this.addRoom(room)
-
-				const topic = `interwallet:room:${room.id}`
-				console.log("registering for room topic", topic)
-				await register(topic, async (key: Uint8Array, value: Uint8Array) => {
-					const { encryptedEvent } = validateEvent(room, key, value)
-					console.log("message event", encryptedEvent)
-
-					let decryptedEvent: Uint8Array | null
-					if (equals(encryptedEvent.userAddress, hexToBytes(user.address))) {
-						// this user is the sender
-						// decrypt an arbitrary message, so choose the first one
-						const encryptedMessage = encryptedEvent.recipients[0]
-						decryptedEvent = nacl.box.open(
-							encryptedMessage.ciphertext,
-							encryptedMessage.nonce,
-							encryptedMessage.publicKey,
-							hexToBytes(user.encryptionPrivateKey)
-						)
-					} else {
-						// otherwise this user is one of the recipients
-						const myEncryptedMessage = encryptedEvent.recipients.find(({ publicKey }) =>
-							equals(publicKey, hexToBytes(user.keyBundle.encryptionPublicKey))
-						)
-						assert(myEncryptedMessage !== undefined, "failed to find encrypted message for this user")
-
-						const sender = await db.users.get({ address: getAddress(bytesToHex(encryptedEvent.userAddress)) })
-						assert(sender !== undefined, "failed to find sender")
-
-						decryptedEvent = nacl.box.open(
-							myEncryptedMessage.ciphertext,
-							myEncryptedMessage.nonce,
-							// sender's public key
-							hexToBytes(sender.keyBundle.encryptionPublicKey),
-							hexToBytes(user.encryptionPrivateKey)
-						)
-					}
-					assert(decryptedEvent !== null, "failed to decrypt room event")
-
-					const event = decode(decryptedEvent) as RoomEvent
-
-					// TODO: runtime validation of room event types
-					if (event.type === "message") {
-						try {
-							const id = await db.messages.add({ room: room.id, ...event.detail })
-							console.log("added message with id", id)
-						} catch (e) {
-							console.log(e)
-						}
-					} else {
-						throw new Error("invalid event type")
-					}
-				})
+				addRoom(room)
 			}
 		})
 
@@ -174,6 +122,16 @@ export const ChatView = ({
 		// send user registration
 	}, [stores[USER_REGISTRY_TOPIC]])
 
+	useEffect(() => {
+		if (!db || !stores[ROOM_REGISTRY_TOPIC]) return
+
+		db.rooms.toArray().then((rooms) => {
+			for (const room of rooms) {
+				addRoom(room)
+			}
+		})
+	}, [db, stores[ROOM_REGISTRY_TOPIC]])
+
 	const createRoom: (members: PublicUserRegistration[]) => Promise<void> = async (members) => {
 		const roomRegistry = stores[ROOM_REGISTRY_TOPIC]
 		if (!roomRegistry) return
@@ -208,6 +166,65 @@ export const ChatView = ({
 
 		const roomId = base58btc.baseEncode(key)
 		db.rooms.add({ id: roomId, creator: user.address, members })
+	}
+
+	const addRoom = async (room: Room) => {
+		if (!db) {
+			console.log("no db, tried to add room", room)
+			return
+		}
+
+		const topic = `interwallet:room:${room.id}`
+		console.log("registering for room topic", topic)
+		await register(topic, async (key: Uint8Array, value: Uint8Array) => {
+			const { encryptedEvent } = validateEvent(room, key, value)
+			console.log("message event", encryptedEvent)
+
+			let decryptedEvent: Uint8Array | null
+			if (equals(encryptedEvent.userAddress, hexToBytes(user.address))) {
+				// this user is the sender
+				// decrypt an arbitrary message, so choose the first one
+				const encryptedMessage = encryptedEvent.recipients[0]
+				decryptedEvent = nacl.box.open(
+					encryptedMessage.ciphertext,
+					encryptedMessage.nonce,
+					encryptedMessage.publicKey,
+					hexToBytes(user.encryptionPrivateKey)
+				)
+			} else {
+				// otherwise this user is one of the recipients
+				const myEncryptedMessage = encryptedEvent.recipients.find(({ publicKey }) =>
+					equals(publicKey, hexToBytes(user.keyBundle.encryptionPublicKey))
+				)
+				assert(myEncryptedMessage !== undefined, "failed to find encrypted message for this user")
+
+				const sender = await db.users.get({ address: getAddress(bytesToHex(encryptedEvent.userAddress)) })
+				assert(sender !== undefined, "failed to find sender")
+
+				decryptedEvent = nacl.box.open(
+					myEncryptedMessage.ciphertext,
+					myEncryptedMessage.nonce,
+					// sender's public key
+					hexToBytes(sender.keyBundle.encryptionPublicKey),
+					hexToBytes(user.encryptionPrivateKey)
+				)
+			}
+			assert(decryptedEvent !== null, "failed to decrypt room event")
+
+			const event = decode(decryptedEvent) as RoomEvent
+
+			// TODO: runtime validation of room event types
+			if (event.type === "message") {
+				try {
+					const id = await db.messages.add({ room: room.id, ...event.detail })
+					console.log("added message with id", id)
+				} catch (e) {
+					console.log(e)
+				}
+			} else {
+				throw new Error("invalid event type")
+			}
+		})
 	}
 
 	const sendMessage = async (roomId: string, message: string) => {
