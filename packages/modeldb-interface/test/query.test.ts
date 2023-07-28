289ff1b8b84142644503616ba3995622f2ef3a71
Bob Webb <bob.wbb@gmail.com>
2023-07-28 15:36:07 +0100

    test for selecting multiple fields

--- packages/modeldb-interface/test/query.test.ts
+++ packages/modeldb-interface/test/query.test.ts
@@ -1,15 +1,16 @@
 import { ModelsInit } from "../src/types.js"
 import { testOnModelDB } from "./utils.js"
 
+// @ts-ignore
+const models = {
+	user: {
+		name: "string",
+		age: "integer",
+		$type: "immutable",
+	},
+} as ModelsInit
+
 testOnModelDB("query the database using select", async (t, modelDBConstructor) => {
-	// @ts-ignore
-	const models = {
-		user: {
-			name: "string",
-			age: "integer",
-			$type: "immutable",
-		},
-	} as ModelsInit
 	const db = await modelDBConstructor(models)
 
 	await db.add("user", { name: "test", age: 10 })
@@ -21,6 +22,24 @@ testOnModelDB("query the database using select", async (t, modelDBConstructor) =
 		[
 			{
 				name: "test",
+			},
+		]
+	)
+})
+
+testOnModelDB("query the database using select on multiple fields", async (t, modelDBConstructor) => {
+	const db = await modelDBConstructor(models)
+
+	await db.add("user", { name: "test", age: 10 })
+
+	t.deepEqual(
+		await db.query("user", {
+			select: { name: true, age: true },
+		}),
+		[
+			{
+				name: "test",
+				age: 10,
 			},
 		]
 	)
