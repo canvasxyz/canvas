c8dbb4548b62e242e049c164b3bc75af166eb486
Joel Gustafson <joelg@mit.edu>
2023-09-28 14:08:21 -0400

    Format .eslintrc.cjs

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,57 +1,57 @@
 module.exports = {
-	extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
-	plugins: ["@typescript-eslint", "import"],
-	parser: "@typescript-eslint/parser",
-	root: true,
-	env: {
-		browser: true,
-		node: true,
-	},
-	ignorePatterns: [
-		"webpack.config.js",
-		"*.d.ts",
-		"*.cts",
-		"**/dist/**/*.js",
-		"**/lib/**/*.js",
-		"**/out/**/*.js",
-		"packages/core/sqlite/*",
-	],
-	settings: {
-		"import/parsers": {
-			"@typescript-eslint/parser": [".ts", ".tsx"],
-		},
-		"import/resolver": {
-			node: {},
-			typescript: {
-				project: ["./tsconfig.json", "./packages/*/tsconfig.json", "./packages/*/tsconfig.test.json"],
-			},
-		},
-	},
-	rules: {
-		"@typescript-eslint/no-explicit-any": "off",
-		"@typescript-eslint/no-empty-function": "off",
-		"@typescript-eslint/no-unused-vars": "off",
-		"@typescript-eslint/no-non-null-assertion": "off",
-		"@typescript-eslint/no-empty-interface": "off",
-		"@typescript-eslint/ban-ts-comment": "off",
-		"no-empty-pattern": "off",
-		"require-yield": "off",
-		"@typescript-eslint/ban-types": [
-			"error",
-			{
-				extendDefaults: true,
-				types: { "{}": false },
-			},
-		],
-	},
-	overrides: [
-		{
-			files: ["*.canvas.js"],
-			rules: {
-				// this rule is disabled because canvas contract have global values
-				// injected into the vm, such as `ethersComputeAddress`
-				"no-undef": "off",
-			},
-		},
-	],
-}
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+  plugins: ["@typescript-eslint", "import"],
+  parser: "@typescript-eslint/parser",
+  root: true,
+  env: {
+    browser: true,
+    node: true,
+  },
+  ignorePatterns: [
+    "webpack.config.js",
+    "*.d.ts",
+    "**/dist/**/*.js",
+    "**/lib/**/*.js",
+  ],
+  settings: {
+    "import/parsers": {
+      "@typescript-eslint/parser": [".ts", ".tsx"],
+    },
+    "import/resolver": {
+      node: {},
+      typescript: {
+        project: [
+          "./tsconfig.json",
+          "./packages/*/tsconfig.json",
+        ],
+      },
+    },
+  },
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "@typescript-eslint/no-unused-vars": "off",
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/no-empty-interface": "off",
+    "@typescript-eslint/ban-ts-comment": "off",
+    "no-empty-pattern": "off",
+    "require-yield": "off",
+    "@typescript-eslint/ban-types": [
+      "error",
+      {
+        extendDefaults: true,
+        types: { "{}": false },
+      },
+    ],
+  },
+  overrides: [
+    {
+      files: ["*.canvas.js"],
+      rules: {
+        // this rule is disabled because canvas contract have global values
+        // injected into the vm, such as `ethersComputeAddress`
+        "no-undef": "off",
+      },
+    },
+  ],
+};
