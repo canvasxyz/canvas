0e1b0b789877f0a2153f2f33de0c3d82d5df9e8f
Joel Gustafson <joelg@mit.edu>
2023-10-13 09:42:54 -0400

    packages/vm: remove std archive

--- packages/vm/src/std/import.browser.ts
+++
@@ -1,5 +0,0 @@
-import type { ImportOptions } from "./import.js"
-
-export function getModuleArchive(options: ImportOptions): AsyncIterable<Uint8Array> {
-	throw new Error("Unsupported platform")
-}
