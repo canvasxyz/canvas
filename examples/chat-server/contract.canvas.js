675eb7219256fe2a9eb44ab7c609084919a8906c
Joel Gustafson <joelg@mit.edu>
2023-10-31 18:16:55 -0400

    examples/chat-server: add chat-server

---
+++ examples/chat-server/contract.canvas.js
@@ -0,0 +1,18 @@
+export const topic = "example-chat.canvas.xyz";
+
+export const models = {
+  message: {
+    id: "primary",
+    user: "string",
+    content: "string",
+    timestamp: "integer",
+    $indexes: ["user", "timestamp"],
+  },
+};
+
+export const actions = {
+  async createMessage(db, { content }, { id, chain, address, timestamp }) {
+    const user = `${chain}:${address}`;
+    await db.message.set({ id, user, content, timestamp });
+  },
+};
