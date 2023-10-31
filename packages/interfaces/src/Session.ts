bbebafbf1180c6a981d4a97117df9bca70be6826
Joel Gustafson <joelg@mit.edu>
2023-10-31 09:57:22 -0400

    packages/interfaces: add jsdoc to Session type

--- packages/interfaces/src/Session.ts
+++ packages/interfaces/src/Session.ts
@@ -1,5 +1,10 @@
 import type { SignatureType } from "./Signature.js"
 
+/**
+ * Sessions consist of an ephemeral keypair and some chain-specific
+ * data representing a user's (temporary) authorization of that
+ * keypair to sign actions on their behalf.
+ */
 export type Session<Data = any> = {
 	type: "session"
 
