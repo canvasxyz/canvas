73023dcdc8c12eacad7e236a69d99c72985d319f
Joel Gustafson <joelg@mit.edu>
2023-10-31 10:00:37 -0400

    packages/interfaces: add jsdoc comments to SessionSigner methods

--- packages/interfaces/src/SessionSigner.ts
+++ packages/interfaces/src/SessionSigner.ts
@@ -7,11 +7,18 @@ export interface SessionSigner extends MessageSigner<Action | Session> {
 	match: (chain: string) => boolean
 
 	/**
-	 * Produce an signed Session object, which authorizes `session.publicKey`
-	 * to represent the user `${session.chain}:${session.address}`.
+	 * `getSession` is called by the Canvas runtime for every new action appended
+	 * to the log (ie for new actions taken by local users, not existing messages
+	 * received from other peers via merkle sync or GossipSub).
 	 *
-	 * The signature is stored in `session.data`, and the entire Session
-	 * object is then signed using the session-key, and appended to our message log.
+	 * It's responsible for returning a `Session` that matches the given parameters,
+	 * either by looking up a cached session, or by getting user authorization to create
+	 * a new one (and then caching it).
+	 *
+	 * "Matching the given parameters" means that the caller passes a `topic: string`
+	 * and an optional `chain?: string; timestamp?: number`, and `getSession` must return
+	 * a `Session` authorized for that topic, that specific chain (if provided), and that
+	 * is valid for the given timestamp (if provided).
 	 */
 	getSession: (topic: string, options?: { chain?: string; timestamp?: number }) => Awaitable<Session>
 
