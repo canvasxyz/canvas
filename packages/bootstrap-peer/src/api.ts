0b84e359376b6deed05033aa324470d4f8926bc6
Joel Gustafson <joelg@mit.edu>
2023-10-31 13:24:09 -0400

    packages/bootstrap-peer: import bootstrap peer package from external repo

---
+++ packages/bootstrap-peer/src/api.ts
@@ -0,0 +1,84 @@
+import http from "node:http"
+
+import express from "express"
+import { StatusCodes } from "http-status-codes"
+import { AbortError } from "abortable-iterator"
+
+import type { Libp2p } from "libp2p"
+import type { PeerId } from "@libp2p/interface/peer-id"
+import { peerIdFromString } from "@libp2p/peer-id"
+import { anySignal } from "any-signal"
+
+import client from "prom-client"
+
+import { PING_TIMEOUT } from "./constants.js"
+import { ServiceMap } from "./libp2p.js"
+
+export function getAPI(libp2p: Libp2p<ServiceMap>) {
+	const api = express()
+	api.set("query parser", "simple")
+	api.use(express.json())
+
+	api.get("/", async (req, res) => res.json({}))
+	api.get("/metrics", async (req, res) => {
+		try {
+			const result = await client.register.metrics()
+			res.status(StatusCodes.OK)
+			res.contentType(client.register.contentType)
+			res.end(result)
+		} catch (err: any) {
+			console.error(err)
+			res.status(StatusCodes.INTERNAL_SERVER_ERROR).end()
+		}
+	})
+
+	api.get("/connections", (req, res) => {
+		const result: Record<string, { peer: string; addr: string; streams: Record<string, string | null> }> = {}
+
+		for (const { id, remotePeer, remoteAddr, streams } of libp2p.getConnections()) {
+			result[id] = {
+				peer: remotePeer.toString(),
+				addr: remoteAddr.toString(),
+				streams: Object.fromEntries(streams.map((stream) => [stream.id, stream.protocol ?? null])),
+			}
+		}
+
+		return res.status(StatusCodes.OK).json(result)
+	})
+
+	api.get("/peers/:topic", async (req, res) => {
+		const subscribers = libp2p.services.pubsub.getSubscribers(req.params.topic)
+		return res.json(subscribers.map((peer) => peer.toString()))
+	})
+
+	// api.get("")
+
+	api.post("/ping/:peerId", async (req, res) => {
+		const requestController = new AbortController()
+		req.on("close", () => requestController.abort())
+
+		const signal = anySignal([AbortSignal.timeout(PING_TIMEOUT), requestController.signal])
+
+		let peerId: PeerId
+		try {
+			peerId = peerIdFromString(req.params.peerId)
+		} catch (err) {
+			return res.status(StatusCodes.BAD_REQUEST).end(`${err}`)
+		}
+
+		try {
+			const latency = await libp2p.services.ping.ping(peerId, { signal })
+			res.status(StatusCodes.OK).end(`Got response from ${peerId} in ${latency}ms\n`)
+		} catch (err) {
+			if (err instanceof AbortError) {
+				res.status(StatusCodes.GATEWAY_TIMEOUT).end(err.toString())
+			} else {
+				res.status(StatusCodes.INTERNAL_SERVER_ERROR).end(`${err}`)
+			}
+		} finally {
+			signal.clear()
+		}
+	})
+
+	return http.createServer(api)
+}
---
+++ packages/bootstrap-peer/src/libp2p.ts
@@ -0,0 +1,115 @@
+import assert from "node:assert"
+
+import { Libp2pOptions } from "libp2p"
+import { PingService, pingService } from "libp2p/ping"
+import { identifyService } from "libp2p/identify"
+import { FetchService, fetchService } from "libp2p/fetch"
+import { CircuitRelayService, circuitRelayServer } from "libp2p/circuit-relay"
+
+import { webSockets } from "@libp2p/websockets"
+import { all } from "@libp2p/websockets/filters"
+import { noise } from "@chainsafe/libp2p-noise"
+import { mplex } from "@libp2p/mplex"
+import { bootstrap } from "@libp2p/bootstrap"
+import { GossipsubEvents, gossipsub } from "@chainsafe/libp2p-gossipsub"
+import { prometheusMetrics } from "@libp2p/prometheus-metrics"
+
+import type { PubSub } from "@libp2p/interface/pubsub"
+import { peerIdFromString } from "@libp2p/peer-id"
+import { isLoopback } from "@libp2p/utils/multiaddr/is-loopback"
+import { Multiaddr, multiaddr } from "@multiformats/multiaddr"
+
+import { DiscoveryService, discovery } from "@canvas-js/discovery"
+
+import { second, minute, MIN_CONNECTIONS, MAX_CONNECTIONS } from "./constants.js"
+import { peerId, bootstrapList, listen, announce } from "./config.js"
+
+async function denyDialMultiaddr(addr: Multiaddr) {
+	const transportRoot = addr.decapsulate("/ws")
+	if (transportRoot.isThinWaistAddress() && isLoopback(transportRoot)) {
+		return true
+	}
+
+	const relayRoot = addr.decapsulateCode(290) // /p2p-circuit
+	if (relayRoot.getPeerId() === peerId.toString()) {
+		return true
+	}
+
+	return false
+}
+
+export type ServiceMap = {
+	identify: {}
+	ping: PingService
+	relay: CircuitRelayService
+	pubsub: PubSub<GossipsubEvents>
+	fetch: FetchService
+	discovery: DiscoveryService
+}
+
+export const options: Libp2pOptions<ServiceMap> = {
+	peerId,
+	start: false,
+	addresses: { listen, announce },
+	transports: [webSockets({ filter: all })],
+	connectionGater: { denyDialMultiaddr },
+	connectionManager: {
+		minConnections: MIN_CONNECTIONS,
+		maxConnections: MAX_CONNECTIONS,
+	},
+
+	peerDiscovery: bootstrapList.length > 0 ? [bootstrap({ list: bootstrapList })] : [],
+
+	streamMuxers: [mplex()],
+	connectionEncryption: [noise()],
+	metrics: prometheusMetrics(),
+	services: {
+		identify: identifyService({ protocolPrefix: "canvas" }),
+
+		ping: pingService({
+			protocolPrefix: "canvas",
+			maxInboundStreams: 256,
+			maxOutboundStreams: 64,
+			timeout: 20 * second,
+		}),
+
+		relay: circuitRelayServer({
+			hopTimeout: 10 * second, // incoming relay requests must be resolved within this time limit
+			advertise: false,
+			reservations: {
+				maxReservations: 512, // how many peers are allowed to reserve relay slots on this server
+				reservationClearInterval: 1 * minute, // how often to reclaim stale reservations
+				applyDefaultLimit: true, // whether to apply default data/duration limits to each relayed connection
+				reservationTtl: 15 * minute,
+
+				// defaultDurationLimit: 15 * minute, // the default maximum amount of time a relayed connection can be open for
+				// defaultDataLimit: 4_000_000_000n, // the default maximum number of bytes that can be transferred over a relayed connection
+				// defaultDurationLimit: 1 * minute, // the default maximum amount of time a relayed connection can be open for
+				// defaultDataLimit: 1_000_000n, // the default maximum number of bytes that can be transferred over a relayed connection
+			},
+
+			// how many inbound HOP streams are allow simultaneously
+			maxInboundHopStreams: 1024,
+
+			// how many outbound HOP streams are allow simultaneously
+			maxOutboundHopStreams: 1024,
+		}),
+
+		pubsub: gossipsub({
+			doPX: true,
+			canRelayMessage: true,
+			fallbackToFloodsub: false,
+			allowPublishToZeroPeers: true,
+			globalSignaturePolicy: "StrictNoSign",
+			directPeers: bootstrapList.map(multiaddr).map((addr) => {
+				const id = addr.getPeerId()
+				assert(id !== null)
+				return { id: peerIdFromString(id), addrs: [addr] }
+			}),
+		}),
+
+		fetch: fetchService({ protocolPrefix: "canvas" }),
+
+		discovery: discovery({}),
+	},
+}
