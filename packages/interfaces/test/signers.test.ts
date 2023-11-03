e809100142d0b835200f6fa0ec542ac3e73b2315
Joel Gustafson <joelg@mit.edu>
2023-11-03 08:24:40 -0400

    packages/interfaces: add chain-solana to interface tests

--- packages/interfaces/test/signers.test.ts
+++ packages/interfaces/test/signers.test.ts
@@ -4,6 +4,8 @@ import { Message, Session, SessionSigner as Signer } from "@canvas-js/interfaces
 import { verifySignature } from "@canvas-js/signed-cid"
 
 import { SIWESigner } from "@canvas-js/chain-ethereum"
+import { SolanaSigner } from "@canvas-js/chain-solana"
+// import { ATPSigner } from "@canvas-js/chain-atp"
 
 type SignerImplementation = { createSigner: () => Promise<Signer>; name: string }
 
@@ -11,6 +13,10 @@ const SIGNER_IMPLEMENTATIONS: SignerImplementation[] = [
 	{
 		name: "chain-ethereum",
 		createSigner: async () => new SIWESigner(),
+	},
+	{
+		name: "chain-solana",
+		createSigner: async () => new SolanaSigner(),
 	},
 ]
 
