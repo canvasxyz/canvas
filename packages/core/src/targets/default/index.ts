5039b6dd42250d63d43496e3cdc6637b750f52c1
Joel Gustafson <joelg@mit.edu>
2023-12-02 11:52:03 -0500

    packages/core: add src/targets/default

---
+++ packages/core/src/targets/default/index.ts
@@ -0,0 +1,25 @@
+import type { Libp2p } from "libp2p"
+import type { PeerId } from "@libp2p/interface/peer-id"
+import type { GossipLogInit } from "@canvas-js/gossiplog"
+import type { PlatformTarget, ServiceMap } from "../interface.js"
+
+export default {
+	async getPeerId(location: { topic: string; path: string | null }): Promise<PeerId> {
+		throw new Error("Unsupported platform")
+	},
+
+	async openDB(location, models, { indexHistory } = {}) {
+		throw new Error("Unsupported platform")
+	},
+
+	async openGossipLog<Payload, Result>(
+		location: { path: string | null; topic: string },
+		init: GossipLogInit<Payload, Result>,
+	) {
+		throw new Error("Unsupported platform")
+	},
+
+	async createLibp2p(peerId, options): Promise<Libp2p<ServiceMap>> {
+		throw new Error("Unsupported platform")
+	},
+} satisfies PlatformTarget
--- packages/core/src/targets/index.ts
+++
@@ -1,5 +0,0 @@
-import type { PlatformTarget } from "./interface.js"
-
-export default (location: string | null): PlatformTarget => {
-	throw new Error("unsupported platform")
-}
