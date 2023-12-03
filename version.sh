97e7169b77c903e33a736a1bdfb3cecff6ae97d6
Raymond Z <raykyri@gmail.com>
2023-12-02 20:53:16 -0500

    update version.sh

--- version.sh
+++ version.sh
@@ -10,6 +10,17 @@ echo "$( jq "${SET_VERSION}" package.json )" > package.json
 
 PACKAGES="$(ls packages/)"
 
+for f in ./package.json; do
+  p=$( cat $f | jq "${SET_VERSION}" )
+  for PACKAGE in ${PACKAGES}; do
+    P="\"@canvas-js/${PACKAGE}\"";
+    SET_DEPENDENCIES="if .dependencies.${P}? then .dependencies.${P} = ${VERSION} else . end";
+    SET_DEV_DEPENDENCIES="if .devDependencies.${P}? then .devDependencies.${P} = ${VERSION} else . end";
+    p=$( echo $p | jq "${SET_DEPENDENCIES} | ${SET_DEV_DEPENDENCIES}" );
+  done;
+  echo $p | jq > $f;
+done
+
 for f in ./packages/*/package.json; do
   p=$( cat $f | jq "${SET_VERSION}" )
   for PACKAGE in ${PACKAGES}; do
