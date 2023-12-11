d1cc090304769b6fb2dc92c062e300a45c34762b
Bob Webb <rjwebb@users.noreply.github.com>
2023-12-11 21:36:02 +0000

    Add @canvas-js/chain-ethereum-viem signer (#221)

---
+++ packages/chain-ethereum-viem/src/types.ts
@@ -0,0 +1,17 @@
+export type SIWESessionData = {
+	signature: Uint8Array
+	domain: string
+	nonce: string
+}
+
+export type SIWEMessage = {
+	version: string
+	address: string
+	chainId: number
+	domain: string
+	uri: string
+	nonce: string
+	issuedAt: string
+	expirationTime: string | null
+	resources: string[]
+}
