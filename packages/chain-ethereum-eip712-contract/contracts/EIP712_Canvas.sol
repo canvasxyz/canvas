3af5a13ffc1a98182d196b883a7ce99db7755afe
Bob Webb <bob.wbb@gmail.com>
2024-01-03 15:43:51 +0000

    WIP: smart contract

--- packages/chain-ethereum-eip712-contract/contracts/EIP712_Canvas.sol
+++ packages/chain-ethereum-eip712-contract/contracts/EIP712_Canvas.sol
@@ -6,12 +6,10 @@ import "@openzeppelin/contracts/utils/cryptography/EIP712.sol";
 
 contract EIP712_Canvas is EIP712{
 
-    /// @dev For this contract it should always be keccak256("Ticket(string eventName,uint256 price,address signedBy)")
-    /// @dev but we are assigning the value in the constructor for the sake of learning
-    bytes32 immutable typedDataHash;
+    bytes32 immutable sessionTypedDataHash;
 
-    constructor(string memory domainName, string memory signatureVersion, bytes32 typedDataHash_) EIP712(domainName,signatureVersion) {
-        typedDataHash = typedDataHash_;
+    constructor(string memory domainName, string memory signatureVersion) EIP712(domainName,signatureVersion) {
+        sessionTypedDataHash = keccak256(bytes("Session(address address,string blockhash,uint256 duration,string publicKey,uint256 timestamp)"));
     }
 
     struct Action {
@@ -23,48 +21,69 @@ contract EIP712_Canvas is EIP712{
         address _blockhash;
     }
 
-    struct ActionMessage {
+    struct MessageWithAction {
         string topic;
         uint256 clock;
         string[] parents;
         Action payload;
     }
 
-    struct Ticket {
-        string eventName;
-        uint256 price;
+    struct Session {
+        address address_;
+	    address blockhash;
+        uint256 duration;
+	    bytes publicKey;
+        uint256 timestamp;
     }
 
     /// Returns the address of the signer of that the ticket
-    /// @param eventName The name of the event
-    /// @param price  The price (in wei) of the event
-    /// @param signature The ticket seller signature
-    function getSigner( string calldata eventName, uint256 price, bytes memory signature) public view returns (address){
-        Ticket memory ticket = Ticket(eventName,price);
-        address signer = _verify(ticket, signature);
-        return signer;
+    // function getSigner( string calldata eventName, uint256 price, bytes memory signature) public view returns (address){
+    //     Ticket memory ticket = Ticket(eventName,price);
+    //     address signer = _verify(ticket, signature);
+    //     return signer;
+    // }
+
+    // SessionMessage is such a bad name
+    function _verifySession(Session memory session, bytes memory signature) internal view returns (address){
+        // bytes32 digest = _hashSession(session);
+
+
+        bytes32 digest = _hashTypedDataV4(
+            keccak256(
+                abi.encode(
+                    sessionTypedDataHash, // keccak hash of typed data
+                    session.address_,
+                    session.blockhash,
+                    session.duration,
+                    session.publicKey,
+                    session.timestamp
+                )
+            )
+        );
+
+        return ECDSA.recover(digest, signature);
     }
 
     /// @notice Verifies the signature for a given Ticket, returning the address of the signer.
     /// @dev Will revert if the signature is invalid.
     /// @param ticket A ticket describing an event
     /// @param signature The ticket seller signature
-    function _verify(Ticket memory ticket, bytes memory signature) internal view returns (address){
-        bytes32 digest = _hashTypedData(ticket);
-        return ECDSA.recover(digest, signature);
-    }
+    // function _verify(Ticket memory ticket, bytes memory signature) internal view returns (address){
+    //     bytes32 digest = _hashTypedData(ticket);
+    //     return ECDSA.recover(digest, signature);
+    // }
 
     /// @notice Returns a hash of a given Ticket, prepared using EIP712 typed data hashing rules.
     /// @param ticket A ticket describing an event
-    function _hashTypedData(Ticket memory ticket) internal view returns (bytes32){
-        return _hashTypedDataV4(
-            keccak256(
-                abi.encode(
-                    typedDataHash, // keccak hash of typed data
-                    keccak256(bytes(ticket.eventName)), // encoding string to get its hash
-                    ticket.price //uint value
-                )
-            )
-        );
-    }
+    // function _hashTypedData(Ticket memory ticket) internal view returns (bytes32){
+        // return _hashTypedDataV4(
+        //     keccak256(
+        //         abi.encode(
+        //             typedDataHash, // keccak hash of typed data
+        //             keccak256(bytes(ticket.eventName)), // encoding string to get its hash
+        //             ticket.price //uint value
+        //         )
+        //     )
+        // );
+    // }
 }
--- packages/chain-ethereum-eip712-contract/test/EIP712_Canvas.ts
+++ packages/chain-ethereum-eip712-contract/test/EIP712_Canvas.ts
@@ -3,37 +3,19 @@ import { expect } from "chai"
 import { ethers, network } from "hardhat"
 import { signTypedData } from "../helpers/EIP712"
 import { EIP712Domain, EIP712TypeDefinition } from "../helpers/EIP712.types"
-import { EIP712Signer } from "@canvas-js/chain-ethereum-eip712"
 
 describe("EIP712_Canvas", function () {
-	// We define a fixture to reuse the same setup in every test.
-	// We use loadFixture to run this setup once, snapshot that state,
-	// and reset Hardhat Network to that snapshot in every test.
 	async function deployFixture() {
 		const EIP712_Canvas = await ethers.getContractFactory("EIP712_Canvas")
 
-		// Create an EIP712 domainSeparator
-		// https://eips.ethereum.org/EIPS/eip-712#definition-of-domainseparator
-		const domainName = "TicketGenerator" // the user readable name of signing domain, i.e. the name of the DApp or the protocol.
-		const signatureVersion = "1" // the current major version of the signing domain. Signatures from different versions are not compatible.
-		const chainId = network.config.chainId as number // the EIP-155 chain id. The user-agent should refuse signing if it does not match the currently active chain.
-		// The typeHash is designed to turn into a compile time constant in Solidity. For example:
-		// bytes32 constant MAIL_TYPEHASH = keccak256("Mail(address from,address to,string contents)");
-		// https://eips.ethereum.org/EIPS/eip-712#rationale-for-typehash
-		const typeHash = "Ticket(string eventName,uint256 price)"
-		const argumentTypeHash = ethers.utils.keccak256(ethers.utils.toUtf8Bytes(typeHash)) // convert to byteslike, then hash it
+		const domainName = "Canvas"
+		const signatureVersion = "1"
+		const chainId = network.config.chainId as number
 
-		// https://eips.ethereum.org/EIPS/eip-712#specification-of-the-eth_signtypeddata-json-rpc
-		const types: EIP712TypeDefinition = {
-			Ticket: [
-				{ name: "eventName", type: "string" },
-				{ name: "price", type: "uint256" },
-			],
-		}
 		// get an instance of the contract
-		const contract = await EIP712_Canvas.deploy(domainName, signatureVersion, argumentTypeHash)
+		const contract = await EIP712_Canvas.deploy(domainName, signatureVersion)
 
-		const verifyingContract = contract.address // the address of the contract that will verify the signature. The user-agent may do contract specific phishing prevention.
+		const verifyingContract = contract.address
 
 		const domain: EIP712Domain = {
 			name: domainName,
@@ -42,23 +24,25 @@ describe("EIP712_Canvas", function () {
 			verifyingContract: verifyingContract,
 		}
 
-		return { contract, domain, types }
+		return { contract, domain }
 	}
 
 	describe("Signing data", function () {
 		it("Should verify that a ticket has been signed by the proper address", async function () {
-			const { contract, domain, types } = await loadFixture(deployFixture)
-			const ticket = {
-				eventName: "EthDenver",
-				price: ethers.constants.WeiPerEther,
-			}
-
-			// const signature = await signTypedData(domain, types, ticket, owner)
+			const { EIP712Signer } = await import("@canvas-js/chain-ethereum-eip712")
+			const { contract, domain } = await loadFixture(deployFixture)
+			// TODO: implement test
 
 			const topic = "example:signer"
 
-			const signer = new EIP712Signer({})
+			const signer = new EIP712Signer({
+				chainId: domain.chainId,
+				verifyingContract: domain.verifyingContract,
+				version: domain.version,
+			})
 			const session = await signer.getSession(topic)
+
+			// abi encode the session
 
 			// expect(await contract.getSigner(ticket.eventName, ticket.price, signature)).to.equal(owner.address)
 		})
