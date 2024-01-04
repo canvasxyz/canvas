d4a49608be9fe236fdd984fc579d1fa25dab8812
Bob Webb <bob.wbb@gmail.com>
2024-01-04 10:06:36 +0000

    remove redundant prettierrc file, update formatting

--- packages/chain-ethereum-eip712-contract/helpers/EIP712.ts
+++ packages/chain-ethereum-eip712-contract/helpers/EIP712.ts
@@ -1,20 +1,16 @@
-import {
-  EIP712Domain,
-  EIP712TypeDefinition,
-  HardhatSignerType,
-} from "./EIP712.types";
+import { EIP712Domain, EIP712TypeDefinition, HardhatSignerType } from "./EIP712.types"
 
 export async function signTypedData(
-  domain: EIP712Domain,
-  types: EIP712TypeDefinition,
-  values: Object,
-  signer: HardhatSignerType
+	domain: EIP712Domain,
+	types: EIP712TypeDefinition,
+	values: any,
+	signer: HardhatSignerType,
 ) {
-  try {
-    const signature = await signer._signTypedData(domain, types, values);
-    return signature;
-  } catch (error) {
-    console.log("[signTypedData]::error ", error);
-    return "";
-  }
+	try {
+		const signature = await signer._signTypedData(domain, types, values)
+		return signature
+	} catch (error) {
+		console.log("[signTypedData]::error ", error)
+		return ""
+	}
 }
