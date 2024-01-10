2460e6b233bd44c6b9963c1912c2bc7171d146e1
Bob Webb <bob.wbb@gmail.com>
2024-01-10 19:38:15 +0000

    simplify test

--- packages/chain-ethereum-eip712-contract/test/EIP712_Canvas.ts
+++ packages/chain-ethereum-eip712-contract/test/EIP712_Canvas.ts
@@ -14,12 +14,11 @@ describe("EIP712_Canvas", function () {
 	}
 
 	describe("Signing data", function () {
-		xit("Should verify that a session has been signed by the proper address with getSession", async function () {
+		it("Should verify that a session has been signed by the proper address with getSession", async function () {
 			const { EIP712Signer } = await import("@canvas-js/chain-ethereum-eip712")
 			const { base58btc } = await import("multiformats/bases/base58")
 			const { varint } = await import("multiformats")
 			const { verifySignedValue, didKeyPattern } = await import("@canvas-js/signed-cid")
-			const { sha256 } = await import("@noble/hashes/sha256")
 			const { publicKeyToAddress } = await import("viem/utils")
 
 			const { contract } = await loadFixture(deployFixture)
@@ -31,7 +30,6 @@ describe("EIP712_Canvas", function () {
 
 			const walletAddress = session.address.split(":")[2]
 
-			console.log(Buffer.from(session.authorizationData.signature).toString("hex"))
 			const recoveredWalletAddress = await contract.recoverAddressFromSession(
 				walletAddress,
 				session.blockhash || "",
@@ -49,6 +47,7 @@ describe("EIP712_Canvas", function () {
 
 			verifySignedValue(sessionSignature, sessionMessage)
 
+			// extract the public key from the URI
 			const result = didKeyPattern.exec(sessionSignature.publicKey)
 			const bytes = base58btc.decode(result![1])
 			const [keyCodec, keyCodecLength] = varint.decode(bytes)
@@ -56,80 +55,22 @@ describe("EIP712_Canvas", function () {
 			const publicKeyHex = Buffer.from(publicKey).toString("hex")
 
 			const expectedAddress = publicKeyToAddress(`0x${publicKeyHex}`)
-			console.log(`expectedAddress using publicKeyToAddress: ${expectedAddress}`)
 
-			console.log(`cid.bytes: ${Buffer.from(sessionSignature.cid.bytes).toString("hex")}`)
+			const signedValue = sessionSignature.cid.bytes.slice(0, 32)
 
-			const shaCidBytes = sha256(sessionSignature.cid.bytes)
-
-			// why doesn't this work?
-			// one of the b values should equal expectedAddress
+			// we don't return the recovery parameter currently, so we need to try both of them
+			let matchingAddressFound = false
+			// we should include the recovery parameter as part of the signature
+			// and then just ignore it if we are verifying using a method that doesn't need it
 			for (const v of [27, 28]) {
-				const b = await contract.validateAddressFromCidSignature2(shaCidBytes, [...sessionSignature.signature, v])
-				console.log(`recovered address when v = ${v}: ${b}`)
+				const signatureWithRecoveryParam = [...sessionSignature.signature, v]
+				const b = await contract.validateAddressFromCidSignature2(signedValue, signatureWithRecoveryParam)
+				console.log(`.sol recovered address when v = ${v}: ${b}`)
+				if (b === expectedAddress) {
+					matchingAddressFound = true
+				}
 			}
-		})
-
-		it("test sign", async function () {
-			const { secp256k1 } = await import("@noble/curves/secp256k1")
-			const { generatePrivateKey, privateKeyToAccount } = await import("viem/accounts")
-			const { hashMessage } = await import("viem/utils")
-			const { toBytes } = await import("viem")
-
-			const { contract } = await loadFixture(deployFixture)
-
-			const message =
-				"hello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello worldhello world"
-			const signedData = hashMessage(message)
-
-			const privateKey = generatePrivateKey()
-			const viemAccount = privateKeyToAccount(privateKey)
-
-			const viemSignature = await viemAccount.signMessage({ message })
-			const secpSignature = secp256k1.sign(signedData.slice(2), privateKey.slice(2))
-
-			const secpSignatureWithRecovery = [...secpSignature.toCompactRawBytes(), secpSignature.recovery + 27]
-
-			console.log(secpSignature)
-			expect(viemSignature).to.equal(viemSignature)
-
-			const expectedAddress = viemAccount.address
-			const recoveredAddressViemSolidity = await contract.validateAddressFromCidSignature2(
-				signedData,
-				toBytes(viemSignature),
-			)
-
-			expect(recoveredAddressViemSolidity).to.equal(expectedAddress)
-			const recoveredAddressSecpSolidity = await contract.validateAddressFromCidSignature2(
-				signedData,
-				secpSignatureWithRecovery,
-			)
-			console.log(`recoveredAddressSecpSolidity: ${recoveredAddressSecpSolidity}`)
-			expect(recoveredAddressSecpSolidity).to.equal(expectedAddress)
-
-			const secpRecoveredPublicKey = secpSignature.recoverPublicKey(signedData.slice(2)).toRawBytes()
-			console.log(`viemAccount.publicKey: ${Buffer.from(viemAccount.publicKey).toString("hex")}`)
-			console.log(`secpRecoveredPublicKey: ${Buffer.from(secpRecoveredPublicKey).toString("hex")}`)
-
-			/*
-			const signer = new Secp256k1Signer()
-			const sessionSignature = signer.sign("hello world", { codec: "dag-cbor", digest: "sha2-256" })
-
-			// secp256k1.sign(cid.bytes, this.#privateKey).toCompactRawBytes()
-
-			const result = didKeyPattern.exec(sessionSignature.publicKey)
-			const bytes = base58btc.decode(result![1])
-			const [keyCodec, keyCodecLength] = varint.decode(bytes)
-			const publicKey = bytes.subarray(keyCodecLength)
-
-			const expectedAddress = `0x${ethers.utils.keccak256(publicKey).slice(-40)}`
-			console.log(`session address from publicKey: ${expectedAddress}`)
-			const signedData = sessionSignature.cid.bytes.slice(0, 32)
-			for (const v of [27, 28]) {
-				const b = await contract.validateAddressFromCidSignature2(signedData, [...sessionSignature.signature, v])
-				console.log(b)
-			}
-			*/
+			expect(matchingAddressFound).to.equal(true)
 		})
 	})
 })
