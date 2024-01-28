3978e9b18b23cdb3059ff8f494ba573953dd378c
Raymond Z <raykyri@gmail.com>
2024-01-28 13:07:23 -0500

    tests: minor changes

--- packages/ethereum-contracts/test/CID.ts
+++ packages/ethereum-contracts/test/CID.ts
@@ -11,21 +11,22 @@ describe("CID", function () {
 		return { contract }
 	}
 
-	describe("Signing data", function () {
-		it("test createDigest solidity implementation", async () => {
+	describe("contract.createDigest", function () {
+		it("matches implementation in multiformats", async () => {
 			const { sha256 } = await import("@noble/hashes/sha256")
 			const { create: createDigest } = await import("multiformats/hashes/digest")
 
 			const { contract } = await loadFixture(deployFixture)
 
 			const [digestSize, contractDigest] = await contract.createDigest(3, sha256("hello world"))
+			const multiformatsDigest = createDigest(3, sha256("hello world"))
 
-			const mfDigest = `0x${Buffer.from(createDigest(3, sha256("hello world")).bytes).toString("hex")}`
-
-			expect(contractDigest).to.equal(mfDigest)
+			expect(contractDigest).to.equal(`0x${Buffer.from(multiformatsDigest.bytes).toString("hex")}`)
 		})
+	})
 
-		it("test encodeCID solidity implementation", async () => {
+	describe("contract.encodeCID", function () {
+		it("matches implementation in multiformats", async () => {
 			const { sha256 } = await import("@noble/hashes/sha256")
 			const { CID } = await import("multiformats/cid")
 			const { create: createDigest } = await import("multiformats/hashes/digest")
@@ -35,9 +36,9 @@ describe("CID", function () {
 			const [digestSize, contractDigest] = await contract.createDigest(0, sha256("hello world"))
 			const contractCid = await contract.encodeCID(1, 712, contractDigest)
 
-			const mfCid = CID.createV1(712, createDigest(0, sha256("hello world")))
+			const multiformatsCid = CID.createV1(712, createDigest(0, sha256("hello world")))
 
-			expect(contractCid).to.equal(`0x${Buffer.from(mfCid.bytes).toString("hex")}`)
+			expect(contractCid).to.equal(`0x${Buffer.from(multiformatsCid.bytes).toString("hex")}`)
 		})
 
 		// it("test creating and verifying CID for Message<Session>", async () => {
