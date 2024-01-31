a124440dea8dd3ba4e1a72077ded54fb6a9c420c
Bob Webb <bob.wbb@gmail.com>
2024-01-31 18:27:32 +0000

    add an example contract that verifies the session/message and updates state

---
+++ packages/ethereum-contracts/contracts/Contract_Example.sol
@@ -0,0 +1,64 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.19;
+
+import "./EIP712_Canvas.sol";
+
+string constant topic = "xyz.canvas.myapp";
+
+contract Contract_Example {
+
+
+  mapping(bytes32 => bool) public appliedActionHashes;
+  uint256 public upvotes;
+
+  function claimUpvoted(
+    address expectedAddress,
+    bytes memory sessionSignature,
+    EIP712_Canvas.SessionMessage memory sessionMessage,
+    bytes memory sessionMessageSignature,
+    EIP712_Canvas.ActionMessage memory actionMessage,
+    bytes memory actionMessageSignature
+  ) public returns (bool)  {
+
+    bytes32 actionHash = EIP712_Canvas.hashAction(actionMessage.payload);
+
+    require(
+      !appliedActionHashes[actionHash],
+      "Each action can only be applied once"
+    );
+
+    // verify the signatures
+    require(
+      EIP712_Canvas.verifySession(sessionMessage.payload, sessionSignature, sessionMessage.payload.address_),
+      "Session must be signed by wallet address"
+    );
+    require(
+      EIP712_Canvas.verifySessionMessage(sessionMessage, sessionMessageSignature, expectedAddress),
+      "Session message must be signed by session address"
+    );
+    require(
+      EIP712_Canvas.verifyActionMessage(actionMessage, actionMessageSignature, expectedAddress),
+      "Action message must be signed by session address"
+    );
+
+    uint256 sessionExpirationTime = sessionMessage.payload.timestamp + sessionMessage.payload.duration;
+    require(
+      actionMessage.payload.timestamp < sessionExpirationTime,
+      "Action must have been signed by a session that has not expired"
+    );
+
+    // validate the action name
+
+    // validate the action args
+    // TODO: how do we do this?
+
+    // Now, increase a counter stored on this contract by +1, and
+    // save the hash of the action in a mapping on the contract's storage,
+    // so someone can't submit the same action twice.
+    appliedActionHashes[actionHash] = true;
+    upvotes += 1;
+
+    return true;
+  }
+
+}
---
+++ packages/ethereum-contracts/test/Contract_Example.ts
@@ -0,0 +1,130 @@
+import { loadFixture } from "@nomicfoundation/hardhat-network-helpers"
+import { expect } from "chai"
+import { ethers } from "hardhat"
+
+const topic = "xyz.canvas.myapp"
+
+describe("Contract_Example", function () {
+	async function deployFixture() {
+		const EIP712_Canvas = await ethers.getContractFactory("EIP712_Canvas")
+		const eip712_Canvas = await EIP712_Canvas.deploy()
+
+		const Contract_Example = await ethers.getContractFactory("Contract_Example", {
+			libraries: {
+				EIP712_Canvas: eip712_Canvas.address,
+			},
+		})
+
+		const contract = await Contract_Example.deploy()
+		await contract.deployed()
+		return { contract }
+	}
+
+	async function getPublicKeyFromSignatureFixture() {
+		const { base58btc } = await import("multiformats/bases/base58")
+		const { varint } = await import("multiformats")
+		const { didKeyPattern } = await import("@canvas-js/signed-cid")
+
+		function getPublicKeyFromSignature(signature: any) {
+			console.log(signature.publicKey)
+			const result = didKeyPattern.exec(signature.publicKey)
+			const bytes = base58btc.decode(result![1])
+			const [keyCodec, keyCodecLength] = varint.decode(bytes)
+			return bytes.subarray(keyCodecLength)
+		}
+
+		return { getPublicKeyFromSignature }
+	}
+
+	describe("Contract_Example.claimUpvoted", function () {
+		it("try to call claimUpvoted with a valid session and action", async () => {
+			const { getAbiString } = await import("@canvas-js/signed-cid")
+			const { EIP712Signer } = await import("@canvas-js/chain-ethereum")
+
+			const { contract } = await loadFixture(deployFixture)
+			expect(await contract.upvotes()).to.equal(0)
+
+			const { getPublicKeyFromSignature } = await loadFixture(getPublicKeyFromSignatureFixture)
+
+			const signer = new EIP712Signer({})
+
+			const session = await signer.getSession(topic)
+
+			const clock = 1
+			const parents = ["parent1", "parent2"]
+			const sessionMessage = { topic, clock, parents, payload: session }
+			const sessionMessageSignature = signer.sign(sessionMessage)
+
+			const publicKey = getPublicKeyFromSignature(sessionMessageSignature)
+			const publicKeyHex = Buffer.from(publicKey).toString("hex")
+			const expectedAddress = ethers.utils.computeAddress(`0x${publicKeyHex}`)
+
+			const sessionMessageForContract = {
+				clock,
+				parents,
+				topic,
+				payload: {
+					address_: session.address.split(":")[2],
+					blockhash: session.blockhash || "",
+					duration: session.duration || 0,
+					publicKey: session.publicKey,
+					timestamp: session.timestamp,
+				},
+			}
+
+			const action = {
+				type: "action" as const,
+				address: session.address,
+				name: "foo",
+				args: { bar: 7 },
+				blockhash: null,
+				timestamp: session.timestamp,
+			}
+			const actionMessage = { topic, clock, parents, payload: action }
+			const actionMessageSignature = signer.sign(actionMessage)
+			const actionMessageForContract = {
+				clock,
+				parents,
+				topic,
+				// action fields
+				payload: {
+					address_: action.address.split(":")[2],
+					args: getAbiString(action.args),
+					blockhash: action.blockhash || "",
+					name: action.name,
+					timestamp: action.timestamp,
+				},
+			}
+
+			// submit the upvote action
+			await contract.claimUpvoted(
+				expectedAddress,
+				session.authorizationData.signature,
+				sessionMessageForContract,
+				sessionMessageSignature.signature,
+				actionMessageForContract,
+				actionMessageSignature.signature,
+			)
+
+			// Expect the upvote to have been applied
+			expect(await contract.upvotes()).to.equal(1)
+
+			// If we submit the action again, it should be rejected
+			expect(
+				contract.claimUpvoted(
+					expectedAddress,
+					session.authorizationData.signature,
+					sessionMessageForContract,
+					sessionMessageSignature.signature,
+					actionMessageForContract,
+					actionMessageSignature.signature,
+				),
+			).to.be.rejectedWith(
+				"Error: VM Exception while processing transaction: reverted with reason string 'Each action can only be applied once'",
+			)
+
+			// Expect the upvote to still be 1
+			expect(await contract.upvotes()).to.equal(1)
+		})
+	})
+})
