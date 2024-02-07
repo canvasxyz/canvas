0293f010085bf80f3a050e620cc886ab0954e514
Bob Webb <bob.wbb@gmail.com>
2024-02-07 11:35:16 +0000

    let useCanvas take the contract type as a type parameter

--- packages/hooks/src/index.ts
+++ packages/hooks/src/index.ts
@@ -1,3 +1,3 @@
-export { useTick } from "./useTick.js"
+export { TickingContract, useTick } from "./useTick.js"
 export { useCanvas } from "./useCanvas.js"
 export { useLiveQuery } from "./useLiveQuery.js"
