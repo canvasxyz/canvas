933152d251c7c1035cdeaeaed0a0b9cdb865879a
Chris Johnson <chris@cjohnson.io>
2024-02-28 12:13:22 -0600

    -More login functionality

--- examples/encrypted-chat/src/App.tsx
+++ examples/encrypted-chat/src/App.tsx
@@ -1,13 +1,58 @@
-import React from 'react';
-import { Select } from './Select';
+import React, { useState } from 'react';
+import { ethers } from "ethers";
+import { LoginSelect, LoginOptions } from './Select-Login';
 
+interface LoginFormProps {
+  selected: any,
+  wallet: ethers.HDNodeWallet,
+  setWallet: Function,
+};
+
+const LoginForm: React.FC<LoginFormProps> = ({ selected, wallet, setWallet }) => {
+
+  const generateBurnerWallet = () => {
+    setWallet(ethers.Wallet.createRandom())
+  }
+
+  if (selected.id === 'burner') {
+    return (
+      <div>
+        <button className="bg-blue-400 hover:bg-blue-400 text-white py-2 px-4 mt-4" onClick={generateBurnerWallet}>Generate wallet</button>
+
+        {wallet && 
+          <div className="text-sm mt-4">
+            <div>Private key: &lt;hidden&gt;</div>
+            <div>Public key: ${wallet.publicKey.slice(0, 10)}...</div>
+          </div>
+        }
+      </div>
+    )
+  }
+
+  if (selected.id === 'ethereum') {
+    return (
+      <h3>Ethereum login form</h3>
+    )
+  }
+
+  return null;
+};
 
 const ChatApp: React.FC = () => {
+  const [selected, setSelected] = useState(LoginOptions[0])
+  const [wallet, setWallet] = useState(null);
+
   return (
-    <div className="w-96">
-      <h1>Sign in to continue:</h1>
-  
-      <Select></Select>
+    <div>
+      <div className="fixed top-0 p-4">
+        <span>Chat</span>
+        <span className="text-xs"> with Canvas</span>
+      </div>
+
+      <div className="w-96 my-0 mx-auto pt-24">
+        <LoginSelect selected={selected} setSelected={setSelected} />
+        <LoginForm wallet={wallet} setWallet={setWallet} selected={selected} />
+      </div>
     </div>
   )
 };
--- examples/encrypted-chat/src/Select-Login.tsx
+++ examples/encrypted-chat/src/Select-Login.tsx
@@ -2,27 +2,31 @@ import { Fragment, useState } from 'react'
 import { Listbox, Transition } from '@headlessui/react'
 import { CheckIcon, ChevronUpDownIcon } from '@heroicons/react/20/solid'
 import IconFire from './img/icons8-fire-48.png';
+import IconMetamask from './img/metamask-logo-512.png';
 
-const people = [
-  {
-    id: 1,
-    name: 'Wade Cooper',
-    avatar: IconFire,
-  },
-  {
-    id: 2,
-    name: 'Arlene Mccoy',
-    avatar:
-      'https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80',
-  },
-]
-
-function classNames(...classes: any) {
+const classNames = (...classes: any) => {
   return classes.filter(Boolean).join(' ')
 }
 
-const Select = () => {
-  const [selected, setSelected] = useState(people[0])
+export const LoginOptions = [
+  {
+    id: 'select',
+    name: '-- Select login option --',
+    avatar: null,
+  },
+  {
+    id: 'burner',
+    name: 'Burner wallet',
+    avatar: IconFire,
+  },
+  {
+    id: 'ethereum',
+    name: 'Ethereum (metamask)',
+    avatar: IconMetamask,
+  },
+]
+
+export const LoginSelect = ({ selected, setSelected }) => {  
 
   return (
     <Listbox value={selected} onChange={setSelected}>
@@ -32,7 +36,9 @@ const Select = () => {
           <div className="relative mt-2">
             <Listbox.Button className="relative w-full cursor-default rounded-md bg-white py-1.5 pl-3 pr-10 text-left text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:outline-none focus:ring-2 focus:ring-indigo-500 sm:text-sm sm:leading-6">
               <span className="flex items-center">
-                <img src={selected.avatar} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
+                {selected.avatar && 
+                  <img src={selected.avatar} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
+                }
                 <span className="ml-3 block truncate">{selected.name}</span>
               </span>
               <span className="pointer-events-none absolute inset-y-0 right-0 ml-3 flex items-center pr-2">
@@ -48,25 +54,25 @@ const Select = () => {
               leaveTo="opacity-0"
             >
               <Listbox.Options className="absolute z-10 mt-1 max-h-56 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                {people.map((person) => (
+                {LoginOptions.map((loginOption) => (
                   <Listbox.Option
-                    key={person.id}
+                    key={loginOption.id}
                     className={({ active }) =>
                       classNames(
                         active ? 'bg-indigo-600 text-white' : 'text-gray-900',
                         'relative cursor-default select-none py-2 pl-3 pr-9'
                       )
                     }
-                    value={person}
+                    value={loginOption}
                   >
                     {({ selected, active }) => (
                       <>
                         <div className="flex items-center">
-                          <img src={person.avatar} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
+                          <img src={loginOption.avatar} alt="" className="h-5 w-5 flex-shrink-0 rounded-full" />
                           <span
                             className={classNames(selected ? 'font-semibold' : 'font-normal', 'ml-3 block truncate')}
                           >
-                            {person.name}
+                            {loginOption.name}
                           </span>
                         </div>
 
@@ -91,6 +97,4 @@ const Select = () => {
       )}
     </Listbox>
   )
-}
-
-export { Select };
+};
