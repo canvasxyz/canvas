c8538a6de51ad287250b3ed9f3ecf5beb2ebba3d
Bob Webb <bob.wbb@gmail.com>
2024-04-04 17:55:01 +0100

    use ethers functions for converting to/from 0x<hex> addresses/signatures in cosmos ethereum signer code

--- packages/chain-cosmos/src/external_signers/ethereum.ts
+++ packages/chain-cosmos/src/external_signers/ethereum.ts
@@ -1,7 +1,6 @@
 import { fromBech32, toBech32 } from "@cosmjs/encoding"
-import { bytesToHex, hexToBytes } from "@noble/hashes/utils"
 import { CosmosMessage } from "../types.js"
-import { verifyMessage } from "ethers"
+import { verifyMessage, getBytes, toQuantity } from "ethers"
 
 export function encodeReadableEthereumMessage(message: CosmosMessage): string {
 	return `
@@ -23,22 +22,18 @@ export type EthereumSigner = {
 
 export const createEthereumSigner = (signer: EthereumSigner, bech32Prefix: string) => ({
 	getAddress: async (chainId: string) => {
-		const address = (await signer.getAddress(chainId)).substring(2)
-
-		return toBech32(bech32Prefix, hexToBytes(address))
+		const address = await signer.getAddress(chainId)
+		// this assumes that `address` is an ethereum hex address prefixed by 0x
+		return toBech32(bech32Prefix, getBytes(address))
 	},
 	getChainId: signer.getChainId,
 	sign: async (cosmosMessage: CosmosMessage, signerAddress: string, chainId: string) => {
 		const encodedMessage = encodeReadableEthereumMessage(cosmosMessage)
-		const ethAddress = `0x${bytesToHex(fromBech32(signerAddress).data)}`
-		let signature = await signer.signEthereum(chainId, ethAddress, encodedMessage)
-		// some signers prepend the `0x` in the signature result (metamask) and some don't (keplr)
-		// we want to return the signature without this 0x prefix
-		if (signature.slice(0, 2) === "0x") {
-			signature = signature.slice(2)
-		}
+		const ethAddress = toQuantity(fromBech32(signerAddress).data)
+		const signature = await signer.signEthereum(chainId, ethAddress, encodedMessage)
+		// signature is a hex string prefixed by 0x
 		return {
-			signature: hexToBytes(signature),
+			signature: getBytes(signature),
 			signatureType: "ethereum" as const,
 		}
 	},
@@ -48,10 +43,10 @@ export const verifyEthereum = (message: CosmosMessage, sessionData: EthereumSign
 	const walletAddress = message.address
 	const encodedReadableMessage = encodeReadableEthereumMessage(message)
 	// validate ethereum signature
-	const recoveredAddress = verifyMessage(encodedReadableMessage, `0x${bytesToHex(sessionData.signature)}`)
+	const recoveredAddress = verifyMessage(encodedReadableMessage, toQuantity(sessionData.signature))
 	const { prefix } = fromBech32(walletAddress)
 
-	if (toBech32(prefix, hexToBytes(recoveredAddress.substring(2))) !== walletAddress) {
+	if (toBech32(prefix, getBytes(recoveredAddress)) !== walletAddress) {
 		throw Error("invalid signature")
 	}
 }
