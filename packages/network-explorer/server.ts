a386eb99a684fb9037e464dc4c5562d984c7e1d6
Bob Webb <bob.wbb@gmail.com>
2024-05-03 12:39:10 +0100

    top level await and make sure network-explorer is an ESM module

--- packages/network-explorer/server.ts
+++ packages/network-explorer/server.ts
@@ -7,11 +7,12 @@ const dev = process.env.NODE_ENV !== "production"
 const app = next({ dev })
 const handle = app.getRequestHandler()
 
-app.prepare().then(() => {
-	createServer((req, res) => {
-		const parsedUrl = parse(req.url!, true)
-		handle(req, res, parsedUrl)
-	}).listen(port)
+await app.prepare()
 
-	console.log(`> Server listening at http://localhost:${port} as ${dev ? "development" : process.env.NODE_ENV}`)
+const server = createServer((req, res) => {
+	const parsedUrl = parse(req.url!, true)
+	handle(req, res, parsedUrl)
 })
+server.listen(port)
+
+console.log(`> Server listening at http://localhost:${port} as ${dev ? "development" : process.env.NODE_ENV}`)
