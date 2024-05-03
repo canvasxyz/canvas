f9099e334e8a1f9d36f94da761fb0c8977627480
Bob Webb <bob.wbb@gmail.com>
2024-05-03 15:07:42 +0100

    list more message fields

--- packages/network-explorer/pages/index.tsx
+++ packages/network-explorer/pages/index.tsx
@@ -1,7 +1,15 @@
+import { parse } from "@ipld/dag-json"
 import useSWR from "swr"
 
+const fetchAndIpldParseJson = async (url) => {
+	const response = await fetch(url)
+	const json = await response.text()
+	return parse(json)
+}
+
 export default function Home() {
-	const { data, error } = useSWR("/api/messages", (url) => fetch(url).then((res) => res.json()))
+	const { data, error } = useSWR("/api/messages", fetchAndIpldParseJson)
+
 	if (error) return <div>failed to load</div>
 	if (!data) return <div>loading...</div>
 
@@ -9,9 +17,20 @@ export default function Home() {
 		<>
 			<h1>Messages:</h1>
 			<table>
-				{data.map(({ cid, signature, message }) => (
-					<div key={cid}>{cid}</div>
-				))}
+				<thead>
+					<td>cid</td>
+					<td>topic</td>
+					<td>type</td>
+				</thead>
+				<tbody>
+					{data.map(({ cid, signature, message }) => (
+						<tr key={cid}>
+							<td>{cid}</td>
+							<td>{message.topic}</td>
+							<td>{message.payload.type}</td>
+						</tr>
+					))}
+				</tbody>
 			</table>
 		</>
 	)
