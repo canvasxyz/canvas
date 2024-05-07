8d4fd0ad85e7c3923468208f34743a729d92da4d
Bob Webb <bob.wbb@gmail.com>
2024-05-07 18:46:49 +0100

    add react router

--- packages/network-explorer-client/src/main.tsx
+++ packages/network-explorer-client/src/main.tsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react"
+import ReactDOM from "react-dom/client"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+import "./index.css"
+import HomePage from "./HomePage.tsx"
+
+const router = createBrowserRouter([
+	{
+		path: "/",
+		element: <HomePage />,
+	},
+])
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+	<React.StrictMode>
+		<RouterProvider router={router} />
+	</React.StrictMode>,
 )
