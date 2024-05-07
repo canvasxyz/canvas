f409a9adbff4704d7cd2367e5864c1767e62b837
Bob Webb <bob.wbb@gmail.com>
2024-05-07 17:08:06 +0100

    move network explorer client to a new package with vite

---
+++ packages/network-explorer-client/src/App.tsx
@@ -0,0 +1,125 @@
+import useSWR from "swr"
+import { Action, Message, Session, Signature } from "@canvas-js/interfaces"
+import { parse } from "@ipld/dag-json"
+
+import "./App.css"
+
+const fetchAndIpldParseJson = async <T,>(path: string) => {
+	const response = await fetch(`http://localhost:3000${path}`)
+	const json = await response.text()
+	return parse(json) as T
+}
+type Result<T> = [string, Signature, Message<T>]
+
+function App() {
+	const { data, error } = useSWR("/api/messages", fetchAndIpldParseJson<Result<Action | Session>[]>, {
+		refreshInterval: 1000,
+	})
+
+	const { data: connectionsData, error: connectionsError } = useSWR(
+		"/api/connections",
+		fetchAndIpldParseJson<Record<string, { peer: string; addr: string; streams: Record<string, string | null> }>>,
+		{
+			refreshInterval: 1000,
+		},
+	)
+
+	const { data: clockData, error: clockError } = useSWR(
+		"/api/clock",
+		fetchAndIpldParseJson<{ clock: number; parents: string[] }>,
+		{
+			refreshInterval: 1000,
+		},
+	)
+
+	if (error || connectionsError || clockError) return <div>failed to load</div>
+	if (!data || !connectionsData || !clockData) return <div>loading...</div>
+
+	const connectionsDataKeys = Object.keys(connectionsData)
+	connectionsDataKeys.sort()
+
+	const actions = data.filter((item) => item[2].payload.type === "action") as Result<Action>[]
+	const sessions = data.filter((item) => item[2].payload.type === "session") as Result<Session>[]
+
+	return (
+		<>
+			Clock: {clockData.clock} <br />
+			<h1>Actions ({actions.length}):</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>cid</th>
+						<th>topic</th>
+						<th>name</th>
+						<th>args</th>
+						<th>timestamp</th>
+					</tr>
+				</thead>
+				<tbody>
+					{actions.map((item) => {
+						const cid = item[0]
+						const message = item[2]
+						return (
+							<tr key={cid}>
+								<td>{cid}</td>
+								<td>{message.topic}</td>
+								<td>{message.payload.name}</td>
+								<td>{JSON.stringify(message.payload.args)}</td>
+								<td>{message.payload.timestamp}</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+			<h1>Sessions ({sessions.length}):</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>cid</th>
+						<th>topic</th>
+						<th>address</th>
+						<th>publicKey</th>
+						<th>timestamp</th>
+					</tr>
+				</thead>
+				<tbody>
+					{sessions.map((item) => {
+						const cid = item[0]
+						const message = item[2]
+						return (
+							<tr key={cid}>
+								<td>{cid}</td>
+								<td>{message.topic}</td>
+								<td>{message.payload.address}</td>
+								<td>{message.payload.publicKey}</td>
+								<td>{message.payload.timestamp}</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+			<h1>Connections ({connectionsDataKeys.length}):</h1>
+			<table>
+				<thead>
+					<tr>
+						<th>addr</th>
+						<th>peer</th>
+					</tr>
+				</thead>
+				<tbody>
+					{connectionsDataKeys.map((connectionKey) => {
+						const { addr, peer } = connectionsData[connectionKey]
+						return (
+							<tr key={connectionKey}>
+								<td>{addr.split("/").slice(0, -1).join("/")}/...</td>
+								<td>{peer}</td>
+							</tr>
+						)
+					})}
+				</tbody>
+			</table>
+		</>
+	)
+}
+
+export default App
--- packages/network-explorer/server.ts
+++ packages/network-explorer/server.ts
@@ -1,5 +1,5 @@
+import cors from "cors"
 import express from "express"
-import next from "next"
 
 import { createAPI } from "@canvas-js/core/api"
 import { Canvas, defaultBootstrapList } from "@canvas-js/core"
@@ -12,11 +12,6 @@ const dev = process.env.NODE_ENV !== "production"
 console.log(`HTTP_PORT: ${HTTP_PORT}`)
 console.log(`HTTP_ADDR: ${HTTP_ADDR}`)
 console.log(`dev: ${dev}`)
-
-console.log("initializing next.js")
-const nextApp = next({ dev })
-const handle = nextApp.getRequestHandler()
-await nextApp.prepare()
 
 console.log("initializing canvas")
 const canvasApp = await Canvas.initialize({
@@ -43,30 +38,21 @@ const canvasApp = await Canvas.initialize({
 	discoveryTopic: "canvas-discovery",
 	trackAllPeers: true,
 	presenceTimeout: 12 * 60 * 60 * 1000, // keep up to 12 hours of offline peers
-	bootstrapList: [
-		"/dns4/canvas-chat-discovery-staging-p0.fly.dev/tcp/443/wss/p2p/12D3KooWFtS485QGEZwquMQbq7MZTMxiuHs6xUKEi664i4yWUhWa",
-		"/dns4/canvas-chat-discovery-staging-p1.fly.dev/tcp/443/wss/p2p/12D3KooWPix1mT8QavTjfiha3hWy85dQDgPb9VWaxRhY8Yq3cC7L",
-		"/dns4/canvas-chat-discovery-staging-p2.fly.dev/tcp/443/wss/p2p/12D3KooWRbxAWmpvc9U7q1ftBTd3bKa1iQ2rn6RkwRb1d9K7hVg5",
-		"/dns4/canvas-chat.fly.dev/tcp/443/wss/p2p/12D3KooWRrJCTFxZZPWDkZJboAHBCmhZ5MK1fcixDybM8GAjJM2Q",
-		"/dns4/canvas-chat-2.fly.dev/tcp/443/wss/p2p/12D3KooWKGP8AqaPALAqjUf9Bs7KtKtkwDavZBjWhaPqKnisQL7M",
-		"/dns4/canvas-chat-3.fly.dev/tcp/443/wss/p2p/12D3KooWAC1vj6ZGhbW8jgsDCZDK3y2sSJG2QGVZEqhEK7Rza8ic",
-		...defaultBootstrapList,
-	],
+	bootstrapList: [],
+	listen: [`/ip4/0.0.0.0/tcp/8080/ws`],
 })
 
 console.log("initializing libp2p")
 await canvasApp.libp2p.start()
+console.log(`peer id: ${canvasApp.libp2p.peerId}`)
 
 console.log("initializing express")
-const canvasApiApp = createAPI(canvasApp, { exposeMessages: true, exposeModels: true, exposeP2P: true })
-
 const expressApp = express()
+expressApp.use(cors())
 
+console.log("initializing canvas api")
+const canvasApiApp = createAPI(canvasApp, { exposeMessages: true, exposeModels: true, exposeP2P: true })
 expressApp.use("/api", canvasApiApp)
-
-expressApp.all("*", (req, res) => {
-	return handle(req, res)
-})
 
 expressApp.listen(HTTP_PORT, HTTP_ADDR, () => {
 	console.log(`> Ready on http://${HTTP_ADDR}:${HTTP_PORT}`)
