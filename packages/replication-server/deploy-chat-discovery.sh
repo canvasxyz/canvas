703018d2f8889a0a23ac9aae3a8751feb0401222
Raymond Z <raykyri@gmail.com>
2024-05-07 15:17:03 -0400

    use random room id in probe server too

--- packages/replication-server/deploy-chat-discovery.sh
+++ packages/replication-server/deploy-chat-discovery.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env sh
-fly deploy --build-arg NODE_ENV=production --remote-only -a canvas-chat-discovery-p0 -c services/p0.fly.toml
-fly deploy --build-arg NODE_ENV=production --remote-only -a canvas-chat-discovery-p1 -c services/p1.fly.toml
-# fly deploy --build-arg NODE_ENV=production --remote-only -a canvas-chat-discovery-p2 -c services/p2.fly.toml
+# fly deploy --build-arg NODE_ENV=production --remote-only -a canvas-chat-discovery-p0 -c services/p0.fly.toml
+# fly deploy --build-arg NODE_ENV=production --remote-only -a canvas-chat-discovery-p1 -c services/p1.fly.toml
+fly deploy --build-arg NODE_ENV=production --remote-only -a canvas-chat-discovery-p2 -c services/p2.fly.toml
