acfe0e273c272e9655fd86c20d163203d0f180c1
Bob Webb <bob.wbb@gmail.com>
2024-05-09 17:13:22 +0100

    navbar should link to applications (home) page

--- packages/network-explorer-client/src/Navbar.tsx
+++ packages/network-explorer-client/src/Navbar.tsx
@@ -2,7 +2,9 @@ function Navbar() {
 	return (
 		<div className="flex flex-row gap-10 pt-5">
 			<div className="font-bold">Canvas Explorer</div>
-			<div>Applications</div>
+			<a href="/">
+				<div>Applications</div>
+			</a>
 		</div>
 	)
 }
