6903d3a882304a7d23e9eb01f854971e64b6611d
Raymond Z <raykyri@gmail.com>
2024-05-17 17:54:23 -0700

    fix executablepath for mac

--- packages/probe-server/src/server.ts
+++ packages/probe-server/src/server.ts
@@ -47,9 +47,8 @@ server.listen(port, "::", () => {
 const checkConnections = async () => {
 	const clientJs = fs.readFileSync("./lib/bundle-compiled.js", { encoding: "utf8" })
 	const browser = await puppeteer.launch({
-		executablePath: "/usr/bin/google-chrome",
 		dumpio: true,
-		// headless: "new",
+		headless: true,
 		args: [
 			"--no-sandbox",
 			"--disable-setuid-sandbox",
