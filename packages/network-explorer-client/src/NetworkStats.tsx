6802ccdb5e7481f6d1eefd50ce0a633dca62c4f1
Bob Webb <bob.wbb@gmail.com>
2024-05-24 12:21:10 +0100

    display canvas version in app

--- packages/network-explorer-client/src/NetworkStats.tsx
+++ packages/network-explorer-client/src/NetworkStats.tsx
@@ -1,5 +1,6 @@
 import useSWR from "swr"
 import { fetchAndIpldParseJson } from "./utils"
+import { version } from "../package.json"
 
 export function NetworkStats() {
 	const { data } = useSWR(
@@ -18,7 +19,7 @@ export function NetworkStats() {
 		<div className="flex flex-row bg-white rounded-lg drop-shadow p-3 gap-3">
 			<div>
 				<div className="font-bold">Network Status</div>
-				<div>Explorer is online, running node v0.9.1</div>
+				<div>Explorer is online, running Canvas version v{version}</div>
 			</div>
 			<div>
 				<div>Observed Actions</div>
