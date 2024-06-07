769ba1f9f1ca869a36a7d379f5393c2c29b9fdd2
Raymond Z <raykyri@gmail.com>
2024-06-06 20:06:04 -0400

    use any type for action functions, wire up action calls and implementations

--- packages/replicated/test/replicated.test.ts
+++ packages/replicated/test/replicated.test.ts
@@ -1,14 +1,33 @@
 import test from "ava"
-import { Replicated as ReplicatedObject } from "@canvas-js/replicated"
+import { ReplicatedObject } from "@canvas-js/replicated"
+import { Awaitable } from "@canvas-js/interfaces"
 
-class Chat extends ReplicatedObject {
-	static db = {}
-	onSendMessage(message: string) {}
+class Chat extends ReplicatedObject<{
+	sendMessage: (message: string) => Awaitable<void>
+}> {
+	static db = {
+		messages: {
+			id: "primary",
+			message: "string",
+			address: "string",
+			timestamp: "string",
+		},
+	}
+	async onSendMessage(message: string): Promise<void> {
+		this.db.set("messages", { id: this.id, message, address: this.address, timestamp: this.timestamp })
+	}
 }
 
-test.serial("do nothing", async (t) => {
+test.serial("send a message", async (t) => {
 	const chat = new Chat()
 	await chat.ready()
+
+	const app = await chat.getApp()
+
+	await chat.sendMessage("hi")
+	t.log("count", await app?.db.count("messages"))
+
+	await chat.sendMessage("hello")
 
 	chat.stop()
 	t.pass()
