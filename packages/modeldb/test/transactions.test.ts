41f069bf550f0d3c1b5d1710ed74f056a9468b43
Raymond Z <raykyri@gmail.com>
2024-06-08 17:10:29 -0400

    packages/modeldb: more detailed typeerrors

--- packages/modeldb/test/transactions.test.ts
+++ packages/modeldb/test/transactions.test.ts
@@ -11,7 +11,7 @@ testOnModelDB("apply should roll back partially performed updates if it fails", 
 				// invalid operation
 				{ operation: "set", model: "message", value: { id: "b", content: 1284 } },
 			]),
-		{ message: "message/content must be a string" },
+		{ message: "write to db.message.content: expected a string, received a number" },
 	)
 
 	// apply should have rolled back after the second operation failed, so the database should be empty
