0441970fdfbe0c0a3416d1330659c83af574c313
Joel Gustafson <joelg@mit.edu>
2024-06-10 08:11:03 -0400

    packages/signatures: remove unused import

--- packages/signatures/src/targets/index.ts
+++ packages/signatures/src/targets/index.ts
@@ -1,5 +1,3 @@
-import type { Action, Session, SignatureScheme, Signer } from "@canvas-js/interfaces"
-
 export interface PlatformTarget {
 	get(key: string): string | null
 	set(key: string, value: string): void
