03d709b445e0171ecec63617ff5bef950ead7310
Joel Gustafson <joelg@mit.edu>
2024-06-19 13:25:32 -0400

    packages/test-network: fix ServiceMap typings in src/relay

--- packages/test-network/src/relay/libp2p.ts
+++ packages/test-network/src/relay/libp2p.ts
@@ -1,5 +1,5 @@
 import { createLibp2p } from "libp2p"
-import { circuitRelayServer } from "@libp2p/circuit-relay-v2"
+import { CircuitRelayService, circuitRelayServer } from "@libp2p/circuit-relay-v2"
 import { webSockets } from "@libp2p/websockets"
 import { all } from "@libp2p/websockets/filters"
 import { yamux } from "@chainsafe/libp2p-yamux"
@@ -13,19 +13,21 @@ import { listen, announce, getPeerId } from "./config.js"
 export async function getLibp2p() {
 	const peerId = await getPeerId()
 
-	return await createLibp2p<{ identify: Identify; fetch: Fetch; ping: PingService }>({
-		peerId: peerId,
-		start: false,
-		addresses: { listen, announce },
-		transports: [webSockets({ filter: all })],
+	return await createLibp2p<{ identify: Identify; circuitRelay: CircuitRelayService; fetch: Fetch; ping: PingService }>(
+		{
+			peerId: peerId,
+			start: false,
+			addresses: { listen, announce },
+			transports: [webSockets({ filter: all })],
 
-		streamMuxers: [yamux()],
-		connectionEncryption: [plaintext({})],
-		services: {
-			identify: identify({ protocolPrefix: "canvas" }),
-			circuitRelay: circuitRelayServer(),
-			fetch: fetch({ protocolPrefix: "canvas" }),
-			ping: ping({ protocolPrefix: "canvas" }),
+			streamMuxers: [yamux()],
+			connectionEncryption: [plaintext({})],
+			services: {
+				identify: identify({ protocolPrefix: "canvas" }),
+				circuitRelay: circuitRelayServer(),
+				fetch: fetch({ protocolPrefix: "canvas" }),
+				ping: ping({ protocolPrefix: "canvas" }),
+			},
 		},
-	})
+	)
 }
