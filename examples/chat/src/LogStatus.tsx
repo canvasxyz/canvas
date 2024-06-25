e1f6c2f361009027072bbd45e94ba2449e5f13ae
Joel Gustafson <joelg@mit.edu>
2024-06-25 00:32:49 -0400

    examples/chat: switch GossipSubEvents import to CanvasEvents importg

--- examples/chat/src/LogStatus.tsx
+++ examples/chat/src/LogStatus.tsx
@@ -1,7 +1,7 @@
 import React, { useContext, useEffect, useState } from "react"
 import { bytesToHex } from "@noble/hashes/utils"
 
-import type { GossipLogEvents } from "@canvas-js/gossiplog"
+import type { CanvasEvents } from "@canvas-js/core"
 
 import { AppContext } from "./AppContext.js"
 
@@ -20,7 +20,7 @@ export const LogStatus: React.FC<LogStatusProps> = ({}) => {
 		app.messageLog.tree.read((txn) => txn.getRoot()).then((root) => setRoot(`${root.level}:${bytesToHex(root.hash)}`))
 		app.db.query<{ id: string }>("$heads").then((records) => setHeads(records.map((record) => record.id)))
 
-		const handleCommit = ({ detail: { root, heads } }: GossipLogEvents["commit"]) => {
+		const handleCommit = ({ detail: { root, heads } }: CanvasEvents["commit"]) => {
 			const rootValue = `${root.level}:${bytesToHex(root.hash)}`
 			setRoot(rootValue)
 			setHeads(heads)
