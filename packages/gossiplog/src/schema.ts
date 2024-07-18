0c961a44592c9a4d531ed750d5a1b5b5405495ff
Raymond Z <raykyri@gmail.com>
2024-07-18 11:20:18 -0400

    packages/gossiplog: export SignedMessage, remove duplicated helpers

--- packages/gossiplog/src/schema.ts
+++ packages/gossiplog/src/schema.ts
@@ -73,22 +73,3 @@ export function getNextClock(parents: Uint8Array[]): number {
 
 	return max + 1
 }
-
-// // keys are made by concatenating a **reverse** unsigned varint clock with the hash
-// // and truncating to 20 bytes to be base32-friendly, e.g "05vj050kb09l7okead3vvi6so7c7tunn"
-// export const KEY_LENGTH = 20
-// export const ID_LENGTH = 32
-// export const MIN_MESSAGE_ID = "00000000000000000000000000000000"
-// export const MAX_MESSAGE_ID = "vvvvvvvvvvvvvvvvvvvvvvvvvvvvvvvv"
-// export const messageIdPattern = /^[0123456789abcdefghijklmnopqrstuv]{32}$/
-
-// export function getKey(clock: number, value: Uint8Array): Uint8Array {
-// 	const hash = sha256(value)
-// 	const key = new Uint8Array(KEY_LENGTH)
-// 	const encodingLength = encodeClock(key, clock)
-// 	key.set(hash.subarray(0, KEY_LENGTH - encodingLength), encodingLength)
-// 	return key
-// }
-
-// export const encodeId = (id: string) => base32hex.baseDecode(id)
-// export const decodeId = (key: Uint8Array) => base32hex.baseEncode(key)
