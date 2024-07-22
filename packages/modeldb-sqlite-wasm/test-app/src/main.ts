f4743cd7f1e2daf635c55a161c377fcdfa5fad45
Bob Webb <bob.wbb@gmail.com>
2024-07-22 14:59:51 +0100

    add more tests for creating modeldbs

--- packages/modeldb-sqlite-wasm/test-app/src/main.ts
+++ packages/modeldb-sqlite-wasm/test-app/src/main.ts
@@ -22,44 +22,76 @@ function done(res: any) {
 
 const startButton = document.getElementById("start")!
 startButton.onclick = async () => {
+	const tests = [
+		test_create_modeldb_no_models,
+		test_create_modeldb_model_valid_fields,
+		test_create_modeldb_model_invalid_fields,
+	]
 	try {
-		const db = await OpfsModelDB.initialize({
-			worker: new DBWorker(),
-			path: "test.db",
-			models: {
-				foo: {
-					id: "primary",
-					exampleStr: "string",
-					exampleBool: "boolean",
-					exampleInt: "integer",
-					exampleFloat: "float",
-					exampleJson: "json",
-					exampleBytes: "bytes",
-				},
-			},
-		})
-
-		const id = nanoid()
-
-		const jsonValue = { foo: "a", bar: "b", qux: null, baz: 0.3 }
-		await db.set("foo", {
-			id,
-			exampleStr: "hello world",
-			exampleBool: true,
-			exampleInt: -1,
-			exampleFloat: 0.1,
-			exampleJson: jsonValue,
-			exampleBytes: new Uint8Array([0, 255]),
-		})
-
-		const result = await db.get("foo", id)
-		console.log(result)
-
-		db.close()
+		for (const test of tests) {
+			await test()
+		}
 
 		done({ done: true })
 	} catch (error: any) {
 		console.log(error)
 		done({ error: error.message, stack: error.stack })
+	}
+}
+
+async function test_create_modeldb_no_models() {
+	const db = await OpfsModelDB.initialize({
+		worker: new DBWorker(),
+		path: `${nanoid()}.db`,
+		models: {},
+	})
+	db.close()
+}
+
+async function test_create_modeldb_model_valid_fields() {
+	const db = await OpfsModelDB.initialize({
+		worker: new DBWorker(),
+		path: `${nanoid()}.db`,
+		models: {
+			room: {
+				id: "primary",
+				name: "string",
+				isModerator: "boolean",
+				creator: "@user",
+				members: "@user[]",
+			},
+		},
+	})
+	db.close()
+}
+
+async function test_create_modeldb_model_invalid_fields() {
+	let exceptionThrown = false
+
+	let db = null
+	try {
+		db = await OpfsModelDB.initialize({
+			worker: new DBWorker(),
+			path: `${nanoid()}.db`,
+			models: {
+				room: {
+					// @ts-ignore
+					name: "unsupported",
+				},
+			},
+		})
+	} catch (e: any) {
+		exceptionThrown = true
+		if (e.message !== `error defining room: invalid property "unsupported"`) {
+			throw new Error(
+				`Expected error message to be "error defining room: invalid property 'unsupported'", but got "${e.message}"`,
+			)
+		}
+	} finally {
+		if (db) db.close()
+	}
+
+	if (!exceptionThrown) {
+		throw new Error("Expected an exception to be thrown, but none was thrown")
 	}
 }
