2a89018581455a48df8fbd29a799b8536c21837f
Bob Webb <bob.wbb@gmail.com>
2024-07-23 14:37:18 +0100

    add relations test

--- packages/modeldb-sqlite-wasm/test-app/src/main.ts
+++ packages/modeldb-sqlite-wasm/test-app/src/main.ts
@@ -127,6 +127,10 @@ startButton.onclick = async () => {
 		test_query_indexed_where,
 		test_query_indexed_order_by,
 		test_query_no_query_json_fields,
+		test_relations_set_get_reference_and_relation_values,
+		test_relations_select_reference_relation_values,
+		test_relations_query_reference_values,
+		test_relations_query_filtering_on_relation_values,
 	]
 	const results: any = {}
 	let suitePassed = true
@@ -643,4 +647,180 @@ async function test_query_no_query_json_fields() {
 	expectThrown(async () => {
 		await db.query("user", { where: { metadata: "something" } })
 	}, "")
+}
+
+async function test_relations_set_get_reference_and_relation_values() {
+	const db = await OpfsModelDB.initialize({
+		worker: new DBWorker(),
+		path: `${nanoid()}.db`,
+		models: {
+			user: { address: "primary" },
+			room: {
+				id: "primary",
+				creator: "@user",
+				members: "@user[]",
+			},
+		},
+	})
+
+	await db.set("user", { address: "a" })
+	await db.set("user", { address: "b" })
+
+	assertIs(await db.count("user"), 2)
+
+	const roomId = nanoid()
+	await db.set("room", { id: roomId, creator: "a", members: ["a", "b"] })
+	assertDeepEqual(await db.get("room", roomId), { id: roomId, creator: "a", members: ["a", "b"] })
+}
+
+async function test_relations_select_reference_relation_values() {
+	const db = await OpfsModelDB.initialize({
+		worker: new DBWorker(),
+		path: `${nanoid()}.db`,
+		models: {
+			user: { address: "primary" },
+			room: {
+				id: "primary",
+				creator: "@user",
+				members: "@user[]",
+			},
+		},
+	})
+
+	await db.set("user", { address: "a" })
+	await db.set("user", { address: "b" })
+	await db.set("user", { address: "c" })
+	await db.set("room", { id: "x", creator: "a", members: ["a", "b"] })
+	await db.set("room", { id: "y", creator: "b", members: ["b", "c"] })
+	await db.set("room", { id: "z", creator: "a", members: ["a", "c"] })
+
+	assertDeepEqual(await db.query("room", { select: { id: true, creator: true } }), [
+		{ id: "x", creator: "a" },
+		{ id: "y", creator: "b" },
+		{ id: "z", creator: "a" },
+	])
+
+	assertDeepEqual(await db.query("room", { select: { id: true, members: true } }), [
+		{ id: "x", members: ["a", "b"] },
+		{ id: "y", members: ["b", "c"] },
+		{ id: "z", members: ["a", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { select: { id: true, creator: true, members: true } }), [
+		{ id: "x", creator: "a", members: ["a", "b"] },
+		{ id: "y", creator: "b", members: ["b", "c"] },
+		{ id: "z", creator: "a", members: ["a", "c"] },
+	])
+}
+
+async function test_relations_query_reference_values() {
+	const db = await OpfsModelDB.initialize({
+		worker: new DBWorker(),
+		path: `${nanoid()}.db`,
+		models: {
+			user: { address: "primary" },
+			room: {
+				id: "primary",
+				creator: "@user",
+				members: "@user[]",
+			},
+		},
+	})
+
+	await db.set("user", { address: "a" })
+	await db.set("user", { address: "b" })
+	await db.set("user", { address: "c" })
+	await db.set("room", { id: "x", creator: "a", members: ["a", "b"] })
+	await db.set("room", { id: "y", creator: "b", members: ["b", "c"] })
+	await db.set("room", { id: "z", creator: "a", members: ["a", "c"] })
+
+	assertDeepEqual(await db.query("room", { where: { creator: "a" } }), [
+		{ id: "x", creator: "a", members: ["a", "b"] },
+		{ id: "z", creator: "a", members: ["a", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { creator: "b" } }), [{ id: "y", creator: "b", members: ["b", "c"] }])
+	assertDeepEqual(await db.query("room", { where: { creator: "c" } }), [])
+	assertDeepEqual(await db.query("room", { where: { creator: { neq: "a" } } }), [
+		{ id: "y", creator: "b", members: ["b", "c"] },
+	])
+}
+
+async function test_relations_query_filtering_on_relation_values() {
+	const db = await OpfsModelDB.initialize({
+		worker: new DBWorker(),
+		path: `${nanoid()}.db`,
+		models: {
+			user: { address: "primary" },
+			room: {
+				id: "primary",
+				creator: "@user",
+				members: "@user[]",
+			},
+		},
+	})
+
+	await db.set("user", { address: "a" })
+	await db.set("user", { address: "b" })
+	await db.set("user", { address: "c" })
+	await db.set("room", { id: "x", creator: "a", members: ["a", "b"] })
+	await db.set("room", { id: "y", creator: "b", members: ["b", "c"] })
+	await db.set("room", { id: "z", creator: "a", members: ["a", "c"] })
+
+	assertDeepEqual(await db.query("room", { where: { members: ["a"] } }), [
+		{ id: "x", creator: "a", members: ["a", "b"] },
+		{ id: "z", creator: "a", members: ["a", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: ["b"] } }), [
+		{ id: "x", creator: "a", members: ["a", "b"] },
+		{ id: "y", creator: "b", members: ["b", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: ["c"] } }), [
+		{ id: "y", creator: "b", members: ["b", "c"] },
+		{ id: "z", creator: "a", members: ["a", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: ["a", "b"] } }), [
+		{ id: "x", creator: "a", members: ["a", "b"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: ["b", "a"] } }), [
+		{ id: "x", creator: "a", members: ["a", "b"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: ["b", "c"] } }), [
+		{ id: "y", creator: "b", members: ["b", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: ["c", "b"] } }), [
+		{ id: "y", creator: "b", members: ["b", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: ["a", "c"] } }), [
+		{ id: "z", creator: "a", members: ["a", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: ["c", "a"] } }), [
+		{ id: "z", creator: "a", members: ["a", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: ["a", "b", "c"] } }), [])
+
+	assertDeepEqual(await db.query("room", { where: { members: { neq: ["a"] } } }), [
+		{ id: "y", creator: "b", members: ["b", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: { neq: ["b"] } } }), [
+		{ id: "z", creator: "a", members: ["a", "c"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: { neq: ["c"] } } }), [
+		{ id: "x", creator: "a", members: ["a", "b"] },
+	])
+
+	assertDeepEqual(await db.query("room", { where: { members: { neq: ["a", "b"] } } }), [])
+	assertDeepEqual(await db.query("room", { where: { members: { neq: ["b", "c"] } } }), [])
+	assertDeepEqual(await db.query("room", { where: { members: { neq: ["a", "c"] } } }), [])
 }
