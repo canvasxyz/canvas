3340805aaafab3efd2b38a0b583cd120772bba32
Bob Webb <bob.wbb@gmail.com>
2024-07-23 18:30:51 +0100

    update worker code in chat app to include logger

--- examples/chat/worker.js
+++ examples/chat/worker.js
@@ -2,10 +2,10 @@ import * as Comlink from "comlink"
 import sqlite3InitModule from "@sqlite.org/sqlite-wasm"
 import { InnerModelDB } from "@canvas-js/modeldb-sqlite-wasm"
 
-export async function initialize(origin, path, config) {
+export async function initialize(path, config, log) {
 	const sqlite3 = await sqlite3InitModule({ print: console.log, printErr: console.error })
 	const db = new sqlite3.oo1.OpfsDb(path)
-	return Comlink.proxy(new InnerModelDB(db, config))
+	return Comlink.proxy(new InnerModelDB(db, config, log))
 }
 
 Comlink.expose(initialize)
