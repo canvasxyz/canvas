05c922d4e5e585507498be36337b4939f2e4af6e
Bob Webb <bob.wbb@gmail.com>
2024-07-23 19:49:16 +0100

    fix log type in worker.ts

--- packages/modeldb-sqlite-wasm/test-app/src/worker.ts
+++ packages/modeldb-sqlite-wasm/test-app/src/worker.ts
@@ -1,9 +1,10 @@
+import { Logger } from "@libp2p/logger"
 import * as Comlink from "comlink"
 import sqlite3InitModule from "@sqlite.org/sqlite-wasm"
 import { InnerModelDB } from "@canvas-js/modeldb-sqlite-wasm"
 import { Config } from "@canvas-js/modeldb"
 
-export async function initialize(path: string, config: Config, log: (message: string) => void) {
+export async function initialize(path: string, config: Config, log: Logger) {
 	const sqlite3 = await sqlite3InitModule({
 		print: console.log,
 		printErr: console.error,
