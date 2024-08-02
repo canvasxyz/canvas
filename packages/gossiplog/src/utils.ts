5335032239cf05e7ac25b4391d286892ac65c7f9
Joel Gustafson <joelg@mit.edu>
2024-08-02 14:34:41 -0400

    packages/gossiplog: remove DelayableController from src/utils.ts

--- packages/gossiplog/src/utils.ts
+++ packages/gossiplog/src/utils.ts
@@ -35,30 +35,3 @@ export async function wait(interval: number, options: { signal: AbortSignal }) {
 		signal.addEventListener("abort", () => resolve())
 	}).finally(() => signal.clear())
 }
-
-export class DelayableController {
-	#interval: number
-	#controller: AbortController
-	#timer: ReturnType<typeof setTimeout>
-	signal: AbortSignal
-
-	constructor(interval: number) {
-		this.#interval = interval
-		this.#controller = new AbortController()
-		this.signal = this.#controller.signal
-		this.#timer = setTimeout(() => {
-			this.#controller.abort()
-		}, this.#interval)
-	}
-
-	delay() {
-		clearTimeout(this.#timer)
-		this.#timer = setTimeout(() => {
-			this.#controller.abort()
-		}, this.#interval)
-	}
-
-	clear() {
-		clearTimeout(this.#timer)
-	}
-}
