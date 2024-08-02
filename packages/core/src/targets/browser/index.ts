b8e411e8b8e77a144942458677fbb049851f437d
Joel Gustafson <joelg@mit.edu>
2024-08-02 14:35:38 -0400

    packages/core: use idb and libp2p/browser-lite for browser target

--- packages/core/src/targets/browser/index.ts
+++ packages/core/src/targets/browser/index.ts
@@ -1,29 +1,20 @@
 import { AbstractGossipLog } from "@canvas-js/gossiplog"
-import { getLibp2p } from "@canvas-js/gossiplog/libp2p/browser"
-import { GossipLog as SqliteWasmGossipLog } from "@canvas-js/gossiplog/sqlite-wasm"
+import { getLibp2p } from "@canvas-js/gossiplog/libp2p/browser-lite"
 import { GossipLog as IdbGossipLog } from "@canvas-js/gossiplog/idb"
 import { ModelDB as IdbModelDB } from "@canvas-js/modeldb-idb"
-import { ModelDB as SqliteWasmModelDB } from "@canvas-js/modeldb-sqlite-wasm"
 
 import type { PlatformTarget } from "../interface.js"
 
 const target: PlatformTarget = {
-	openDB: async ({ path, topic }, models) => {
-		if (path) {
-			if (typeof path !== "string") throw new Error("Expected path to be a string")
-			return SqliteWasmModelDB.initialize({ path, models: { ...models, ...AbstractGossipLog.schema } })
-		} else {
-			return IdbModelDB.initialize({ name: `canvas/v1/${topic}`, models: { ...models, ...AbstractGossipLog.schema } })
-		}
+	openDB: async ({ topic }, models) => {
+		return IdbModelDB.initialize({
+			name: `canvas/v1/${topic}`,
+			models: { ...models, ...AbstractGossipLog.schema },
+		})
 	},
 
 	openGossipLog: ({ path }, init) => {
-		if (path) {
-			if (typeof path !== "string") throw new Error("Expected path to be a string")
-			return SqliteWasmGossipLog.open(init)
-		} else {
-			return IdbGossipLog.open(init)
-		}
+		return IdbGossipLog.open(init)
 	},
 
 	createLibp2p: (config, messageLog) => getLibp2p(config, messageLog),
