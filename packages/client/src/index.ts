59a5a07eaf49128bfae014bdcd538369f68b6ed3
Joel Gustafson <joelg@mit.edu>
2024-08-07 12:25:57 -0400

    packages/client: format src/index.ts

--- packages/client/src/index.ts
+++ packages/client/src/index.ts
@@ -5,11 +5,7 @@ import type { Action, Message, Session, SessionSigner, Signer } from "@canvas-js
 import { assert } from "@canvas-js/utils"
 
 export class Client {
-	constructor(
-		readonly signer: SessionSigner,
-		readonly host: string,
-		readonly topic: string,
-	) {}
+	constructor(readonly signer: SessionSigner, readonly host: string, readonly topic: string) {}
 
 	public async sendAction(name: string, args: any) {
 		const timestamp = Date.now()
