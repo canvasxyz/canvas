b66421f419cb9ac10ba9da81c9dc68ad03974d81
Joel Gustafson <joelg@mit.edu>
2024-08-26 14:10:35 -0400

    packages/test-network: use multiple puppeteer contexts per process

--- packages/test-network/spawn.sh
+++ packages/test-network/spawn.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-# Number of instances you want to run
-count="${1:-1}"
+# Number of processes
+PROCESS_COUNT="${1:-1}"
 
-echo "starting $count browser peers"
+# Peers per process
+export PEER_COUNT="${2:-10}"
+export DELAY=10
+
+echo "starting $PROCESS_COUNT processes of $PEER_COUNT peers"
 
 # Command to run (replace 'your_command_here' with your actual command)
 command="node lib/peer-browser/entrypoint.js"
@@ -11,7 +15,7 @@ command="node lib/peer-browser/entrypoint.js"
 export BOOTSTRAP_LIST="/dns4/localhost/tcp/8080/ws/p2p/12D3KooWNbCWxWV3Tmu38pEi2hHVUiBHbr7x6bHLFQXRqgui6Vrn"
 # export RELAY_SERVER="/dns4/localhost/tcp/8081/ws/p2p/12D3KooWEaSQnJTxztTHmKmcQkDy3oScjo8ww4JCKig73XH4cmb3"
 
-for i in $(seq 1 $count)
+for i in $(seq 1 $PROCESS_COUNT)
 do
     # Start the command and prefix its output
     (SERVICE_NAME=browser-$i $command) | sed "s/^/[browser-$i] /" &
--- packages/test-network/src/peer-browser/entrypoint.ts
+++ packages/test-network/src/peer-browser/entrypoint.ts
@@ -1,7 +1,7 @@
 import { bytesToHex, randomBytes } from "@noble/hashes/utils"
 import puppeteer from "puppeteer"
 
-const { RELAY_SERVER, BOOTSTRAP_LIST, MIN_CONNECTIONS, MAX_CONNECTIONS } = process.env
+const { RELAY_SERVER, BOOTSTRAP_LIST, MIN_CONNECTIONS, MAX_CONNECTIONS, DELAY, PEER_COUNT } = process.env
 
 const query: Record<string, string> = {}
 
@@ -21,6 +21,16 @@ if (MAX_CONNECTIONS !== undefined) {
 	query.maxConnections = MAX_CONNECTIONS
 }
 
+if (DELAY !== undefined) {
+	query.delay = DELAY
+}
+
+const q = Object.entries(query)
+	.map(([name, value]) => `${name}=${encodeURIComponent(value)}`)
+	.join("&")
+
+const url = `http://localhost:8000/peer-browser/index.html?${q}`
+
 const browser = await puppeteer.launch({
 	userDataDir: `data/${bytesToHex(randomBytes(8))}`,
 	headless: true,
@@ -32,19 +42,21 @@ const browser = await puppeteer.launch({
 	],
 })
 
-const page = await browser.newPage()
-
-page.on("console", (msg) => {
-	console.log(`[${msg.type()}] ${msg.text()}`)
-})
-
-const q = Object.entries(query)
-	.map(([name, value]) => `${name}=${encodeURIComponent(value)}`)
-	.join("&")
-
-await page.goto(`http://localhost:8000/peer-browser/index.html?${q}`)
-
 process.addListener("SIGINT", async () => {
 	process.stdout.write("\nReceived SIGINT\n")
 	browser.close()
 })
+
+const peerCount = parseInt(PEER_COUNT ?? "1")
+for (let i = 0; i < peerCount; i++) {
+	browser
+		.createBrowserContext()
+		.then((context) => context.newPage())
+		.then((page) => {
+			page.on("console", (msg) => {
+				console.log(`[page-${i}] [${msg.type()}] ${msg.text()}`)
+			})
+
+			page.goto(url)
+		})
+}
