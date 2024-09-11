26f4814189978eef719051ba5eed179ca7478705
Bob Webb <bob.wbb@gmail.com>
2024-09-11 14:48:50 +0100

    set correct styling for navbar home link

--- examples/network-explorer/src/components/Navbar.tsx
+++ examples/network-explorer/src/components/Navbar.tsx
@@ -1,12 +1,13 @@
 import { Card } from "@radix-ui/themes"
-import { Link } from "react-router-dom"
+import { Link as ReactRouterLink } from "react-router-dom"
 
 function Navbar() {
 	return (
 		<Card className="flex flex-col gap-5">
-			<Link to={"/"}>
-				<div className="font-bold">🌐 &nbsp;Explorer</div>
-			</Link>
+			{/* --accent-a11 is set by radix-ui, this reflects whatever the accent color has been set to */}
+			<ReactRouterLink to="/" style={{ textDecoration: "unset", color: "var(--accent-a11)" }}>
+				🌐 &nbsp;Explorer
+			</ReactRouterLink>
 		</Card>
 	)
 }
