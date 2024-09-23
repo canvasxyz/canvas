3ec2db0c9516d41430158380f2a849e95367688f
Raymond Z <raykyri@gmail.com>
2024-09-23 17:40:07 -0400

    update install-prod.sh

--- install-prod.sh
+++ install-prod.sh
@@ -6,3 +6,5 @@ echo "#!/usr/bin/env sh" >> ${INSTALL_PATH}
 echo "node ${PWD}/packages/cli/dist/index.js \$@" >> ${INSTALL_PATH}
 chmod +x ${INSTALL_PATH}
 echo "Done! Installed CLI to" $INSTALL_PATH
+
+PATH=$INSTALL_PATH:$PATH
