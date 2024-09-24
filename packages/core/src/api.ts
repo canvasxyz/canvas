201c1c8ba9797a33f64ed3f63c4dde0ab115e588
Joel Gustafson <joelg@mit.edu>
2024-09-24 11:53:45 -0400

    packages/core: revert api refactor

--- packages/core/src/api.ts
+++ packages/core/src/api.ts
@@ -4,9 +4,8 @@ import { StatusCodes } from "http-status-codes"
 import { Counter, Gauge, Summary, Registry, register } from "prom-client"
 
 import * as json from "@ipld/dag-json"
-import ipld from "express-ipld"
 
-import { Signature, Message, Session, Action } from "@canvas-js/interfaces"
+import { createAPI as createBaseAPI } from "@canvas-js/gossiplog/api"
 import { assert } from "@canvas-js/utils"
 
 import { Canvas } from "./Canvas.js"
@@ -14,89 +13,9 @@ import { Canvas } from "./Canvas.js"
 export interface APIOptions {}
 
 export function createAPI(app: Canvas): express.Express {
-	const api = express()
-
-	api.set("query parser", "simple")
+	const api = createBaseAPI(app.messageLog)
 
 	api.get("/", (req, res) => void res.json(app.getApplicationData()))
-
-	api.get("/clock", async (req, res) => {
-		const [clock, parents] = await app.messageLog.getClock()
-		return void res.json({ clock, parents })
-	})
-
-	api.get("/messages/count", async (req, res) => {
-		const count = await app.db.count("$messages")
-		return void res.json({ count })
-	})
-
-	api.get("/messages/:id", async (req, res) => {
-		const { id } = req.params
-
-		const signedMessage = await app.messageLog.get(id)
-		if (signedMessage === null) {
-			return void res.status(StatusCodes.NOT_FOUND).end()
-		}
-
-		const { signature, message } = signedMessage
-		res.writeHead(StatusCodes.OK, { "content-type": "application/json" })
-		return void res.end(json.encode({ id, signature, message }))
-	})
-
-	api.get("/messages", async (req, res) => {
-		const { gt, gte, lt, lte, order, type } = req.query
-
-		assert(gt === undefined || typeof gt === "string", "invalid `gt` query parameter")
-
-		assert(gte === undefined || typeof gte === "string", "invalid `gte` query parameter")
-		assert(lt === undefined || typeof lt === "string", "invalid `lt` query parameter")
-		assert(lte === undefined || typeof lte === "string", "invalid `lte` query parameter")
-
-		let limit = 64
-		if (typeof req.query.limit === "string") {
-			limit = parseInt(req.query.limit)
-		}
-
-		assert(Number.isSafeInteger(limit) && 0 < limit && limit <= 64, "invalid `limit` query parameter")
-		assert(order === undefined || order === "asc" || order === "desc", "invalid `order` query parameter")
-
-		assert(type === undefined || type === "action" || type === "session", "invalid `type` query parameter")
-
-		type MessageRecord = { id: string; signature: Signature; message: Message<Action | Session> }
-		const results: MessageRecord[] = []
-
-		if (limit > 0) {
-			for await (const { id, signature, message } of app.db.iterate<MessageRecord>("$messages", {
-				select: { id: true, signature: true, message: true },
-				where: { id: { gt, gte, lt, lte } },
-				orderBy: { id: order ?? "asc" },
-			})) {
-				if (type === undefined || message.payload.type === type) {
-					const count = results.push({ id, signature, message })
-					if (count >= limit) {
-						break
-					}
-				}
-			}
-		}
-
-		res.writeHead(StatusCodes.OK, { "content-type": "application/json" })
-		return void res.end(json.encode(results))
-	})
-
-	api.post("/messages", ipld(), async (req, res) => {
-		try {
-			const { signature, message }: { signature: Signature; message: Message<Action | Session> } = req.body
-			const signedMessage = app.messageLog.encode(signature, message)
-			await app.messageLog.insert(signedMessage)
-			res.status(StatusCodes.CREATED)
-			res.setHeader("Location", `messages/${signedMessage.id}`)
-			return void res.end()
-		} catch (e) {
-			console.error(e)
-			return void res.status(StatusCodes.BAD_REQUEST).end(`${e}`)
-		}
-	})
 
 	api.get("/sessions", async (req, res) => {
 		const { did, publicKey, minExpiration } = req.query
