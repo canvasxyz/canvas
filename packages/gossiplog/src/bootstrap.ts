3e899d2e1fe4eb12febb9a885de9827ab7d7097b
Joel Gustafson <joelg@mit.edu>
2024-09-26 17:04:16 -0400

    packages/gossiplog: update bootstrap lists

--- packages/gossiplog/src/bootstrap.ts
+++ packages/gossiplog/src/bootstrap.ts
@@ -1,11 +1,11 @@
 export const defaultBootstrapList = [
-	"/dns4/canvas-bootstrap-p0.fly.dev/tcp/443/wss/p2p/12D3KooWMPVqgFJoscoy5zkvPKUVMV2Tq4AMfvNKKB9dbsujm5RC",
-	"/dns4/canvas-bootstrap-p1.fly.dev/tcp/443/wss/p2p/12D3KooWRmA9W9LMEKTvirjajr7YVfwMHdiLtDT76rQnTwksNSXD",
-	"/dns4/canvas-bootstrap-p2.fly.dev/tcp/443/wss/p2p/12D3KooWS3L9Fh5V7z3hN2UwsNZ5xc8DXAeXmsDyFrhudkgea9P8",
+	"/dns4/canvas-bootstrap-p0.fly.dev/tcp/443/wss/p2p/12D3KooWB3X5KnsTMknFLqxMf2979zddpwCWZ5ix1FosV5sFC4py",
+	"/dns4/canvas-bootstrap-p1.fly.dev/tcp/443/wss/p2p/12D3KooWCSsK4VGWx4gA2FiUKQFEHyLrL6bf4xAJm6kqB2gkB59w",
+	"/dns4/canvas-bootstrap-p2.fly.dev/tcp/443/wss/p2p/12D3KooWHWLKtWifVTMwJQRG6jcVCV3kLQ49GCB4DgevLTnKWor3",
 ]
 
 export const testnetBootstrapList = [
-	"/dns4/canvas-testnet-p0.fly.dev/tcp/443/wss/p2p/12D3KooWHjdmy6FPvupg8eCNao1TQFQQvhxmJPTMZLtSiqL7NTQD",
-	"/dns4/canvas-testnet-p1.fly.dev/tcp/443/wss/p2p/12D3KooWQpoAUCAxyLTwVHSATSyQrQEnmfQFXyQcTfwmrmzhQdLT",
-	"/dns4/canvas-testnet-p2.fly.dev/tcp/443/wss/p2p/12D3KooWM6SUtPGyHHwyuMqNAczLG88phJXbUupdn8euwGkojr7V",
+	"/dns4/canvas-testnet-p0.fly.dev/tcp/443/wss/p2p/12D3KooWQoSrJD9g9D1qeLWwUE9JomQeEMDeEmtRRG2znudA9bm8",
+	"/dns4/canvas-testnet-p1.fly.dev/tcp/443/wss/p2p/12D3KooWPWiKt9yrdDBhL9dQ25j4F2VZS12sgGeQYhRn66WBiCmX",
+	"/dns4/canvas-testnet-p2.fly.dev/tcp/443/wss/p2p/12D3KooWFGBSKFVxM5XUvTFcniDpJHUtggY7meTzz9MpoVN72eQy",
 ]
