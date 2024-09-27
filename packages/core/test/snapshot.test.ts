f2554910b47693466df25372c7105fa0597a8f44
Raymond Z <raykyri@gmail.com>
2024-09-27 14:24:54 -0400

    packages/core: test snapshot with merge function

--- packages/core/test/snapshot.test.ts
+++ packages/core/test/snapshot.test.ts
@@ -1,19 +1,10 @@
-import assert from "node:assert"
 import test from "ava"
-import * as json from "@ipld/dag-json"
-import Prando from "prando"
 
-import { ethers } from "ethers"
-
-import { SIWESigner } from "@canvas-js/chain-ethereum"
 import { Canvas, Contract, createSnapshot } from "@canvas-js/core"
-
-const rng = new Prando.default()
-
-const random = (n: number) => rng.nextInt(0, n - 1)
+import { Action, Session, Message } from "@canvas-js/interfaces";
+import { SIWESigner } from "@canvas-js/chain-ethereum";
 
 test("snapshot persists data across apps", async (t) => {
-	const wallet = ethers.Wallet.createRandom()
 	const contract = {
 		models: {
 			posts: {
@@ -41,7 +32,7 @@ test("snapshot persists data across apps", async (t) => {
 	const [clock0, parents0] = await app.messageLog.getClock()
 	t.is(clock0, 1)
 	t.is(parents0.length, 0)
-	
+
 	await app.actions.createPost({ id: "a", content: "foo" })
 	await app.actions.createPost({ id: "b", content: "bar" })
 	await app.actions.createPost({ id: "c", content: "qux" })
@@ -96,4 +87,83 @@ test("snapshot persists data across apps", async (t) => {
 	const [clock3, parents3] = await app3.messageLog.getClock()
 	t.is(clock3, 2) // one snapshot
 	t.is(parents2.length, 1)
+})
+
+test("snapshot persists data with merge functions", async (t) => {
+	const contract = {
+		models: {
+			posts: {
+				id: "primary",
+				content: "string",
+				$merge: ({ id, content }, { id: id2, content: content2 }) => {
+					return { id, content: content + content2 }
+				},
+			},
+		},
+		actions: {
+			async createPost(db, { id, content }: { id: string; content: string }) {
+				console.log(id, content)
+				await db.set("posts", { id, content })
+			},
+			async deletePost(db, { id }: { id: string }) {
+				await db.delete("posts", id)
+			},
+		},
+	} satisfies Contract
+
+	const config = {
+		topic: "com.example.app",
+		contract,
+	}
+
+	const app = await Canvas.initialize(config)
+
+	const session = await app.signers.getFirst().newSession(config.topic)
+	const sessionMessage: Message<Session> = {
+		topic: config.topic,
+		clock: 1,
+		parents: [],
+		payload: session.payload
+	}
+	const sessionMessageSignature = await session.signer.sign(sessionMessage)
+	await app.insert(sessionMessageSignature, sessionMessage)
+
+	const a: Message<Action> = {
+		topic: config.topic,
+		clock: 1,
+		parents: [],
+		payload: {
+			type: "action",
+			did: session.payload.did,
+			name: "createPost",
+			args: { id: "a", content: "foo", },
+			context: { timestamp: new Date().getTime() }
+		}
+	}
+	const { id: idA } = await app.insert(await session.signer.sign(a), a)
+
+	const b: Message<Action> = {
+		topic: config.topic,
+		clock: 1,
+		parents: [],
+		payload: {
+			type: "action",
+			did: session.payload.did,
+			name: "createPost",
+			args: { id: "a", content: "foo", },
+			context: { timestamp: new Date().getTime() }
+		}
+	}
+	const { id: idB } = await app.insert(await session.signer.sign(b), b)
+
+	t.deepEqual(await app.db.get("posts", "a"), { id: "a", content: "foofoo" })
+
+	const snapshot = await createSnapshot(app)
+	await app.stop()
+
+	const app2 = await Canvas.initialize({ reset: true, snapshot, ...config })
+
+	t.deepEqual(await app2.db.get("posts", "a"), { id: "a", content: "foofoo" })
+
+	t.pass()
 })
