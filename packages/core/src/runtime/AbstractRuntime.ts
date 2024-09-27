1a8d942780dea4c7849b2aded7e6be96035e03d7
Joel Gustafson <joelg@mit.edu>
2024-09-27 14:38:06 -0400

    packages/core: assert that sessions are direct ancestors of actions

--- packages/core/src/runtime/AbstractRuntime.ts
+++ packages/core/src/runtime/AbstractRuntime.ts
@@ -184,11 +184,25 @@ export abstract class AbstractRuntime {
 
 		const address = signer.getAddressFromDid(did)
 
-		const sessions = await this.db.query<{ id: string; expiration: number | null }>("$sessions", {
+		const sessions = await this.db.query<{ message_id: string; expiration: number | null }>("$sessions", {
 			where: { public_key: signature.publicKey, did: did },
 		})
 
-		if (sessions.every(({ expiration }) => expiration !== null && expiration < context.timestamp)) {
+		const activeSessions = sessions.filter(({ expiration }) => expiration === null || expiration > context.timestamp)
+
+		let sessionId: string | null = null
+		for (const { message_id } of activeSessions) {
+			const visited = new Set<string>()
+			for (const parentId of message.parents) {
+				const isAncestor = await messageLog.isAncestor(parentId, message_id, visited)
+				if (isAncestor) {
+					sessionId = message_id
+					break
+				}
+			}
+		}
+
+		if (sessionId === null) {
 			throw new Error(`missing session ${signature.publicKey} for ${did}`)
 		}
 
