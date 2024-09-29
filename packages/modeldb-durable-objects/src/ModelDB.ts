4fdb88fd2ec05d16b62c47a4f710f35b9cf14a44
Raymond Z <raykyri@gmail.com>
2024-09-29 06:16:07 -0400

    packages/modeldb-durable-objects: wip

---
+++ packages/modeldb-durable-objects/src/ModelDB.ts
@@ -0,0 +1,106 @@
+import { assert } from "@canvas-js/utils"
+
+import {
+	AbstractModelDB,
+	parseConfig,
+	Effect,
+	ModelValue,
+	ModelSchema,
+	QueryParams,
+	WhereCondition,
+} from "@canvas-js/modeldb"
+
+import { SqlStorage } from "@cloudflare/workers-types"
+
+import { ModelAPI } from "./api.js"
+
+export interface ModelDBOptions {
+	db: SqlStorage
+	models: ModelSchema
+}
+
+export class ModelDB extends AbstractModelDB {
+	public readonly db: SqlStorage
+
+	#models: Record<string, ModelAPI> = {}
+	// #transaction: sqlite.Transaction<(effects: Effect[]) => void>
+
+	constructor({ db, models }: ModelDBOptions) {
+		super(parseConfig(models))
+		this.db = db
+
+		for (const model of Object.values(this.models)) {
+			this.#models[model.name] = new ModelAPI(this.db, model)
+		}
+
+		// this.#transaction = this.db.transaction((effects) => {
+		// 	for (const effect of effects) {
+		// 		const model = this.models[effect.model]
+		// 		assert(model !== undefined, `model ${effect.model} not found`)
+		// 		if (effect.operation === "set") {
+		// 			this.#models[effect.model].set(effect.value)
+		// 		} else if (effect.operation === "delete") {
+		// 			this.#models[effect.model].delete(effect.key)
+		// 		} else {
+		// 			signalInvalidType(effect)
+		// 		}
+		// 	}
+		// })
+	}
+
+	public async close() {
+		this.log("closing")
+	}
+
+	public async apply(effects: Effect[]) {
+		// this.#transaction(effects)
+
+		for (const { model, query, filter, callback } of this.subscriptions.values()) {
+			if (effects.some(filter)) {
+				const api = this.#models[model]
+				assert(api !== undefined, `model ${model} not found`)
+				try {
+					callback(api.query(query))
+				} catch (err) {
+					this.log.error(err)
+				}
+			}
+		}
+	}
+
+	public async get<T extends ModelValue<any> = ModelValue<any>>(modelName: string, key: string): Promise<T | null> {
+		const api = this.#models[modelName]
+		assert(api !== undefined, `model ${modelName} not found`)
+		return api.get(key) as T | null
+	}
+
+	public async *iterate<T extends ModelValue<any> = ModelValue<any>>(
+		modelName: string,
+		query: QueryParams = {},
+	): AsyncIterable<T> {
+		const api = this.#models[modelName]
+		assert(api !== undefined, `model ${modelName} not found`)
+		yield* api.iterate(query) as Iterable<T>
+	}
+
+	public async count(modelName: string, where?: WhereCondition): Promise<number> {
+		const api = this.#models[modelName]
+		assert(api !== undefined, `model ${modelName} not found`)
+		return api.count(where)
+	}
+
+	public async clear(modelName: string): Promise<void> {
+		const api = this.#models[modelName]
+		assert(api !== undefined, `model ${modelName} not found`)
+		return api.clear()
+	}
+
+	public async query<T extends ModelValue<any> = ModelValue<any>>(
+		modelName: string,
+		query: QueryParams = {},
+	): Promise<T[]> {
+		const api = this.#models[modelName]
+		assert(api !== undefined, `model ${modelName} not found`)
+		return api.query(query) as T[]
+	}
+}
---
+++ packages/modeldb-durable-objects/src/utils.ts
@@ -0,0 +1,32 @@
+import { SqlStorage } from "@cloudflare/workers-types"
+
+export class Query<P, R> {
+	constructor(
+		private readonly db: SqlStorage,
+		private readonly sql: string,
+	) {}
+
+	public get(params: P): R | null {
+		const result = this.db.exec(this.sql, params).one() as R | undefined
+		return result ?? null
+	}
+
+	public all(params: P): R[] {
+		return this.db.exec(this.sql, params).toArray() as R[]
+	}
+
+	public iterate(params: P): IterableIterator<R> {
+		return this.db.exec(this.sql, params) as IterableIterator<R>
+	}
+}
+
+export class Method<P> {
+	constructor(
+		private readonly db: SqlStorage,
+		private readonly sql: string,
+	) {}
+
+	public run(params: P) {
+		this.db.exec(this.sql, params)
+	}
+}
