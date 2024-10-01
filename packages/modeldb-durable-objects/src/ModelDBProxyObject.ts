6cf51879b973b30d5c4d3cf9fef826cadee8a01e
Raymond Z <raykyri@gmail.com>
2024-10-01 14:44:36 -0400

    packages/modeldb-durable-objects: pass back errors

--- packages/modeldb-durable-objects/src/ModelDBProxyObject.ts
+++ packages/modeldb-durable-objects/src/ModelDBProxyObject.ts
@@ -33,34 +33,40 @@ export class ModelDBProxyObject {
 		const [root, objectId, call] = url.pathname.split("/")
 		const args = call === "fetchSubscriptions" ? null : json.decode(await request.bytes())
 
-		if (call === "initialize") {
-			const [modelSchema] = args as ModelSchema[]
-			this.initialize(modelSchema)
-			return new Response(json.stringify({ status: "Success" }))
-		} else if (call === "iterate") {
-			if (!this.db) throw new Error("uninitialized")
-			const iterateArgs = args as [string, QueryParams | undefined]
-			const result = []
-			for await (const item of this.db.iterate.apply(this.db, iterateArgs)) {
-				result.push(item)
+		try {
+			if (call === "initialize") {
+				const [modelSchema] = args as ModelSchema[]
+				this.initialize(modelSchema)
+				return new Response(json.stringify({ status: "Success" }))
+			} else if (call === "iterate") {
+				if (!this.db) throw new Error("uninitialized")
+				const iterateArgs = args as [string, QueryParams | undefined]
+				const result = []
+				for await (const item of this.db.iterate.apply(this.db, iterateArgs)) {
+					result.push(item)
+				}
+				return new Response(json.stringify(result))
+			} else if (call === "subscribe") {
+				if (!this.db) throw new Error("uninitialized")
+				const [modelName, query, subscriptionId] = args as [modelName: string, query: QueryParams, subscriptionId: number]
+				this.db.subscribe(modelName, query, (results: ModelValue[]) => {
+					this.subscriptionResults.push({ subscriptionId, results })
+				})
+				return new Response(json.stringify(this.subscriptionResults))
+			} else if (call === "fetchSubscriptions") {
+				const result = json.stringify(this.subscriptionResults)
+				this.subscriptionResults = []
+				return new Response(result)
+			} else {
+				if (!this.db) throw new Error("uninitialized")
+				const callFn = (this.db as any)[call] as Function
+				const result = await callFn.apply(this.db, args)
+				return new Response(json.stringify(result ?? null))
 			}
-			return new Response(json.stringify(result))
-		} else if (call === "subscribe") {
-			if (!this.db) throw new Error("uninitialized")
-			const [modelName, query, subscriptionId] = args as [modelName: string, query: QueryParams, subscriptionId: number]
-			this.db.subscribe(modelName, query, (results: ModelValue[]) => {
-				this.subscriptionResults.push({ subscriptionId, results })
-			})
-			return new Response(json.stringify(this.subscriptionResults))
-		} else if (call === "fetchSubscriptions") {
-			const result = json.stringify(this.subscriptionResults)
-			this.subscriptionResults = []
-			return new Response(result)
-		} else {
-			if (!this.db) throw new Error("uninitialized")
-			const callFn = (this.db as any)[call] as Function
-			const result = await callFn.apply(this.db, args)
-			return new Response(json.stringify(result ?? null))
+		} catch (err) {
+			const errorMessage = err instanceof Error ? err.message : err
+			const errorMessageString = (errorMessage as any).toString()
+			return new Response(errorMessageString, { status: 400 });
 		}
 	}
 }
