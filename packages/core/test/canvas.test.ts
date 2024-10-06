b83c39072a99d0231abd79cad4d825bbede76984
Raymond Z <raykyri@gmail.com>
2024-10-06 18:43:23 -0400

    packages/core: factor out ipld test

--- packages/core/test/canvas.test.ts
+++ packages/core/test/canvas.test.ts
@@ -3,7 +3,6 @@ import { randomUUID } from "node:crypto"
 import test, { ExecutionContext } from "ava"
 
 import { ethers } from "ethers"
-import pg from "pg"
 
 import type { Message } from "@canvas-js/interfaces"
 import { ed25519 } from "@canvas-js/signatures"
@@ -329,60 +328,6 @@ test("get a value set by another action", async (t) => {
 			{ id: b, from: `did:pkh:eip155:1:${wallet.address}`, content: "bar" },
 		].sort(compareIDs),
 	)
-})
-
-test("validate action args using IPLD schemas", async (t) => {
-	const schema = `
-		type CreatePostPayload struct {
-			content String
-			inReplyTo nullable String
-		} representation tuple
-	`
-
-	const wallet = ethers.Wallet.createRandom()
-	const app = await Canvas.initialize({
-		topic: "com.example.app",
-		contract: {
-			models: {
-				posts: {
-					id: "primary",
-					content: "string",
-					timestamp: "integer",
-					address: "string",
-				},
-			},
-			actions: {
-				createPost: {
-					requireSessionAuthentication: false,
-					argsType: { schema, name: "CreatePostPayload" },
-					apply: async (
-						db,
-						{ content, inReplyTo }: { content: string; inReplyTo: string | null },
-						{ id, address, timestamp },
-					) => {
-						const postId = [address, id].join("/")
-						await db.set("posts", { id: postId, content, timestamp, address })
-					},
-				},
-			},
-		},
-		signers: [new SIWESigner({ signer: wallet })],
-	})
-
-	t.teardown(() => app.stop())
-
-	const { id } = await app.actions.createPost({ content: "hello world!", inReplyTo: null })
-
-	// validate that the args are represented as tuples inside the action
-	const signedMessage = await app.getMessage(id)
-	assert(signedMessage !== null && signedMessage.message.payload.type === "action")
-	t.deepEqual(signedMessage.message.payload.args, ["hello world!", null])
-
-	await t.throwsAsync(() => app.actions.createPost({ content: 8 } as any), {
-		message: "action args did not validate the provided schema type",
-	})
-
-	t.is(await app.db.count("posts"), 1)
 })
 
 test("apply an action and read a record from the database using eip712", async (t) => {
