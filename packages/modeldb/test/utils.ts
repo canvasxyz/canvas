6816cd143eabd638fd4a8285796612deb17a2052
Bob Webb <bob.wbb@gmail.com>
2024-10-07 16:44:46 +0200

    re-enable wasm tests

--- packages/modeldb/test/utils.ts
+++ packages/modeldb/test/utils.ts
@@ -21,43 +21,49 @@ import { ModelDBProxy as ModelDBDurableObjectsProxy } from "@canvas-js/modeldb-d
 let browser: puppeteer.Browser
 let page: puppeteer.Page
 let server: ViteDevServer
+let worker: UnstableDevWorker
 
-// test.before(async (t) => {
-// 	server = await createServer({
-// 		root: path.resolve(__dirname, "server"),
-// 	})
+test.before(async (t) => {
+	server = await createServer({
+		root: path.resolve(__dirname, "server"),
+	})
 
-// 	await server.listen()
+	await server.listen()
 
-// 	browser = await puppeteer.launch({
-// 		dumpio: true,
-// 		headless: true,
-// 		args: [
-// 			"--no-sandbox",
-// 			"--disable-setuid-sandbox",
-// 			"--disable-extensions",
-// 			"--enable-chrome-browser-cloud-management",
-// 		],
-// 	})
-// 	page = await browser.newPage()
+	browser = await puppeteer.launch({
+		dumpio: true,
+		headless: true,
+		args: [
+			"--no-sandbox",
+			"--disable-setuid-sandbox",
+			"--disable-extensions",
+			"--enable-chrome-browser-cloud-management",
+		],
+	})
+	page = await browser.newPage()
 
-// 	page.on("workercreated", (worker) => t.log("Worker created: " + worker.url()))
-// 	page.on("workerdestroyed", (worker) => t.log("Worker destroyed: " + worker.url()))
+	page.on("workercreated", (worker) => t.log("Worker created: " + worker.url()))
+	page.on("workerdestroyed", (worker) => t.log("Worker destroyed: " + worker.url()))
 
-// 	page.on("console", async (e) => {
-// 		const args = await Promise.all(e.args().map((a) => a.jsonValue()))
-// 		t.log(...args)
-// 	})
+	page.on("console", async (e) => {
+		const args = await Promise.all(e.args().map((a) => a.jsonValue()))
+		t.log(...args)
+	})
 
-// 	const { port } = server.config.server
-// 	await page.goto(`http://localhost:${port}`)
-// })
+	const { port } = server.config.server
+	await page.goto(`http://localhost:${port}`)
 
-// test.after(async (t) => {
-// 	await page.close()
-// 	await browser.close()
-// 	await server.close()
-// })
+	worker = await unstable_dev("test/worker.ts", {
+		experimental: { disableExperimentalWarning: true },
+	})
+})
+
+test.after(async (t) => {
+	await page.close()
+	await browser.close()
+	await server.close()
+	worker.stop()
+})
 
 function getConnectionConfig() {
 	const { POSTGRES_HOST, POSTGRES_PORT } = process.env
@@ -73,10 +79,6 @@ function getConnectionConfig() {
 		return "postgresql://localhost:5432/test"
 	}
 }
-
-const worker = await unstable_dev("test/worker.ts", {
-	experimental: { disableExperimentalWarning: true },
-})
 
 export const testOnModelDB = (
 	name: string,
@@ -113,30 +115,28 @@ export const testOnModelDB = (
 		return mdb
 	})
 
-	test.after.always(() => worker.stop())
+	test(`Sqlite Wasm Opfs - ${name}`, async (t) => {
+		const testResult = await page.evaluate(async (run) => {
+			// @ts-ignore
+			const ctx = new InnerExecutionContext()
+			const testFunc = eval(`(${run})`)
+			try {
+				// @ts-ignore
+				await testFunc(ctx, openOpfsDB)
+				return { result: "passed" }
+			} catch (error: any) {
+				return { result: "failed", error: error.message }
+			} finally {
+				if (ctx.teardownFunction) ctx.teardownFunction()
+			}
+		}, run.toString())
 
-	// test(`Sqlite Wasm Opfs - ${name}`, async (t) => {
-	// 	const testResult = await page.evaluate(async (run) => {
-	// 		// @ts-ignore
-	// 		const ctx = new InnerExecutionContext()
-	// 		const testFunc = eval(`(${run})`)
-	// 		try {
-	// 			// @ts-ignore
-	// 			await testFunc(ctx, openOpfsDB)
-	// 			return { result: "passed" }
-	// 		} catch (error: any) {
-	// 			return { result: "failed", error: error.message }
-	// 		} finally {
-	// 			if (ctx.teardownFunction) ctx.teardownFunction()
-	// 		}
-	// 	}, run.toString())
-
-	// 	if (testResult.result === "passed") {
-	// 		t.pass()
-	// 	} else {
-	// 		t.fail(testResult.error)
-	// 	}
-	// })
+		if (testResult.result === "passed") {
+			t.pass()
+		} else {
+			t.fail(testResult.error)
+		}
+	})
 	// test(`Sqlite Wasm Transient - ${name}`, async (t) => {
 	// 	const testResult = await page.evaluate(async (run) => {
 	// 		// @ts-ignore
