a2233ef3378d14469c2e53297e52cdebe7bee418
Raymond Z <raykyri@gmail.com>
2024-10-09 03:22:31 -0400

    packages/core: container function syntax tests

--- packages/core/test/container.test.ts
+++ packages/core/test/container.test.ts
@@ -30,7 +30,7 @@ const updateGameMultipleMerges: FnImplementation = async function (db) {
 	await db.merge("game", { id: "0", state: { extra3: null, extra1: { b: 2, c: 3 } } as any })
 }
 
-test("merge into a value set by another action", async (t) => {
+test("apply container actions with db.merge", async (t) => {
 	const app = await Canvas.initializeContainer({
 		connection: { topic: "example.com", url: "https://my-app-replicator.canvas.xyz" },
 		models: {
@@ -75,4 +75,77 @@ test("merge into a value set by another action", async (t) => {
 		},
 		label: "foosball",
 	})
+})
+
+test("apply container actions with different function syntaxes", async (t) => {
+	const external = {
+		testMemberFunction(db: any) {
+			db.set("check", { id: "member" })
+		},
+		testAsyncMemberFunction(db: any) {
+			db.set("check", { id: "async_member" })
+		},
+	}
+	function testExternalFunction(db: any) {
+		db.set("check", { id: "external_named" })
+	}
+
+	const app = await Canvas.initializeContainer({
+		connection: { topic: "example.com", url: "https://my-app-replicator.canvas.xyz" },
+		models: {
+			check: { id: "primary" },
+		},
+		actions: {
+			testArrowFunction: (db) => {
+				db.set("check", { id: "arrow" })
+			},
+			testAsyncArrowFunction: async (db) => {
+				db.set("check", { id: "async_arrow" })
+			},
+			testBareArrowFunction: (db) => db.set("check", { id: "bare_arrow" }),
+			testAnonymousFunction: function (db) {
+				db.set("check", { id: "anonymous" })
+			},
+			testAsyncAnonymousFunction: async function (db) {
+				db.set("check", { id: "async_anonymous" })
+			},
+			testNamedFunction: function foo(db) {
+				db.set("check", { id: "named" })
+			},
+			testAsyncNamedFunction: async function foo(db) {
+				db.set("check", { id: "async_named" })
+			},
+			testExternalFunction,
+			testMemberFunction: external.testMemberFunction,
+			testAsyncMemberFunction: external.testAsyncMemberFunction,
+		},
+		imports: {
+			GLOBAL_ID,
+		},
+	})
+
+	t.teardown(() => app.stop())
+
+	await app.actions.testArrowFunction()
+	await app.actions.testAsyncArrowFunction()
+	await app.actions.testBareArrowFunction()
+	await app.actions.testAnonymousFunction()
+	await app.actions.testAsyncAnonymousFunction()
+	await app.actions.testNamedFunction()
+	await app.actions.testAsyncNamedFunction()
+	await app.actions.testExternalFunction()
+	await app.actions.testMemberFunction()
+	await app.actions.testAsyncMemberFunction()
+
+	t.truthy(await app.db.get("check", "arrow"))
+	t.truthy(await app.db.get("check", "async_arrow"))
+	t.truthy(await app.db.get("check", "bare_arrow"))
+	t.truthy(await app.db.get("check", "anonymous"))
+	t.truthy(await app.db.get("check", "async_anonymous"))
+	t.truthy(await app.db.get("check", "named"))
+	t.truthy(await app.db.get("check", "async_named"))
+	t.truthy(await app.db.get("check", "external_named"))
+	t.truthy(await app.db.get("check", "member"))
+	t.truthy(await app.db.get("check", "async_member"))
+	t.falsy(await app.db.get("check", "nonexistent"))
 })
