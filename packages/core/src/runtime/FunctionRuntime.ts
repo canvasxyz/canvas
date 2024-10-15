970ce514014ea703b6956db4ba4f1c66c59c756d
Raymond Z <raykyri@gmail.com>
2024-10-15 16:25:37 -0400

    packages/core: error when writing to a value with null/undefined primary key

--- packages/core/src/runtime/FunctionRuntime.ts
+++ packages/core/src/runtime/FunctionRuntime.ts
@@ -80,6 +80,7 @@ export class FunctionRuntime<M extends ModelSchema> extends AbstractRuntime {
 				validateModelValue(this.db.models[model], value)
 				const { primaryKey } = this.db.models[model]
 				assert(primaryKey in value, `db.set(${model}): missing primary key ${primaryKey}`)
+				assert(primaryKey !== null && primaryKey !== undefined, `db.set(${model}): ${primaryKey} primary key`)
 				const key = (value as ModelValue)[primaryKey] as string
 				this.#context.modelEntries[model][key] = value
 			},
@@ -88,6 +89,7 @@ export class FunctionRuntime<M extends ModelSchema> extends AbstractRuntime {
 				validateModelValue(this.db.models[model], value)
 				const { primaryKey } = this.db.models[model]
 				assert(primaryKey in value, `db.update(${model}): missing primary key ${primaryKey}`)
+				assert(primaryKey !== null && primaryKey !== undefined, `db.set(${model}): ${primaryKey} primary key`)
 				const key = (value as ModelValue)[primaryKey] as string
 				this.#context.modelEntries[model][key] = value
 			},
@@ -95,6 +97,7 @@ export class FunctionRuntime<M extends ModelSchema> extends AbstractRuntime {
 				assert(this.#context !== null, "expected this.#context !== null")
 				const { primaryKey } = this.db.models[model]
 				assert(primaryKey in value, `db.update(${model}): missing primary key ${primaryKey}`)
+				assert(primaryKey !== null && primaryKey !== undefined, `db.set(${model}): ${primaryKey} primary key`)
 				const key = (value as ModelValue)[primaryKey] as string
 				const modelValue = await this.getModelValue(this.#context, model, key)
 				const mergedValue = updateModelValues(value as ModelValue, modelValue ?? {})
@@ -105,6 +108,7 @@ export class FunctionRuntime<M extends ModelSchema> extends AbstractRuntime {
 				assert(this.#context !== null, "expected this.#context !== null")
 				const { primaryKey } = this.db.models[model]
 				assert(primaryKey in value, `db.merge(${model}): missing primary key ${primaryKey}`)
+				assert(primaryKey !== null && primaryKey !== undefined, `db.set(${model}): ${primaryKey} primary key`)
 				const key = (value as ModelValue)[primaryKey] as string
 				const modelValue = await this.getModelValue(this.#context, model, key)
 				const mergedValue = mergeModelValues(value as ModelValue, modelValue ?? {})
