8ff6d2f996d43185dfa5d73b36d9b915940a37f8
Joel Gustafson <joelg@mit.edu>
2024-10-21 15:07:48 -0400

    examples/chat: remove old contract

--- examples/chat/src/contract.ts
+++ examples/chat/src/contract.ts
@@ -18,20 +18,3 @@ export const contract = {
 		},
 	},
 } satisfies Contract<typeof models>
-
-// export const contract = {
-// 	models: {
-// 		message: {
-// 			id: "primary",
-// 			address: "string",
-// 			content: "string",
-// 			timestamp: "integer",
-// 			$indexes: ["address", "timestamp"],
-// 		},
-// 	},
-// 	actions: {
-// 		async createMessage(db, { content }, { id, address, timestamp }) {
-// 			await db.set("message", { id, address, content, timestamp })
-// 		},
-// 	},
-// } satisfies Contract
