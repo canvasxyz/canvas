46f20e5e365c8571fdc58faa6525ce9df5544f0b
Raymond Z <raykyri@gmail.com>
2024-10-28 22:34:04 -0400

    update examples/encrypted-chat, examples/common-explorer

--- examples/encrypted-chat/src/contract.ts
+++ examples/encrypted-chat/src/contract.ts
@@ -21,12 +21,19 @@ const models = {
 } satisfies ModelSchema
 
 export const actions = {
-	registerEncryptionKey({ key }: { key: string }) {
-		const { db, address } = this
+	registerEncryptionKey(db, { key }: { key: string }) {
+		const { address } = this
 		db.set("encryptionKeys", { address, key })
 	},
-	createEncryptionGroup({ members, groupKeys, groupPublicKey }: { members: string[], groupKeys: EthEncryptedData[], groupPublicKey: string }) {
-		const { db, address } = this
+	createEncryptionGroup(
+		db,
+		{
+			members,
+			groupKeys,
+			groupPublicKey,
+		}: { members: string[]; groupKeys: EthEncryptedData[]; groupPublicKey: string },
+	) {
+		const { address } = this
 		// TODO: enforce the encryption group is sorted correctly, and each groupKey is registered correctly
 		if (members.indexOf(address) === -1) throw new Error()
 		const id = members.join()
@@ -37,9 +44,9 @@ export const actions = {
 			key: groupPublicKey,
 		})
 	},
-	sendPrivateMessage({ group, ciphertext }: { group: string, ciphertext: string }) {
+	sendPrivateMessage(db, { group, ciphertext }: { group: string; ciphertext: string }) {
 		// TODO: check address is in group
-		const { db, timestamp, id } = this
+		const { timestamp, id } = this
 		db.set("privateMessages", { id, ciphertext, group, timestamp })
 	},
 } satisfies Actions<typeof models>
