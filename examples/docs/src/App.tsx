dce53c045a30e88cdc7419ddd65e2c339bfd2af7
Raymond Z <raykyri@gmail.com>
2024-11-30 19:46:21 -0500

    examples/docs: doc editor interface, no crdt

--- examples/docs/src/App.tsx
+++ examples/docs/src/App.tsx
@@ -7,9 +7,9 @@ import { CosmosSigner } from "@canvas-js/chain-cosmos"
 import { SubstrateSigner } from "@canvas-js/chain-substrate"
 import { SolanaSigner } from "@canvas-js/chain-solana"
 
-import type { Contract, Actions, ModelSchema } from "@canvas-js/core"
+import type { Actions, ModelSchema, DeriveModelType } from "@canvas-js/core"
 
-import { useCanvas } from "@canvas-js/hooks"
+import { useCanvas, useLiveQuery } from "@canvas-js/hooks"
 
 import { AppContext } from "./AppContext.js"
 import { ControlPanel } from "./ControlPanel.js"
@@ -20,6 +20,8 @@ import { LogStatus } from "./LogStatus.js"
 
 const topic = "docs-example.canvas.xyz"
 
+const ACTIVE_DOC_KEY = "canvas-docs-active-doc"
+
 const wsURL = import.meta.env.VITE_CANVAS_WS_URL ?? null
 console.log("websocket API URL:", wsURL)
 
@@ -27,22 +29,35 @@ export const App: React.FC<{}> = ({}) => {
 	const [sessionSigner, setSessionSigner] = useState<SessionSigner | null>(null)
 	const [address, setAddress] = useState<string | null>(null)
 
+	const [activeDoc, setActiveDoc] = useState<string | null>(() => {
+		return localStorage.getItem(ACTIVE_DOC_KEY)
+	})
+
+	React.useEffect(() => {
+		if (activeDoc) {
+			localStorage.setItem(ACTIVE_DOC_KEY, activeDoc)
+		} else {
+			localStorage.removeItem(ACTIVE_DOC_KEY)
+		}
+	}, [activeDoc])
+
 	const topicRef = useRef(topic)
 
 	const models = {
-		message: {
+		documents: {
 			id: "primary",
-			address: "string",
 			content: "string",
-			timestamp: "integer",
-			$indexes: ["address", "timestamp"],
+			creator: "string", // immutable
+			$indexes: ["creator"],
 		},
 	} satisfies ModelSchema
 
 	const actions = {
-		async createMessage(db, content) {
-			const { id, address, timestamp } = this
-			await db.set("message", { id, address, content, timestamp })
+		async createDocument(db) {
+			await db.set("documents", { id: this.id, content: "", creator: this.did })
+		},
+		async updateDocument(db, id: string, content: string) {
+			await db.update("documents", { id, content })
 		},
 	} satisfies Actions<typeof models>
 
@@ -52,18 +67,55 @@ export const App: React.FC<{}> = ({}) => {
 		signers: [new SIWESigner(), new ATPSigner(), new CosmosSigner(), new SubstrateSigner({}), new SolanaSigner()],
 	})
 
+	const documents = useLiveQuery<DeriveModelType<typeof models.documents>>(app, "documents")
+
 	return (
 		<AppContext.Provider value={{ address, setAddress, sessionSigner, setSessionSigner, app: app ?? null }}>
 			{app ? (
 				<main>
 					<div className="flex flex-row gap-4 h-full">
-						<div className="min-w-[480px] flex-1 flex flex-col justify-stretch gap-2">
-							TODO
-							{/*
-                 ...
-               */}
+						<div className="min-w-[180px] flex-0 flex flex-col gap-2">
+							<div className="w-full border rounded cursor-pointer py-3">
+								{documents?.map((doc) => (
+									<div
+										key={doc.id}
+										className={`py-1 px-4 hover:bg-gray-100 transition-colors cursor-pointer ${activeDoc === doc.id ? "bg-gray-100" : ""}`}
+										onClick={() => {
+											setActiveDoc(doc.id)
+										}}
+									>
+										{doc.id.slice(0, 12)}...
+									</div>
+								))}
+								<div
+									className="py-1 px-4 hover:bg-gray-100 transition-colors cursor-pointer"
+									onClick={async (event) => {
+										if (!app) return
+										const { id } = await app.actions.createDocument()
+										setActiveDoc(id)
+									}}
+								>
+									New Document
+								</div>
+							</div>
 						</div>
-						<div className="flex flex-col gap-4 w-[480px] break-all">
+						<div className="min-w-[300px] flex-1 flex flex-col justify-stretch gap-2">
+							{activeDoc && (
+								<div className="w-full p-[1px] border rounded">
+									<textarea
+										rows={20}
+										className="w-full h-full outline-none box-shadow-none"
+										value={documents?.find((doc) => doc.id === activeDoc)?.content ?? ""}
+										onChange={async (event) => {
+											if (!app) return
+											if (!activeDoc) return
+											await app.actions.updateDocument(activeDoc, event.target.value)
+										}}
+									></textarea>
+								</div>
+							)}
+						</div>
+						<div className="flex flex-col gap-4 w-[320px] break-all">
 							<Connect />
 							<SessionStatus />
 							<ConnectionStatus topic={topicRef.current} />
