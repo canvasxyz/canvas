b011012e1d0cfadc485cbbfdf461c6436f52ebe4
Bob Webb <bob.wbb@gmail.com>
2025-01-13 18:46:06 +0100

    remove generic search filters code

--- packages/network-explorer/src/hooks/useSearchFilters.ts
+++ packages/network-explorer/src/hooks/useSearchFilters.ts
@@ -2,32 +2,35 @@ import { ColumnFiltersState } from "@tanstack/react-table"
 import { Dispatch, SetStateAction } from "react"
 import { useSearchParams } from "react-router-dom"
 
-function read<O>(searchParams: URLSearchParams, defaultValue: O): O {
-	const searchParamsFilterString = searchParams.get("filter")
-	let value = defaultValue
-	if (searchParamsFilterString) {
-		try {
-			value = JSON.parse(searchParamsFilterString)
-		} catch {}
-	}
-	return value
-}
-
-function write<O>(newValue: O): URLSearchParams {
-	return new URLSearchParams({ filter: JSON.stringify(newValue) })
-}
-
-export const useSearchFilters = function (
+export const useSearchFilters = (
 	defaultValue: ColumnFiltersState,
-): [ColumnFiltersState, Dispatch<SetStateAction<ColumnFiltersState>>] {
+): [ColumnFiltersState, Dispatch<SetStateAction<ColumnFiltersState>>] => {
 	const [searchParams, setSearchParams] = useSearchParams()
 
-	const columnFilters = read(searchParams, defaultValue)
+	const searchParamsFilterString = searchParams.get("filter")
+	let columnFilters = defaultValue
+	if (searchParamsFilterString) {
+		try {
+			columnFilters = JSON.parse(searchParamsFilterString)
+		} catch {}
+	}
 
-	const setColumnFilters = (newColumnFilters: SetStateAction<ColumnFiltersState>) => {
-		const value = Array.isArray(newColumnFilters) ? newColumnFilters : newColumnFilters(columnFilters)
-		const processedValue = write(value)
-		setSearchParams(processedValue)
+	const setColumnFilters = (updater: SetStateAction<ColumnFiltersState>) => {
+		if (Array.isArray(updater)) {
+			setSearchParams(new URLSearchParams({ filter: JSON.stringify(updater) }))
+		} else {
+			setSearchParams((oldParams) => {
+				const searchParamsFilterString = oldParams.get("filter")
+				let columnFilters = defaultValue
+				if (searchParamsFilterString) {
+					try {
+						columnFilters = JSON.parse(searchParamsFilterString)
+					} catch {}
+				}
+
+				return { filter: JSON.stringify(updater(columnFilters)) }
+			})
+		}
 	}
 
 	return [columnFilters, setColumnFilters]
