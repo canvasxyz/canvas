c2736bd4ecab3f9c963c01c67e535811b74aa4ed
Joel Gustafson <joelg@mit.edu>
2025-02-04 13:27:46 -0800

    packages/core: update getRecordId to handle composite primary keys

--- packages/core/src/utils.ts
+++ packages/core/src/utils.ts
@@ -1,10 +1,12 @@
 import AggregateError from "aggregate-error"
 import { anySignal } from "any-signal"
 import { blake3 } from "@noble/hashes/blake3"
+import { utf8ToBytes } from "@noble/hashes/utils"
 import { base64 } from "multiformats/bases/base64"
 
 import { Action, Session, Snapshot } from "@canvas-js/interfaces"
 import { SignedMessage } from "@canvas-js/gossiplog"
+import { PrimaryKeyValue } from "@canvas-js/modeldb"
 
 export const isAction = (
 	signedMessage: SignedMessage<Action | Session | Snapshot>,
@@ -42,7 +44,63 @@ export async function wait(interval: number, options: { signal: AbortSignal }) {
 	}).finally(() => signal.clear())
 }
 
-export function getRecordId(model: string, key: string) {
-	const hash = blake3(`${model}/${key}`, { dkLen: 18 })
-	return base64.baseEncode(hash)
+export function getRecordId(model: string, key: PrimaryKeyValue | PrimaryKeyValue[]): string {
+	const hash2 = blake3.create({ dkLen: 18 })
+
+	const components = Array.isArray(key) ? [model, ...key] : [model, key]
+	hash2.update(writeArgument(majorTypes.ARRAY, components.length))
+
+	for (const value of components) {
+		if (typeof value === "number") {
+			if (value >= 0) {
+				hash2.update(writeArgument(majorTypes.UNSIGNED_INTEGER, value))
+			} else {
+				hash2.update(writeArgument(majorTypes.NEGATIVE_INTEGER, -value - 1))
+			}
+		} else if (typeof value === "string") {
+			const bytes = utf8ToBytes(value)
+			hash2.update(writeArgument(majorTypes.TEXT_STRING, bytes.byteLength))
+			hash2.update(bytes)
+		} else if (value instanceof Uint8Array) {
+			hash2.update(writeArgument(majorTypes.BYTE_STRING, value.byteLength))
+			hash2.update(value)
+		}
+	}
+
+	return base64.baseEncode(hash2.digest())
+}
+
+const majorTypes = {
+	UNSIGNED_INTEGER: 0,
+	NEGATIVE_INTEGER: 1,
+	BYTE_STRING: 2,
+	TEXT_STRING: 3,
+	ARRAY: 4,
+}
+
+const buffers = [new ArrayBuffer(1), new ArrayBuffer(2), new ArrayBuffer(3), new ArrayBuffer(5), new ArrayBuffer(9)]
+const arrays = buffers.map((buffer) => new Uint8Array(buffer))
+const dataViews = buffers.map((buffer) => new DataView(buffer, 1))
+
+function writeArgument(majorType: number, argument: number): Uint8Array {
+	if (argument < 24) {
+		arrays[0][0] = (majorType << 5) | argument
+		return arrays[0]
+	} else if (argument < 0xff) {
+		arrays[1][0] = (majorType << 5) | 24
+		dataViews[1].setUint8(0, argument)
+		return arrays[1]
+	} else if (argument < 0xffff) {
+		arrays[2][0] = (majorType << 5) | 25
+		dataViews[2].setUint16(0, argument, false)
+		return arrays[2]
+	} else if (argument < 0xffffffff) {
+		arrays[3][0] = (majorType << 5) | 26
+		dataViews[3].setUint32(0, argument, false)
+		return arrays[3]
+	} else {
+		arrays[4][0] = (majorType << 5) | 27
+		dataViews[4].setBigUint64(0, BigInt(argument), false)
+		return arrays[4]
+	}
 }
