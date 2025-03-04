0f2ffec424b5b4746bd78d51831dc3af8d745f9c
Bob Webb <bob.wbb@gmail.com>
2025-03-04 14:09:05 +0100

    add yjs to snapshot test

--- packages/core/test/snapshot.test.ts
+++ packages/core/test/snapshot.test.ts
@@ -1,4 +1,5 @@
 import test from "ava"
+import * as Y from "yjs"
 
 import { Canvas, Config } from "@canvas-js/core"
 
@@ -11,6 +12,10 @@ test("snapshot persists data across apps", async (t) => {
 					id: "primary",
 					content: "string",
 				},
+				documents: {
+					id: "primary",
+					content: "yjs-doc",
+				},
 			},
 			actions: {
 				async createPost(db, { id, content }: { id: string; content: string }) {
@@ -18,6 +23,12 @@ test("snapshot persists data across apps", async (t) => {
 				},
 				async deletePost(db, { id }: { id: string }) {
 					await db.delete("posts", id)
+				},
+				async insertIntoDocument(db, key, index, text) {
+					await db.yjsInsert("documents", key, index, text)
+				},
+				async deleteFromDocument(db, key, index, length) {
+					await db.yjsDelete("documents", key, index, length)
 				},
 			},
 		},
@@ -35,9 +46,11 @@ test("snapshot persists data across apps", async (t) => {
 	await app.actions.createPost({ id: "d", content: "baz" })
 	await app.actions.deletePost({ id: "b" })
 	await app.actions.deletePost({ id: "d" })
+	await app.actions.insertIntoDocument("e", 0, "Hello, world")
+	await app.actions.deleteFromDocument("e", 5, 7)
 
 	const [clock, parents] = await app.messageLog.getClock()
-	t.is(clock, 8) // one session, six actions
+	t.is(clock, 12) // one session, eight actions, two "updates" messages
 	t.is(parents.length, 1)
 
 	// snapshot and add some more actions
@@ -52,13 +65,19 @@ test("snapshot persists data across apps", async (t) => {
 	t.is(await app2.db.get("posts", "d"), null)
 	t.is(await app2.db.get("posts", "e"), null)
 
+	const docDiff1 = await app2.db.get("documents:state", "e")
+	const doc1 = new Y.Doc()
+	Y.applyUpdate(doc1, docDiff1!.content)
+	t.is(doc1.getText().toJSON(), "Hello")
+
 	await app2.actions.createPost({ id: "a", content: "1" })
 	await app2.actions.createPost({ id: "b", content: "2" })
 	await app2.actions.createPost({ id: "e", content: "3" })
 	await app2.actions.createPost({ id: "f", content: "4" })
+	await app2.actions.insertIntoDocument("e", 6, "?")
 
 	const [clock2, parents2] = await app2.messageLog.getClock()
-	t.is(clock2, 7) // one snapshot, one session, four actions
+	t.is(clock2, 9) // one snapshot, one session, four actions
 	t.is(parents2.length, 1)
 
 	t.is((await app2.db.get("posts", "a"))?.content, "1")
@@ -67,6 +86,10 @@ test("snapshot persists data across apps", async (t) => {
 	t.is(await app2.db.get("posts", "d"), null)
 	t.is((await app2.db.get("posts", "e"))?.content, "3")
 	t.is((await app2.db.get("posts", "f"))?.content, "4")
+	const docDiff2 = await app2.db.get("documents:state", "e")
+	const doc2 = new Y.Doc()
+	Y.applyUpdate(doc2, docDiff2!.content)
+	t.is(doc2.getText().toJSON(), "Hello?")
 
 	// snapshot a second time
 	const snapshot2 = await app2.createSnapshot()
@@ -79,6 +102,10 @@ test("snapshot persists data across apps", async (t) => {
 	t.is((await app3.db.get("posts", "e"))?.content, "3")
 	t.is((await app3.db.get("posts", "f"))?.content, "4")
 	t.is(await app3.db.get("posts", "g"), null)
+	const docDiff3 = await app3.db.get("documents:state", "e")
+	const doc3 = new Y.Doc()
+	Y.applyUpdate(doc3, docDiff3!.content)
+	t.is(doc3.getText().toJSON(), "Hello?")
 
 	const [clock3] = await app3.messageLog.getClock()
 	t.is(clock3, 2) // one snapshot
