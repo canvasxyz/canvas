303edd82524d53a3df3adff112619364ba9e07e2
Bob Webb <bob.wbb@gmail.com>
2025-03-05 16:11:27 +0100

    WIP basic collaborative editor demo

---
+++ examples/document/README.md
@@ -0,0 +1,54 @@
+# Document Editor Example
+
+[Github](https://github.com/canvasxyz/canvas/tree/main/examples/document)
+
+This example app demonstrates collaborative document editing using the CRDT provided by [Yjs](https://github.com/yjs/yjs). It allows users to collaboratively edit a single text document with persistence over libp2p.
+
+```ts
+export const models = {
+  documents: {
+    id: "primary",
+    content: "yjs-doc",
+  },
+}
+
+export const actions = {
+  async applyDeltaToDoc(db, index, text) {
+    await db.yjsApplyDelta("documents", "0", index, text)
+  },
+}
+```
+
+## Server
+
+Run `npm run dev:server` to start a temporary in-memory server, or
+`npm run start:server` to persist data to a `.cache` directory.
+
+To deploy the replication server:
+
+```
+$ cd server
+$ fly deploy
+```
+
+If you are forking this example, you should change:
+
+- the Fly app name
+- the `ANNOUNCE` environment variable to match your Fly app name
+
+## Running the Docker container locally
+
+Mount a volume to `/data`. Set the `PORT`, `LISTEN`, `ANNOUNCE`, and
+`BOOTSTRAP_LIST` environment variables if appropriate.
+
+## Deploying to Railway
+
+Create a Railway space based on the root of this Github workspace (e.g. canvasxyz/canvas).
+
+- Custom build command: `npm run build && VITE_CANVAS_WS_URL=wss://chat-example.canvas.xyz npm run build --workspace=@canvas-js/example-chat`
+- Custom start command: `./install-prod.sh && canvas run /tmp/canvas-example-chat --port 8080 --static examples/chat/dist --topic chat-example.canvas.xyz --init examples/chat/src/contract.ts`
+- Watch paths: `/examples/chat/**`
+- Public networking:
+  - Add a service domain for port 8080.
+  - Add a service domain for port 4444.
+- Watch path: `/examples/chat/**`. (Only build when chat code is updated, or a chat package is updated.)
---
+++ examples/document/src/App.tsx
@@ -0,0 +1,113 @@
+import React, { useEffect, useRef, useState } from "react"
+
+import type { SessionSigner } from "@canvas-js/interfaces"
+import { Eip712Signer, SIWESigner, SIWFSigner } from "@canvas-js/chain-ethereum"
+import { ATPSigner } from "@canvas-js/chain-atp"
+import { CosmosSigner } from "@canvas-js/chain-cosmos"
+import { SolanaSigner } from "@canvas-js/chain-solana"
+import { SubstrateSigner } from "@canvas-js/chain-substrate"
+
+import { useCanvas, useLiveQuery } from "@canvas-js/hooks"
+
+import { AuthKitProvider } from "@farcaster/auth-kit"
+import { JsonRpcProvider } from "ethers"
+import Quill from "quill/core"
+import * as Y from "yjs"
+
+import { AppContext } from "./AppContext.js"
+import { ControlPanel } from "./ControlPanel.js"
+import { SessionStatus } from "./SessionStatus.js"
+import { ConnectEIP712Burner } from "./connect/ConnectEIP712Burner.js"
+import { ConnectionStatus } from "./ConnectionStatus.js"
+import { LogStatus } from "./LogStatus.js"
+import * as contract from "./contract.js"
+import { Editor } from "./Editor.js"
+
+export const topic = "document-example.canvas.xyz"
+
+const wsURL = import.meta.env.VITE_CANVAS_WS_URL ?? null
+console.log("websocket API URL:", wsURL)
+
+const config = {
+	// For a production app, replace this with an Optimism Mainnet
+	// RPC URL from a provider like Alchemy or Infura.
+	relay: "https://relay.farcaster.xyz",
+	rpcUrl: "https://mainnet.optimism.io",
+	domain: "document-example.canvas.xyz",
+	siweUri: "https://document-example.canvas.xyz",
+	provider: new JsonRpcProvider(undefined, 10),
+}
+
+export const App: React.FC<{}> = ({}) => {
+	const [sessionSigner, setSessionSigner] = useState<SessionSigner | null>(null)
+	const [address, setAddress] = useState<string | null>(null)
+
+	const quillRef = useRef<Quill>()
+
+	const topicRef = useRef(topic)
+
+	const { app, ws } = useCanvas(wsURL, {
+		topic: topicRef.current,
+		contract,
+		signers: [
+			new SIWESigner(),
+			new Eip712Signer(),
+			new SIWFSigner(),
+			new ATPSigner(),
+			new CosmosSigner(),
+			new SubstrateSigner({}),
+			new SolanaSigner(),
+		],
+	})
+
+	const existingStateEntries = useLiveQuery(app, "documents:state", {
+		where: { id: "0" },
+		limit: 1,
+	}) as { id: string; content: Uint8Array }[]
+
+	useEffect(() => {
+		if (existingStateEntries && existingStateEntries.length > 0) {
+			const doc = new Y.Doc()
+			Y.applyUpdate(doc, existingStateEntries[0].content)
+			// doc.getText().toJSON()
+			console.log("doc.getText().toJSON():", doc.getText().toJSON())
+			quillRef.current?.setText(doc.getText().toJSON(), "silent")
+		}
+	}, [existingStateEntries])
+
+	return (
+		<AppContext.Provider value={{ address, setAddress, sessionSigner, setSessionSigner, app: app ?? null }}>
+			<AuthKitProvider config={config}>
+				{app && ws ? (
+					<main>
+						<div className="flex flex-row gap-4 h-full">
+							<div
+								id="quill-editor"
+								className="sm:min-w-[300px] md:min-w-[480px] flex-1 flex flex-col justify-stretch gap-2"
+							>
+								<Editor
+									ref={quillRef}
+									readOnly={false}
+									defaultValue={null}
+									onSelectionChange={() => {}}
+									onTextChange={async (delta) => {
+										await app.actions.applyDeltaToDoc(JSON.stringify(delta))
+									}}
+								/>
+							</div>
+							<div className="flex flex-col gap-4 w-[480px] break-all">
+								<ConnectEIP712Burner />
+								<SessionStatus />
+								<ConnectionStatus topic={topicRef.current} ws={ws} />
+								<LogStatus />
+								<ControlPanel />
+							</div>
+						</div>
+					</main>
+				) : (
+					<div className="text-center my-20">Connecting to {wsURL}...</div>
+				)}
+			</AuthKitProvider>
+		</AppContext.Provider>
+	)
+}
