7eee0a8c63da91a58de2e79b9d9fef643a336b6e
Raymond Z <raykyri@gmail.com>
2025-03-10 17:50:44 -0400

    packages/core: add buildContract to browser using esbuild-wasm

--- packages/core/src/targets/browser/index.ts
+++ packages/core/src/targets/browser/index.ts
@@ -2,6 +2,8 @@ import { GossipLog as IdbGossipLog } from "@canvas-js/gossiplog/idb"
 
 import type { PlatformTarget } from "../interface.js"
 
+import esbuild from "esbuild-wasm"
+
 const target: PlatformTarget = {
 	openGossipLog: ({ path }, init) => {
 		return IdbGossipLog.open(init)
@@ -11,8 +13,26 @@ const target: PlatformTarget = {
 		throw new Error("Cannot start API server in the browser")
 	},
 
-	buildContract(location: string): string {
-		throw new Error("Unimplemented: buildContract in the browser")
+	async buildContract(contract: string) {
+		try {
+			await esbuild.initialize({
+				worker: true,
+				wasmURL: "https://unpkg.com/esbuild-wasm@0.25.1/esbuild.wasm",
+			})
+		} catch (err) {
+			// initialize should only be called once
+		}
+
+		const { code, warnings } = await esbuild.transform(contract, {
+			loader: 'tsx',
+			format: "esm",
+		})
+		for (const warning of warnings) {
+			const location = warning.location ? ` (${warning.location.line}:${warning.location.column})` : ''
+			console.log(`esbuild warning: ${warning.text}${location}`)
+		}
+
+		return code
 	},
 }
 
