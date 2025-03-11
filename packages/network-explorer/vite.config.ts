7ea0b8fa3f8293bcbe1cf142aadecfc01b4b4337
Raymond Z <raykyri@gmail.com>
2025-03-11 19:00:28 -0400

    packages/network-explorer: build in prod

--- packages/network-explorer/vite.config.ts
+++ packages/network-explorer/vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { viteStaticCopy } from "vite-plugin-static-copy"
+import { existsSync } from 'fs'
+import { resolve } from 'path'
+
+const workspaceRootWasmPath = resolve(__dirname, "../../node_modules/esbuild-wasm/esbuild.wasm")
+const subpackageWasmPath = resolve(__dirname, "./node_modules/esbuild-wasm/esbuild.wasm")
+
+let wasmPath
+if (existsSync(workspaceRootWasmPath)) {
+	wasmPath = workspaceRootWasmPath
+ } else if (existsSync(subpackageWasmPath)) {
+	wasmPath = subpackageWasmPath
+ } else {
+	throw new Error("could not find esbuild.wasm, which is required to build network-explorer")
+ }
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,11 +26,11 @@ export default defineConfig({
 		viteStaticCopy({
 			targets: [
 				{
-					src: "../../node_modules/esbuild-wasm/esbuild.wasm",
+					src: wasmPath,
 					dest: "",
 				},
 			],
 		}),
-		react(),
+		react()
 	],
 })
