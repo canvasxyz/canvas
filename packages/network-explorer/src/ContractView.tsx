6f5b9e257299122a26b92d539536674196bd9d06
Joel Gustafson <joelgustafson@protonmail.com>
2025-03-14 15:18:15 -0400

    packages/network-explorer: use new Editor for ContractView.tsx

--- packages/network-explorer/src/ContractView.tsx
+++ packages/network-explorer/src/ContractView.tsx
@@ -1,5 +1,6 @@
 import { Box, Heading, Text } from "@radix-ui/themes"
 import { useContractData } from "./hooks/useContractData.js"
+import { Editor } from "./components/Editor.js"
 
 export const ContractView = () => {
 	const contractData = useContractData()
@@ -13,10 +14,8 @@ export const ContractView = () => {
 			{contractData === null ? (
 				<>Loading...</>
 			) : (
-				<Box style={{ border: '1px solid var(--gray-6)', borderRadius: '6px', width: '100%' }}>
-					<Text size="2">
-						<pre style={{ padding: '4px 20px' }}>{contractData.contract}</pre>
-					</Text>
+				<Box style={{ border: "1px solid var(--gray-6)", borderRadius: "6px", width: "100%" }}>
+					<Editor initialValue={contractData.contract} readOnly />
 				</Box>
 			)}
 		</Box>
