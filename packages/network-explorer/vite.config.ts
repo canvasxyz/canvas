d95feb6d47c99c5e6cb217a37706d2441ad92d44
Raymond Z <raykyri@gmail.com>
2025-03-13 20:17:04 -0400

    ci check for esbuild.wasm

--- packages/network-explorer/vite.config.ts
+++ packages/network-explorer/vite.config.ts
@@ -2,6 +2,16 @@ import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
 import { viteStaticCopy } from "vite-plugin-static-copy"
 
+// debug
+import { existsSync } from "fs"
+import { resolve } from "path"
+
+const workspaceRootWasmPath = resolve(__dirname, "../../node_modules/esbuild-wasm/esbuild.wasm")
+const subpackageWasmPath = resolve(__dirname, "./node_modules/esbuild-wasm/esbuild.wasm")
+
+console.log("workspace root:", existsSync(workspaceRootWasmPath))
+console.log("subpackage path:", existsSync(subpackageWasmPath))
+
 // https://vitejs.dev/config/
 export default defineConfig({
 	build: {
