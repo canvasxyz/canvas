b37d92ccd999ae5b42106d35088536375f0aecb7
Joel Gustafson <joelgustafson@protonmail.com>
2025-03-31 15:16:41 -0400

    packages/core: export decodeRecordKey, encodeRecordKey, decodeRecordValue, and encodeRecordValue from src/utils.ts

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,5 +2,7 @@ export * from "./Canvas.js"
 export * from "./CanvasLoadable.js"
 export * from "./types.js"
 export { hashContract } from "./snapshot.js"
+export { encodeRecordKey, decodeRecordKey, encodeRecordValue, decodeRecordValue } from "./utils.js"
+
 export { Action, Session, Snapshot } from "@canvas-js/interfaces"
 export { NetworkClient } from "@canvas-js/gossiplog"
