e8fbba65c9e70514cf613c3fd385e52a4d8c367a
Raymond Z <raykyri@gmail.com>
2025-04-02 19:38:21 -0400

    packages/hooks: set up remote contract

--- packages/hooks/src/useCanvas.ts
+++ packages/hooks/src/useCanvas.ts
@@ -1,61 +1,133 @@
 import { useState, useEffect, useRef } from "react"
-import { Canvas, NetworkClient, ModelSchema as Models, Config, Snapshot, Actions, hashContract } from "@canvas-js/core"
+import { Canvas, NetworkClient, ModelSchema, Config, Snapshot, Actions, hashContract } from "@canvas-js/core"
 
-export const useCanvas = <ModelsT extends Models = Models, ActionsT extends Actions<ModelsT> = Actions<ModelsT>>(
+/**
+ * React hook for Canvas applications, using client-to-server sync.
+ *
+ * @param url The wss:// endpoint to connect to.
+ * @param config The application to run inside the hook. If `topic` and `contract` are left empty, this will fetch the application from the URL, and the local application will live-update when the server side application is changed.
+ * @returns
+ */
+export const useCanvas = <
+	ModelsT extends ModelSchema = ModelSchema,
+	ActionsT extends Actions<ModelsT> = Actions<ModelsT>,
+>(
 	url: string | null,
-	config: Config<ModelsT, ActionsT>,
+	config: Config<ModelsT, ActionsT> | Omit<Config<ModelsT, ActionsT>, "topic" | "contract">,
 ) => {
 	const [app, setApp] = useState<Canvas<ModelsT, ActionsT>>()
 	const [networkClient, setNetworkClient] = useState<NetworkClient<any>>()
 	const [error, setError] = useState<Error>()
 
-	// TODO: ensure effect hook re-runs on signer change
-	const hashRef = useRef<string>()
-	const snapshotRef = useRef<Snapshot>()
-	const renderedRef = useRef(false) // skip second render in React.StrictMode
+	// TODO: Ensure effect hook re-runs when signers are changed.
+	const localContractHashRef = useRef<string>() // Ref for last-rendered contractHash of a local application.
+	const remoteContractHashRef = useRef<Record<string, string>>({}) // Ref for last-rendered contractHash for remote applications.
+	const snapshotRef = useRef<Snapshot>() // Ref for caching a local application's snapshot.
+	const renderedRef = useRef(false) // Ref for skipping extra render in React.StrictMode.
 
-	const contractHash = hashContract(config.contract)
+	const contractHash = config && "contract" in config ? hashContract(config.contract) : null
 
 	useEffect(() => {
 		if (renderedRef.current) return
 		renderedRef.current = true
 
-		function setupApp(appUrl: string | null, app: Canvas<ModelsT, ActionsT>) {
+		// Assign a new application to the hook's state vars.
+		function assign(appUrl: string | null, app: Canvas<ModelsT, ActionsT>) {
 			if (url) {
-				app.connect(url).then((networkClient) => {
-					setApp(app)
-					setNetworkClient(networkClient)
-				}).catch((err) => {
-					setApp(app)
-					setTimeout(() => setupApp(appUrl, app), 2000)
-				})
+				app
+					.connect(url)
+					.then((networkClient) => {
+						setApp(app)
+						setNetworkClient(networkClient)
+					})
+					.catch((err) => {
+						setApp(app)
+						setTimeout(() => assign(appUrl, app), 2000)
+					})
 			} else {
 				setApp(app)
 			}
 		}
 
-		async function updateSnapshot() {
-			if (!app || contractHash === hashRef.current) {
-				// app just initialized, or contract remains unchanged
-				await Canvas.initialize<ModelsT, ActionsT>(config).then(setupApp.bind(null, url))
-			} else if ((await app.db.count("$messages")) > 1 && snapshotRef.current) {
-				// contract changed, reuse the old snapshot
-				const snapshot = snapshotRef.current
-				await Canvas.initialize<ModelsT, ActionsT>({ ...config, reset: true, snapshot }).then(setupApp.bind(null, url))
-			} else {
-				// contract changed, make a new snapshot
-				const snapshot = await app.createSnapshot()
-				await Canvas.initialize<ModelsT, ActionsT>({ ...config, reset: true, snapshot }).then(setupApp.bind(null, url))
-				snapshotRef.current = snapshot
+		if (contractHash === null) {
+			// Set up a remotely fetched application.
+			async function setupRemoteApplication([info, contractInfo]: [{ topic: string }, { contract: string }]) {
+				if (config === undefined || "topic" in config || "contract" in config) {
+					console.error("Unexpected: Internal error (remote application)")
+					return
+				}
+
+				const topic = info.topic
+				const contract = contractInfo.contract
+				const remoteContractHash = hashContract(contractInfo.contract)
+				let reset: boolean
+
+				// TODO: Maybe the contract is already in IndexedDB?
+				// We should store the contract in ModelDB, and/or always include contractHash in the topic.
+				if (remoteContractHashRef.current[topic] === undefined) {
+					reset = false
+				} else if (contractInfo.contract === remoteContractHashRef.current[topic]) {
+					reset = false
+				} else {
+					reset = true
+				}
+
+				// TODO: Add a remote snapshot.
+				await Canvas.initialize<ModelsT, ActionsT>({
+					topic,
+					contract,
+					reset,
+					...config,
+				})
+					.then(assign.bind(null, url))
+					.finally(() => {
+						remoteContractHashRef.current[topic] = remoteContractHash
+					})
 			}
+
+			const httpRoot = url?.replace("ws://", "http://").replace("wss://", "https://")
+			const baseApi = `${httpRoot}/api`
+			const contractApi = `${httpRoot}/api/contract`
+
+			Promise.all([
+				fetch(baseApi).then((response) => response.json()),
+				fetch(contractApi).then((response) => response.json()),
+			])
+				.then(setupRemoteApplication)
+				.catch((error) => {
+					console.error(error)
+					setError(error)
+				})
+		} else {
+			// Set up the application from a local `config`.
+			// Snapshot, reset, and restart the application from a snapshot if the contract changed.
+			async function setupLocalApplicationBySnapshot() {
+				if (config === undefined || !("topic" in config) || !("contract" in config)) {
+					console.error("Unexpected: Internal error (local application)")
+					return
+				}
+				if (!app || contractHash === localContractHashRef.current) {
+					// Application just initialized, or contract remains unchanged
+					await Canvas.initialize<ModelsT, ActionsT>(config).then(assign.bind(null, url))
+				} else if ((await app.db.count("$messages")) > 1 && snapshotRef.current) {
+					// Contract changed, reuse the old snapshot
+					const snapshot = snapshotRef.current
+					await Canvas.initialize<ModelsT, ActionsT>({ ...config, reset: true, snapshot }).then(assign.bind(null, url))
+				} else {
+					// Contract changed, make a new snapshot
+					const snapshot = await app.createSnapshot()
+					await Canvas.initialize<ModelsT, ActionsT>({ ...config, reset: true, snapshot }).then(assign.bind(null, url))
+					snapshotRef.current = snapshot
+				}
+			}
+
+			setupLocalApplicationBySnapshot().catch((error) => {
+				console.error(error)
+				setError(error)
+			})
+
+			localContractHashRef.current = contractHash
 		}
-
-		updateSnapshot().catch((error) => {
-			console.error(error)
-			setError(error)
-		})
-
-		hashRef.current = contractHash
 	}, [url, contractHash])
 
 	return { app, ws: networkClient, error }
