b2874deb1c5ceac47469b9d6d7d55fdb54494bdf
Raymond Z <raykyri@gmail.com>
2025-04-05 04:17:10 -0400

    examples/chat: follow the server

--- examples/chat/src/App.tsx
+++ examples/chat/src/App.tsx
@@ -46,8 +46,6 @@ export const App: React.FC<{}> = ({}) => {
 	const topicRef = useRef(topic)
 
 	const { app, ws } = useCanvas(wsURL, {
-		topic: topicRef.current,
-		contract,
 		signers: [
 			new SIWESigner(),
 			new Eip712Signer(),
