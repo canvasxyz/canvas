d5ad10e144e573f2f977ef5965cb9a04ba1bf63d
Raymond Z <raykyri@gmail.com>
2025-04-13 03:39:07 -0400

    examples/forum: refactor siwf connection

--- examples/forum/src/connect/ConnectSIWF.tsx
+++ examples/forum/src/connect/ConnectSIWF.tsx
@@ -1,12 +1,13 @@
 import "@farcaster/auth-kit/styles.css"
 
-import React, { useContext, useEffect, useRef, useState } from "react"
+import React, { useCallback, useContext, useEffect, useRef, useState } from "react"
 import { hexlify, getBytes } from "ethers"
 import { SIWFSigner } from "@canvas-js/chain-ethereum"
-import { SignInButton, useProfile } from "@farcaster/auth-kit"
+import { AuthClientError, SignInButton, useProfile, UseSignInData } from "@farcaster/auth-kit"
 import { sdk } from "@farcaster/frame-sdk"
 
 import { AppContext } from "../AppContext.js"
+import { Canvas } from "@canvas-js/core"
 
 export interface ConnectSIWFProps {
 	topic: string
@@ -18,38 +19,114 @@ export const ConnectSIWF: React.FC<ConnectSIWFProps> = ({ topic }) => {
 	const profile = useProfile()
 	const {
 		isAuthenticated,
-		profile: { fid, displayName, custody, verifications },
+		profile: { fid, displayName, custody },
 	} = profile
 
-	const [privateKey, setPrivateKey] = useState<string | null>(null)
-	const [error, setError] = useState<Error | null>(null)
-
-	// requestId for vanilla SIWF, nonce for frame-based SIWF
+	// requestId for browser SIWF, nonce for frame-based SIWF
 	const [requestId, setRequestId] = useState<string | null>(null)
 	const [nonce, setNonce] = useState<string | null>(null)
+	const [newSessionPrivateKey, setNewSessionPrivateKey] = useState<string | null>(null)
 
-	const initialRef = useRef(false)
+	const [error, setError] = useState<Error | null>(null)
+	const initializedRef = useRef(false)
+	
 	useEffect(() => {
-		if (initialRef.current) {
+		if (initializedRef.current) {
 			return
 		}
-		initialRef.current = true
+		initializedRef.current = true
 
-		sdk.context.then((frameContext) => {
-			if (frameContext) {
-				const { nonce, privateKey } = SIWFSigner.newSIWFRequestNonce(topic)
-				setNonce(nonce)
-				setPrivateKey(hexlify(privateKey))
-				sdk.actions.ready()
-			} else {
-				const { requestId, privateKey } = SIWFSigner.newSIWFRequestId(topic)
-				setRequestId(requestId)
-				setPrivateKey(hexlify(privateKey))
-			}
-		}).catch((err) => {
-			alert('Error initializing FrameSDK, application may be out of date.')
-		})
-	}, [])
+		sdk.context
+			.then((frameContext) => {
+				if (frameContext) {
+					// inside a frame
+					const { nonce, privateKey } = SIWFSigner.newSIWFRequestNonce(topic)
+					setNonce(nonce)
+					setNewSessionPrivateKey(hexlify(privateKey))
+					sdk.actions.ready()
+				} else {
+					// inside the browser
+					const { requestId, privateKey } = SIWFSigner.newSIWFRequestId(topic)
+					setRequestId(requestId)
+					setNewSessionPrivateKey(hexlify(privateKey))
+				}
+			})
+			.catch((err) => {
+				alert("Error initializing FrameSDK, application may be out of date.")
+			})
+	}, [topic])
+
+	const frameSignIn = useCallback(async () => {
+		if (!app || !nonce || !newSessionPrivateKey) return
+
+		const now = new Date()
+		const exp = new Date(now.getTime() + 10 * 60 * 1000)
+
+		const result = await sdk.actions
+			.signIn({
+				nonce,
+				notBefore: now.toISOString(),
+				expirationTime: exp.toISOString(),
+			})
+			.catch((err) => {
+				const message = err.message
+				alert(`SIWF frame sign-in error: ${message}`)
+				throw err
+			})
+
+		const { message, signature } = result
+
+		const { authorizationData, topic, custodyAddress } = SIWFSigner.parseSIWFMessage(message, signature)
+		const signer = new SIWFSigner({ custodyAddress, privateKey: newSessionPrivateKey.slice(2) })
+
+		const address = await signer.getDid()
+		const timestamp = new Date(authorizationData.siweIssuedAt).valueOf()
+		const { payload, signer: delegateSigner } = await signer
+			.newSIWFSession(topic, authorizationData, timestamp, getBytes(newSessionPrivateKey))
+			.catch((err) => {
+				const message = err.message
+				alert(`SIWF new session error: ${message}`)
+				throw err
+			})
+		setAddress(address)
+		setSessionSigner(signer)
+		app.updateSigners([
+			signer,
+			...app.signers.getAll().filter((signer) => signer.key !== "chain-ethereum-farcaster"),
+		])
+		app.messageLog.append(payload, { signer: delegateSigner })
+		console.log("started SIWF chat session inside frame", authorizationData)
+	}, [app, nonce, newSessionPrivateKey, topic])
+
+	const browserSignIn = useCallback(async (result: UseSignInData) => {
+		if (!app || !nonce || !newSessionPrivateKey) return
+
+		const { signature, message } = result
+		if (!message || !signature) {
+			setError(new Error("login succeeded but did not return a valid SIWF message"))
+			return
+		}
+
+		const { authorizationData, topic, custodyAddress } = SIWFSigner.parseSIWFMessage(message, signature)
+		const signer = new SIWFSigner({ custodyAddress, privateKey: newSessionPrivateKey.slice(2) })
+		const address = await signer.getDid()
+
+		const timestamp = new Date(authorizationData.siweIssuedAt).valueOf()
+		const { payload, signer: delegateSigner } = await signer.newSIWFSession(
+			topic,
+			authorizationData,
+			timestamp,
+			getBytes(newSessionPrivateKey),
+		)
+		setAddress(address)
+		setSessionSigner(signer)
+		app.updateSigners([
+			signer,
+			...app.signers.getAll().filter((signer) => signer.key !== "chain-ethereum-farcaster"),
+		])
+		app.messageLog.append(payload, { signer: delegateSigner })
+		console.log("started SIWF chat session", authorizationData)
+	}, [app, nonce, newSessionPrivateKey, topic])
 
 	if (error !== null) {
 		return (
@@ -57,7 +134,7 @@ export const ConnectSIWF: React.FC<ConnectSIWFProps> = ({ topic }) => {
 				<code>{error.message}</code>
 			</div>
 		)
-	} else if (!privateKey || (!requestId && !nonce) || !app) {
+	} else if (!newSessionPrivateKey || (!requestId && !nonce) || !app) {
 		return (
 			<div className="p-2 border rounded bg-gray-200">
 				<button disabled>Loading...</button>
@@ -68,100 +145,28 @@ export const ConnectSIWF: React.FC<ConnectSIWFProps> = ({ topic }) => {
 			<div style={{ marginTop: "12px", right: "12px" }}>
 				{isAuthenticated && (
 					<div>
-						<p>
-							Hello, {displayName}! Your FID is {fid}.
-						</p>
-						<p>Your custody address is: </p>
-						<pre>{custody}</pre>
-						<p>Your connected signers: </p>
-						{verifications?.map((v, i) => <pre key={i}>{v}</pre>)}
+						{displayName} (FID: {fid}, Custody: {custody?.slice(0, 6)})
 					</div>
 				)}
 				{/* frame login */}
-				{nonce && (
+				{nonce && !isAuthenticated && (
 					<button
 						type="submit"
 						className="w-full p-2 border rounded hover:cursor-pointer hover:bg-gray-100 active:bg-gray-200"
-						onClick={async () => {
-							const now = new Date()
-							const exp = new Date(now.getTime() + 10 * 60 * 1000)
-
-							const result = await sdk.actions.signIn({
-								nonce,
-								notBefore: now.toISOString(),
-								expirationTime: exp.toISOString(),
-							}).catch((err) => {
-								const message = err.message
-								alert(`SIWF frame sign-in error: ${message}`)
-								throw err
-							})
-							alert(JSON.stringify(result))
-
-							const { message, signature } = result
-
-							const { authorizationData, topic, custodyAddress } = SIWFSigner.parseSIWFMessage(message, signature)
-							const signer = new SIWFSigner({ custodyAddress, privateKey: privateKey.slice(2) })
-
-							const address = await signer.getDid()
-							const timestamp = new Date(authorizationData.siweIssuedAt).valueOf()
-							const { payload, signer: delegateSigner } = await signer.newSIWFSession(
-								topic,
-								authorizationData,
-								timestamp,
-								getBytes(privateKey),
-							).catch((err) => {
-								const message = err.message
-								alert(`SIWF new session error: ${message}`)
-								throw err
-							})
-							setAddress(address)
-							setSessionSigner(signer)
-							app.updateSigners([
-								signer,
-								...app.signers.getAll().filter((signer) => signer.key !== "chain-ethereum-farcaster"),
-							])
-							app.messageLog.append(payload, { signer: delegateSigner })
-							console.log("started SIWF chat session inside frame", authorizationData)
-						}}
+						onClick={frameSignIn}
 					>
 						Sign in with Farcaster (Frame)
 					</button>
 				)}
 				{/* non-frame login */}
-				{requestId && (
+				{requestId && !isAuthenticated && (
 					<SignInButton
 						requestId={requestId}
-						onSuccess={async (result) => {
-							const { signature, message } = result
-							if (!message || !signature) {
-								setError(new Error("login succeeded but did not return a valid SIWF message"))
-								return
-							}
-
-							const { authorizationData, topic, custodyAddress } = SIWFSigner.parseSIWFMessage(message, signature)
-							const signer = new SIWFSigner({ custodyAddress, privateKey: privateKey.slice(2) })
-							const address = await signer.getDid()
-
-							const timestamp = new Date(authorizationData.siweIssuedAt).valueOf()
-							const { payload, signer: delegateSigner } = await signer.newSIWFSession(
-								topic,
-								authorizationData,
-								timestamp,
-								getBytes(privateKey),
-							)
-							setAddress(address)
-							setSessionSigner(signer)
-							app.updateSigners([
-								signer,
-								...app.signers.getAll().filter((signer) => signer.key !== "chain-ethereum-farcaster"),
-							])
-							app.messageLog.append(payload, { signer: delegateSigner })
-							console.log("started SIWF chat session", authorizationData)
+						onSuccess={browserSignIn}
+						onError={(error: AuthClientError | undefined) => {
+							console.log("Browser SIWF login error:", error)
 						}}
-						onError={(...args) => {
-							console.log("received SIWF error", args)
-						}}
-						onSignOut={(...args) => {
+						onSignOut={() => {
 							setAddress(null)
 							setSessionSigner(null)
 						}}
