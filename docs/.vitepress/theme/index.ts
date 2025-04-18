46317c41e703bbf9617f1635ab9be2eac0a0d5e5
Raymond Z <raykyri@gmail.com>
2025-04-18 14:33:28 -0400

    docs: fix

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -14,7 +14,7 @@ import TextRow from "../components/TextRow.vue"
 import HomepageFooter from "../components/HomepageFooter.vue"
 import FeatureTags from "../components/FeatureTags.vue"
 import EmailForm from "../components/EmailForm.vue"
-import Version from "../components/version.vue"
+import Version from "../components/Version.vue"
 import { MotionPlugin } from "@vueuse/motion"
 import "./style.css"
 
