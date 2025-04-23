7623e2132415df1e5f02302bfa5e9c25855dc4ef
Raymond Z <raykyri@gmail.com>
2025-04-23 12:16:49 -0400

    simplified generated actions

--- packages/core/src/runtime/utils.ts
+++ packages/core/src/runtime/utils.ts
@@ -28,117 +28,99 @@ export const generateActions = <T extends ModelSchema>(rules: Record<string, Rul
 	for (const [modelName, modelRules] of Object.entries(rules)) {
 		/**
 		 * Create rules run on the data that is being *written* to a database row.
-		 * Update/delete rules run on the data that is being *read from* a database row,
-		 * necessarily from a local perspective.
+		 * Update/delete rules run on the data that is being *read from* a database row.
 		 */
-		const createRule = modelRules["create"]
-		const updateRule = modelRules["update"]
-		const deleteRule = modelRules["delete"]
+		const createRule = modelRules["create"].toString()
+		const updateRule = modelRules["update"].toString()
+		const deleteRule = modelRules["delete"].toString()
 
-		const checkCreateRule = async (rule: string, context: ActionContext<DeriveModelTypes<T>>, newModel: any) => {
-			const ruleFunction = new Function("$model", `with ($model) { return (${rule}) }`)
-			const result = ruleFunction.call(context, newModel)
+		/**
+		 * These functions use outside variables from the closure:
+		 *
+		 * - createRule, updateRule, deleteRule: string
+		 * - modelName: string
+		 * - models: {} // used to access model schema information for pk lookups
+		 */
+
+		const createAction = async function proxiedCreateAction(
+			this: ActionContext<DeriveModelTypes<T>>,
+			newModel: DeriveModelTypes<T>[string],
+		) {
+			console.log("createAction", this, newModel)
+			const ruleFunction = new Function("$model", `with ($model) { return (${createRule}) }`)
+			const result = ruleFunction.call(this, newModel)
 			if (result !== true) {
-                const contextString = JSON.stringify({ ...newModel, this: context })
 				throw new Error(
-					`Create rule check failed: ${rule} returned ${result}, context: ${contextString}`,
+					`Create rule check failed: ${createRule} returned ${result}, context: ${JSON.stringify({ ...newModel, this: this })}`,
 				)
 			}
+
+			await this.db.set(modelName, newModel)
 		}
-		const checkUpdateDeleteRule = async (
-			rule: string,
-			context: ActionContext<DeriveModelTypes<T>>,
-			existingModel: any,
-			update?: boolean,
-		) => {
-			const ruleFunction = new Function("$model", `with ($model) { return (${rule}) }`)
-			const result = ruleFunction.call(context, existingModel)
-			if (result !== true) {
-                const contextString = JSON.stringify({ ...existingModel, this: context })
+
+		const updateAction = async function proxiedUpdateAction(
+			this: ActionContext<DeriveModelTypes<T>>,
+			newModel: Partial<DeriveModelTypes<T>[string]>,
+		) {
+			const findPrimaryKey = (m: ModelInit) => {
+				if (models[modelName]["$primary"] !== undefined) {
+					return models[modelName]["$primary"]
+				}
+				const tuple = Object.entries(m).find(([k, v]) => {
+					return v === "primary"
+				})
+				if (!tuple) {
+					throw new Error("Must provide model primary key")
+				}
+				return tuple[0]
+			}
+
+			const primaryKey = models[modelName]["$primary"] ?? findPrimaryKey(models[modelName])
+			assert(
+				primaryKey in newModel && typeof newModel[primaryKey as keyof typeof newModel] === "string",
+				"Must provide model primary key",
+			)
+			const existingModel = await this.db.get(modelName, newModel[primaryKey as keyof typeof newModel] as string)
+
+			// check update rule
+			const updateRuleFunction = new Function("$model", `with ($model) { return (${updateRule}) }`)
+			const updateResult = updateRuleFunction.call(this, existingModel)
+			if (updateResult !== true) {
 				throw new Error(
-					`${update ? "Update" : "Delete"} rule check failed: ${rule} returned ${result}, context: ${contextString}`,
+					`Update rule check failed: ${updateRule} returned ${updateResult}, context: ${JSON.stringify({ ...existingModel, this: this })}`,
 				)
 			}
-		}
 
-		const createAction = async function (
-			this: ActionContext<DeriveModelTypes<T>>,
-			createdModel: DeriveModelTypes<T>[string],
-		) {
-			if (createRule === false) {
-				throw new Error("Disallowed by $rules.create")
-			} else if (createRule === true) {
-				await this.db.set(modelName, createdModel)
-			} else {
-				await checkCreateRule(createRule, this, createdModel)
-				await this.db.set(modelName, createdModel)
+			// check create rule
+			const createRuleFunction = new Function("$model", `with ($model) { return (${createRule}) }`)
+			const createResult = createRuleFunction.call(this, newModel)
+			if (createResult !== true) {
+				throw new Error(
+					`Create rule check failed: ${createRule} returned ${createResult}, context: ${JSON.stringify({ ...newModel, this: this })}`,
+				)
 			}
+
+			await this.db.update(modelName, newModel)
 		}
-		const updateAction = async function (
-			this: ActionContext<DeriveModelTypes<T>>,
-			updatedModel: Partial<DeriveModelTypes<T>[string]>,
-		) {
-            const findPrimaryKey = (m: ModelInit) => {
-                if (models[modelName]["$primary"] !== undefined) {
-                    return models[modelName]["$primary"]
-                }
-                const tuple = Object.entries(m).find(([k, v]) => {
-                    return v === "primary"
-                })
-                if (!tuple) {
-                    throw new Error("Must provide model primary key")
-                }
-                return tuple[0]
-            }
+		const deleteAction = async function proxiedDeleteAction(this: ActionContext<DeriveModelTypes<T>>, pk: string) {
+			const existing = await this.db.get(modelName, pk)
 
-			if (updateRule === false) {
-				throw new Error("Disallowed by $rules.delete")
-			} else if (updateRule === true) {
-                const primaryKey = findPrimaryKey(models[modelName])
-				assert(primaryKey in updatedModel && typeof updatedModel[primaryKey as keyof typeof updatedModel] === "string", "Must provide model primary key")
-
-				if (createRule === false) {
-					throw new Error("Disallowed by $rules.create")
-				} else if (createRule !== true) {
-					await checkCreateRule(createRule, this, updatedModel)
-				}
-
-				await this.db.update(modelName, updatedModel)
-			} else {
-                const primaryKey = models[modelName]["$primary"] ?? findPrimaryKey(models[modelName])
-				assert(primaryKey in updatedModel && typeof updatedModel[primaryKey as keyof typeof updatedModel] === "string", "Must provide model primary key")
-				const existingModel = await this.db.get(modelName, updatedModel[primaryKey as keyof typeof updatedModel] as string)
-				await checkUpdateDeleteRule(updateRule, this, existingModel, true)
-
-				if (createRule === false) {
-					throw new Error("Disallowed by $rules.create")
-				} else if (createRule !== true) {
-					await checkCreateRule(createRule, this, updatedModel)
-				}
-
-				await this.db.update(modelName, updatedModel)
+			// check delete rule
+			const deleteRuleFunction = new Function("$model", `with ($model) { return (${deleteRule}) }`)
+			const deleteResult = deleteRuleFunction.call(this, existing)
+			if (deleteResult !== true) {
+				throw new Error(
+					`Delete rule check failed: ${deleteRule} returned ${deleteResult}, context: ${pk}, ${JSON.stringify({ this: this })}`,
+				)
 			}
-		}
-		const deleteAction = async function (this: ActionContext<DeriveModelTypes<T>>, pk: string) {
-			if (deleteRule === false) {
-				throw new Error("Disallowed by $rules.delete")
-			} else if (deleteRule === true) {
-				await this.db.delete(modelName, pk)
-			} else {
-				const existing = await this.db.get(modelName, pk)
-				checkUpdateDeleteRule(deleteRule, this, existing)
-				await this.db.delete(modelName, pk)
-			}
+
+			await this.db.delete(modelName, pk)
 		}
 
 		const action = capitalize(modelName)
-		const createActionName = `create${action}`
-		const updateActionName = `update${action}`
-		const deleteActionName = `delete${action}`
-
-		actions[createActionName] = createAction
-		actions[updateActionName] = updateAction
-		actions[deleteActionName] = deleteAction
+		actions[`create${action}`] = createAction
+		actions[`update${action}`] = updateAction
+		actions[`delete${action}`] = deleteAction
 	}
 
 	return actions
