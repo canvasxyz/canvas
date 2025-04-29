68f26c7c73b4af1dd9d0255ffe9d7e913cec227e
Bob Webb <bob.wbb@gmail.com>
2025-04-29 11:46:52 +0200

    fix bug where multiple rows cannot be deleted with one click

--- packages/network-explorer/src/hooks/useChangedRows.tsx
+++ packages/network-explorer/src/hooks/useChangedRows.tsx
@@ -18,21 +18,30 @@ export const useChangedRows = () => {
 		ImmutableMap(),
 	)
 
-	const stageRowChange = useCallback((tableName: string, rowKey: RowKey, rowChange: RowChange) => {
-		const tableRows = changedRows.get(tableName) || ImmutableMap()
-		const newTableRows = tableRows.set(encodeRowKey(rowKey), rowChange)
-		setChangedRows(changedRows.set(tableName, newTableRows))
-	}, [changedRows])
+	const stageRowChange = useCallback(
+		(tableName: string, rowKey: RowKey, rowChange: RowChange) => {
+			setChangedRows((oldChangedRows) => {
+				const tableRows = oldChangedRows.get(tableName) || ImmutableMap()
+				const newTableRows = tableRows.set(encodeRowKey(rowKey), rowChange)
+				return oldChangedRows.set(tableName, newTableRows)
+			})
+		},
+		[changedRows],
+	)
 
-	const restoreRowChange = useCallback((tableName: string, rowKey: string[]) => {
-		const tableRows = changedRows.get(tableName) || ImmutableMap()
-		const newTableRows = tableRows.delete(encodeRowKey(rowKey))
-		if (newTableRows.size === 0) {
-			setChangedRows(changedRows.delete(tableName))
-		} else {
-			setChangedRows(changedRows.set(tableName, newTableRows))
-		}
-	}, [changedRows])
+	const restoreRowChange = useCallback(
+		(tableName: string, rowKey: string[]) =>
+			setChangedRows((oldChangedRows) => {
+				const tableRows = changedRows.get(tableName) || ImmutableMap()
+				const newTableRows = tableRows.delete(encodeRowKey(rowKey))
+				if (newTableRows.size === 0) {
+					return oldChangedRows.delete(tableName)
+				} else {
+					return oldChangedRows.set(tableName, newTableRows)
+				}
+			}),
+		[changedRows],
+	)
 
 	const clearRowChanges = useCallback(() => {
 		setChangedRows(ImmutableMap())
