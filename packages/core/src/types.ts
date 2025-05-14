8190eefe632988cd4580797b7ed457c38c6cbc96
Raymond Z <raykyri@gmail.com>
2025-05-14 01:08:14 -0400

    packages/core: add DeriveActions type for model-only contracts

--- packages/core/src/types.ts
+++ packages/core/src/types.ts
@@ -2,13 +2,32 @@ import type { ModelValue } from "@canvas-js/modeldb"
 import type { Awaitable } from "@canvas-js/interfaces"
 
 export type { ModelValue } from "@canvas-js/modeldb"
-import type { ModelInit as DbModelInit, ModelSchema as DbModelSchema, DeriveModelTypes as DbDeriveModelTypes, DeriveModelType as DbDeriveModelType } from "@canvas-js/modeldb"
+import type {
+	ModelInit as DbModelInit,
+	ModelSchema as DbModelSchema,
+	DeriveModelTypes as DbDeriveModelTypes,
+	DeriveModelType as DbDeriveModelType,
+} from "@canvas-js/modeldb"
 
 export type RulesInit = { create: string | boolean; update: string | boolean; delete: string | boolean }
 export type ModelInit = DbModelInit<{ $rules?: RulesInit }>
 export type ModelSchema = DbModelSchema<{ $rules?: RulesInit }>
 export type DeriveModelType<T extends ModelSchema> = DbDeriveModelType<T, { $rules?: RulesInit }>
 export type DeriveModelTypes<T extends ModelSchema> = DbDeriveModelTypes<T, { $rules?: RulesInit }>
+
+export type DeriveActions<T extends ModelSchema> = {
+	[K in keyof T as T[K] extends { $rules: any } ? `create${Capitalize<string & K>}` : never]: (
+		item: Partial<DeriveModelTypes<T>[K]>,
+	) => Promise<void>
+} & {
+	[K in keyof T as T[K] extends { $rules: any } ? `update${Capitalize<string & K>}` : never]: (
+		item: DeriveModelTypes<T>[K],
+	) => Promise<void>
+} & {
+	[K in keyof T as T[K] extends { $rules: any } ? `delete${Capitalize<string & K>}` : never]: (
+		id: string,
+	) => Promise<void>
+}
 
 export type Contract<
 	ModelsT extends ModelSchema = ModelSchema,
