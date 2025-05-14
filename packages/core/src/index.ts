a951bf8d54a5009979d5d7f6f0f5fabf5106ce96
Raymond Z <raykyri@gmail.com>
2025-05-14 15:05:24 -0400

    packages/core: add action transformers for arrow function apps

--- packages/core/src/index.ts
+++ packages/core/src/index.ts
@@ -2,6 +2,7 @@ export * from "./Canvas.js"
 export * from "./CanvasLoadable.js"
 export * from "./types.js"
 
+export { TransformActionParams, transformArrowFns, transformArrowFnsTransactional } from "./compatibility.js"
 export { TableChange, RowChange, hashContract, hashSnapshot, generateChangesets } from "./snapshot.js"
 export { encodeRecordKey, decodeRecordKey, encodeRecordValue, decodeRecordValue } from "./utils.js"
 
