26bc78bb376c7282a36987534570ba396fdb0c2f
Raymond Z <raykyri@gmail.com>
2025-05-16 13:00:38 -0400

    packages/core: move synchronous initializer to its own export, add a test, fix proxied fn context

--- packages/core/src/synchronous.ts
+++ packages/core/src/synchronous.ts
@@ -1,26 +1,30 @@
 import type { ModelSchema } from "@canvas-js/core"
 import { assert } from "@canvas-js/utils"
 
-import type { Actions } from "./types.js"
+import type { Actions, ModelValue } from "./types.js"
 import type { Config } from "./Canvas.js"
-import { Canvas } from "./Canvas.js"
+import { Canvas as Core } from "./Canvas.js"
+import { ModelValueWithIncludes, QueryParams } from "@canvas-js/modeldb"
+import { Awaitable } from "@canvas-js/interfaces"
 
 /**
  * A synchronously loaded proxy for the main Canvas object, that
- * defers calls on `app.actions` and `app.db`.
+ * provides deferred calls on `app.actions` and `app.db`.
+ * Other calls will fail with an error until the object is initialized.
  *
- * This can be used in SSR applications, in the Vercel Edge runtime,
- * and other places that require an immediately usable `Canvas` object
- * by calling `const app = CanvasLoadable({ ... })`.
+ * This can be used in SSR applications that require an immediately
+ * usable `Canvas` object: `new Canvas({ ... })`
  */
-export class CanvasLoadable<M extends ModelSchema = any, A extends Actions<M> = Actions<M>> {
-	initPromise: Promise<Canvas<M, Actions<M>>>
+export class Canvas<M extends ModelSchema = any, A extends Actions<M> = Actions<M>> {
+	initPromise: Promise<Core<M, Actions<M>>>
 	ready: boolean
-	app: Canvas<M, Actions<M>> | undefined
-	actions: {}
+	app: Core<M, Actions<M>> | undefined
+	actions: any
+	db: any
 
 	constructor(config: Config<M>, { disableSSR }: { disableSSR?: boolean } = {}) {
-		this.actions = {} // stub actions for ssr
+		this.actions = {} // empty actions stub
+		this.db = {}
 
 		if (disableSSR && typeof window === "undefined") {
 			this.initPromise = Promise.reject()
@@ -28,11 +32,11 @@ export class CanvasLoadable<M extends ModelSchema = any, A extends Actions<M> = 
 			return
 		}
 
-		if (typeof config.contract === "string") {
-			throw new Error("unsupported")
-		}
+		// if (typeof config.contract === "string") {
+		// 	throw new Error("unsupported")
+		// }
 
-		this.initPromise = Canvas.initialize(config)
+		this.initPromise = Core.initialize(config)
 		this.ready = false
 
 		this.initPromise.then((app) => {
@@ -55,10 +59,10 @@ export class CanvasLoadable<M extends ModelSchema = any, A extends Actions<M> = 
 			get: (innerTarget, call) => {
 				if (call === "subscribe") {
 					if (!this.app)
-						return async (...args) => {
+						return async (modelName: string, queryParams: QueryParams, callback: (results: ModelValue[] | ModelValueWithIncludes[]) => Awaitable<void>) => {
 							await this.initPromise
 							assert(this.app, "app failed to initialize")
-							return this.app.db.subscribe.call(this.app.db, ...args)
+							return this.app.db.subscribe.call(this.app.db, modelName, queryParams, callback)
 						}
 					return this.app.db.subscribe.bind(this.app.db)
 				}
@@ -83,12 +87,16 @@ export class CanvasLoadable<M extends ModelSchema = any, A extends Actions<M> = 
 				} else if (prop === "initPromise" || prop === "ready") {
 					return target[prop]
 				} else if (!target.app) {
-					throw new Error("CanvasLoadable: only actions and db can be accessed before initialization")
+					throw new Error("@canvas-js/core/sync: only actions and db can be accessed before initialization")
 				} else {
 					assert(typeof prop === "string")
-					return (target.app as any)[prop]
+					return typeof (target.app as any)[prop] === "function" ? (target.app as any)[prop].bind(target.app) : (target.app as any)[prop]
 				}
 			},
 		})
 	}
+}
+
+declare module "./synchronous.js" {
+	interface Canvas<M extends ModelSchema, A extends Actions<M>> extends Omit<Core<M, A>, 'addEventListener' | 'removeEventListener' | 'dispatchEvent'> {}
 }
