d6a0e12f6a9fe7e932f879fcd49b485e1cbc3e8e
Raymond Z <raykyri@gmail.com>
2025-06-12 14:39:57 -0400

    packages/hooks: add bare useSyncStatus hook

---
+++ packages/hooks/src/useSyncStatus.ts
@@ -0,0 +1,21 @@
+import { useState, useEffect } from "react"
+import { Canvas, ClientSyncStatus } from "@canvas-js/core"
+
+export const useSyncStatus = (app: Canvas) => {
+	const [syncStatus, setSyncStatus] = useState<ClientSyncStatus>("offline")
+	useEffect(() => {
+		if (!app) return
+		const updateSyncStatus = () => {
+			setTimeout(() => setSyncStatus(app.syncStatus))
+		}
+		app.messageLog.addEventListener("connect", updateSyncStatus)
+		app.messageLog.addEventListener("disconnect", updateSyncStatus)
+		app.messageLog.addEventListener("sync:status", updateSyncStatus)
+		return () => {
+			app.messageLog.removeEventListener("connect", updateSyncStatus)
+			app.messageLog.removeEventListener("disconnect", updateSyncStatus)
+			app.messageLog.removeEventListener("sync:status", updateSyncStatus)
+		}
+	}, [app])
+	return syncStatus
+}
