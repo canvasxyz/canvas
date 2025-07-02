c383aa8eef3cced6d19b8767d5329341ae115ac4
Joel Gustafson <joelgustafson@protonmail.com>
2025-07-02 14:05:47 -0400

    packages/client: memoize query string

--- packages/client/src/useLiveQuery.ts
+++ packages/client/src/useLiveQuery.ts
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react"
+import { useEffect, useMemo, useState } from "react"
 
 import * as json from "@ipld/dag-json"
 
@@ -12,19 +12,21 @@ export function useLiveQuery<T extends ModelValue = ModelValue>(
 	query: Exclude<QueryParams, "include"> | null | undefined,
 ): T[] | null {
 	const [results, setResults] = useState<T[] | null>(null)
+	const queryString = useMemo(() => json.stringify(query), [query])
+
 	useEffect(() => {
 		if (!client || !model || !query) {
 			return
 		}
 
-		const queryParams = new URLSearchParams({ model, query: json.stringify(query) })
+		const queryParams = new URLSearchParams({ model, query: queryString })
 		const eventSource = new EventSource(`${client.host}/api/subscribe?${queryParams}`)
 		eventSource.addEventListener("message", (event) => {
 			setResults(json.parse(event.data))
 		})
 
 		return () => eventSource.close()
-	}, [client, model, query])
+	}, [client, model, queryString])
 
 	return results
 }
