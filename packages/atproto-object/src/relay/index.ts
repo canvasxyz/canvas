e5be0fc31650de63eeb76777118c132f278351b1
Raymond Z <raykyri@gmail.com>
2025-07-22 12:09:34 -0400

    packages/atproto-object: move

--- packages/atproto-object/src/relay/index.ts
+++
@@ -1,1 +0,0 @@
-export * from "./relay.js"
--- packages/atproto-object/test/lexicons/app/bsky/embed/index.ts
+++
@@ -1,6 +0,0 @@
-import images from "./images.json" with { type: "json" }
-import external from "./external.json" with { type: "json" }
-import record from "./record.json" with { type: "json" }
-import recordWithMedia from "./recordWithMedia.json" with { type: "json" }
-
-export { images, external, record, recordWithMedia }
